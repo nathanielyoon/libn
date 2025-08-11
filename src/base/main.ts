a51f3448c706163eb93bed1314f5a48a8dcf093e
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-11 14:27:56 -0400

    doc: add module doc comment

--- src/base/main.ts
+++ src/base/main.ts
@@ -1,3 +1,22 @@
+/**
+ * Encode/decode in base16, base32, base32hex, and base64url.
+ *
+ * @example
+ * ```ts
+ * import * as $ from "jsr:@nyoon/lib/base";
+ * import { assertEquals } from "jsr:@std/assert";
+ *
+ * const data = crypto.getRandomValues(new Uint8Array(100));
+ * assertEquals($.de_b16($.en_b16(data)), data);
+ * assertEquals($.de_b32($.en_b32(data)), data);
+ * assertEquals($.de_h32($.en_h32(data)), data);
+ * assertEquals($.de_b64($.en_b64(data)), data);
+ * ```
+ *
+ * @see [RFC 4648](https://www.rfc-editor.org/rfc/rfc4648)
+ * @module
+ */
+
 export * from "./16.ts";
 export * from "./32.ts";
 export * from "./64.ts";
