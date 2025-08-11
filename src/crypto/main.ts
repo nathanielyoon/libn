25d0d1b2fbde2eefb6e40fa616731b091a34a4d5
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-11 15:42:44 -0400

    doc: add doc comment

--- src/crypto/main.ts
+++ src/crypto/main.ts
@@ -1,2 +1,13 @@
+/**
+ * Hash, sign, verify, exchange, and derive.
+ *
+ * @see [RFC 6234](https://www.rfc-editor.org/rfc/rfc6234)
+ * @see [RFC 2104](https://www.rfc-editor.org/rfc/rfc2104)
+ * @see [RFC 8032](https://www.rfc-editor.org/rfc/rfc8032)
+ * @see [RFC 7748](https://www.rfc-editor.org/rfc/rfc7748)
+ * @see [RFC 5869](https://www.rfc-editor.org/rfc/rfc5869)
+ * @module
+ */
+
 export * from "./25519.ts";
 export * from "./hash.ts";
