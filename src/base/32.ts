5fe2ace8b1194a56e916c92e65a809b8a7de02bb
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-11 13:44:11 -0400

    fix: simplify functions

--- src/base/32.ts
+++ src/base/32.ts
@@ -1,42 +1,36 @@
-const base32 = (alphabet: string) => {
-  const BIN = new Uint8Array(256), STR = Array<string>(32);
-  for (let z = 0, a; z < 32; ++z) {
-    STR[BIN[a = alphabet.charCodeAt(z) | 32] = BIN[a - 32] = z] = alphabet[z];
+const encode = (str: string) => ($: Uint8Array): string => {
+  let a = "";
+  for (let z = 0, b, c, d, e, f; z < $.length;) {
+    b = $[z++], c = $[z++], d = $[z++], e = $[z++], f = $[z++];
+    a += str[b >> 3] + str[b << 2 & 28 | c >> 6] + str[c >> 1 & 31] +
+      str[c << 4 & 16 | d >> 4] + str[d << 1 & 30 | e >> 7] + str[e >> 2 & 31] +
+      str[e << 3 & 24 | f >> 5] + str[f & 31];
   }
-  return {
-    encode: ($: Uint8Array): string => {
-      let a = "";
-      for (let z = 0, b, c, d, e, f; z < $.length;) {
-        b = $[z++], c = $[z++], d = $[z++], e = $[z++], f = $[z++];
-        a += STR[b >> 3] +
-          STR[b << 2 & 28 | c >> 6] +
-          STR[c >> 1 & 31] +
-          STR[c << 4 & 16 | d >> 4] +
-          STR[d << 1 & 30 | e >> 7] +
-          STR[e >> 2 & 31] +
-          STR[e << 3 & 24 | f >> 5] +
-          STR[f & 31];
-      }
-      return a.slice(0, Math.ceil($.length / 5 * 8));
-    },
-    decode: ($: string) => {
-      const A = new Uint8Array($.length * 5 >> 3), B = $.charCodeAt.bind($);
-      for (let z = 0, y = 0, a, b, c, d, e, f, g, h; z < $.length;) {
-        a = BIN[B(z++)], b = BIN[B(z++)], c = BIN[B(z++)], d = BIN[B(z++)];
-        e = BIN[B(z++)], f = BIN[B(z++)], g = BIN[B(z++)], h = BIN[B(z++)];
-        A[y++] = a << 3 | b >> 2 & 7;
-        A[y++] = b << 6 & 192 | c << 1 | d >> 4 & 1;
-        A[y++] = d << 4 & 240 | e >> 1 & 15;
-        A[y++] = e << 7 & 128 | f << 2 | g >> 3 & 7;
-        A[y++] = g << 5 & 96 | h;
-      }
-      return A;
-    },
-  };
+  return a.slice(0, Math.ceil($.length / 5 * 8));
 };
-const b32 = base32("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567");
-export const en_b32 = b32.encode;
-export const de_b32 = b32.decode;
-const h32 = base32("0123456789ABCDEFGHIJKLMNOPQRSTUV");
-export const en_h32 = h32.encode;
-export const de_h32 = h32.decode;
+const decode = (bin: Uint8Array) => ($: string): Uint8Array<ArrayBuffer> => {
+  const a = new Uint8Array($.length * 5 >> 3), b = $.charCodeAt.bind($);
+  for (let z = 0, y = 0, c, d, e, f, g, h, i, j; z < $.length;) {
+    c = bin[b(z++)], d = bin[b(z++)], e = bin[b(z++)], f = bin[b(z++)];
+    g = bin[b(z++)], h = bin[b(z++)], i = bin[b(z++)], j = bin[b(z++)];
+    a[y++] = c << 3 | d >> 2 & 7, a[y++] = d << 6 & 192 | e << 1 | f >> 4 & 1;
+    a[y++] = f << 4 & 240 | g >> 1 & 15;
+    a[y++] = g << 7 & 128 | h << 2 | i >> 3 & 7, a[y++] = i << 5 & 96 | j;
+  }
+  return a;
+};
+const binary = ($: string) => {
+  const a = new Uint8Array(256);
+  for (let z = 0, b; z < 32; ++z) a[b = $.charCodeAt(z) | 32] = a[b - 32] = z;
+  return a;
+};
+const B32 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+const H32 = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
+/** Encodes binary -> base32 string. */
+export const en_b32 = encode(B32);
+/** Decodes base32 string -> binary. */
+export const de_b32 = decode(binary(B32));
+/** Encodes binary -> base32hex string. */
+export const en_h32 = encode(H32);
+/** Decodes base32hex string -> binary. */
+export const de_h32 = decode(binary(H32));
