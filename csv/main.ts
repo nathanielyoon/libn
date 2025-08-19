6eb0b0172199b1d0712a0da44fa266df02a1ab51
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-19 12:19:17 -0400

    doc: add module doc comment

--- csv/main.ts
+++ csv/main.ts
@@ -1,3 +1,18 @@
+/**
+ * Work with CSV data.
+ *
+ * @example
+ * ```ts
+ * import * as $ from "@nyoon/lib/csv";
+ * import { assertEquals } from "jsr:@std/assert@^1.0.14";
+ *
+ * const a = "aaa,bbb,ccc\nzzz,yyy,xxx\n";
+ * assertEquals($.en_csv($.en_header($.de_header($.de_csv(a)))), a);
+ * ```
+ *
+ * @see [RFC 4180](https://www.rfc-editor.org/rfc/rfc4180)
+ */
+
 /** Parsed CSV row. */
 export type Row<A = string> = (string | A)[];
 export * from "./csv.ts";
