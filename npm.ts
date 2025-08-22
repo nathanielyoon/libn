cd008d3bca32996ab4a4f866d837909434f3e603
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-22 00:55:53 -0400

    fix: rename modules

--- npm.ts
+++ npm.ts
@@ -2,34 +2,29 @@ import { build, emptyDir } from "jsr:@deno/dnt@^0.42.3";
 
 await Deno.mkdir("./npm", { recursive: true });
 await emptyDir("./npm");
+const { exports, name, version } =
+  (await import("./deno.json", { with: { type: "json" } })).default;
 await build({
-  entryPoints: [
-    { name: "./alien-signals", path: "./alien-signals/main.ts" },
-    { name: "./base", path: "./base/main.ts" },
-    { name: "./crypto", path: "./crypto/main.ts" },
-    { name: "./csv", path: "./csv/main.ts" },
-    { name: "./fp", path: "./fp.ts" },
-    { name: "./match", path: "./match/main.ts" },
-    { name: "./query", path: "./query.ts" },
-    { name: "./schema", path: "./schema/main.ts" },
-    { name: "./vite-plugin-inline", path: "./vite-plugin-inline.ts" },
-  ],
+  entryPoints: Object.entries(exports).map(([name, path]) => ({
+    name,
+    path,
+  })),
   outDir: "./npm",
-  shims: {},
   package: {
-    name: "@nyoon/lib",
+    name,
     description: "Common modules.",
-    version:
-      (await import("./deno.json", { with: { type: "json" } })).default.version,
+    version,
     license: "GPL-3.0-or-later",
     repository: {
       type: "git",
       url: "git+https://github.com/nathanielyoon/lib.git",
     },
   },
+  shims: {},
   typeCheck: false,
   test: false,
   scriptModule: false,
+  skipNpmInstall: true,
 });
 
 await Deno.copyFile("./LICENSE", "./npm/LICENSE");
