cac89f27a29c03580f29b806cbd645df82882a5d
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-22 00:08:16 -0400

    doc: add doc comment

--- match/main.ts
+++ match/main.ts
@@ -1,3 +1,19 @@
+/**
+ * Match with weights.
+ * @module match
+ *
+ * ```ts
+ * import { myers } from "jsr:@nyoon/lib/match";
+ * import { assertLess } from "jsr:@std/assert@^1.0.14";
+ *
+ * assertLess(myers("nathan", "nathaniel"), myers("nathan", "mathaniel"));
+ * ```
+ *
+ * @see [Hungarian algorithm](https://en.wikipedia.org/wiki/Hungarian_algorithm#Implementation_in_C++)
+ * @see [Blossom algorithm](https://en.wikipedia.org/wiki/Blossom_algorithm)
+ * @see [Myers diff source](https://github.com/ka-weihe/fastest-levenshtein)
+ */
+
 export * from "./assign.ts";
 export * from "./blossom.ts";
 export * from "./fuzz.ts";
