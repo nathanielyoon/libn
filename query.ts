3735d99a840bf3bfcf66eb6dae1cf8da81d991ce
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-23 13:19:12 -0400

    fix: simplify and improve types

--- query.ts
+++ query.ts
@@ -4,84 +4,44 @@
  *
  * @example
  * ```ts
- * import { qs } from "@nyoon/lib/query";
+ * import { qa, qs } from "@nyoon/lib/query";
  * import { assertEquals } from "jsr:@std/assert@^1.0.14";
  * import { DOMParser } from "jsr:@b-fuze/deno-dom@^0.1.56";
  *
- * assertEquals(
- *   qs(
- *     "div>span.b",
- *     new DOMParser().parseFromString(
- *       '<div><span class="a">a</span><span class="b">b</span></div>',
- *       "text/html",
- *     ).body as unknown as Element,
- *   ).textContent,
- *   "b",
- * );
+ * const body = new DOMParser().parseFromString(
+ *   '<div><span class="a">a</span><span id="b" class="b">b</span></div>',
+ *   "text/html",
+ * ).body as unknown as Element;
+ * const span = qs("div>span#b.b", body); // HTMLSpanElement | null
+ * assertEquals(span?.textContent, "b");
+ * const spans = qa("div span:not([id])", body); // HTMLSpanElement[]
+ * assertEquals(spans.length, 1);
  * ```
  */
 
-type Lo<A, B extends string> = A extends `${any}${B}${infer D}` ? Lo<D, B> : A;
-type Hi<A, B extends string> = A extends `${infer C}${B}${any}` ? Hi<C, B> : A;
+type Trim<A extends string> = A extends `${any}${" " | ">"}${infer B}` ? Trim<B>
+  : A extends `${infer B}${"#" | "." | "[" | ":"}${any}` ? Trim<B>
+  : A;
 type Tag = keyof HTMLElementTagNameMap;
 type As<A extends string> = A extends Tag ? HTMLElementTagNameMap[A] : Element;
 /** Query-selects an element. */
-export const qs = ((selector: string, parent = document.body) => {
-  const element = parent.querySelector(selector);
-  if (element) return element;
-  throw Error("UNREACHABLE");
-}) as {
-  <A extends Tag>(selector: A, parent?: Element): HTMLElementTagNameMap[A];
-  <A extends string>(
-    selector: A,
-    parent?: Element,
-  ): As<Hi<Lo<A, " " | ">">, "." | "#" | "[" | ":">>;
-  <A extends Element>(selector: string, parent?: Element): A;
-};
+export const qs =
+  ((query: string, parent = document.body) => parent.querySelector(query)) as {
+    <A extends Tag>(tag: A, parent?: Element): HTMLElementTagNameMap[A] | null;
+    <A extends string>(selector: A, parent?: Element): As<Trim<A>> | null;
+    <A extends Element>(selector: string, parent?: Element): A | null;
+  };
 /** Query-selects all elements. */
-export const qa = ((selector: string, parent = document.body) => [
-  ...parent.querySelectorAll(selector),
+export const qa = ((query: string, parent = document.body) => [
+  ...parent.querySelectorAll(query),
 ]) as {
-  <A extends Tag>(selector: A, parent?: Element): HTMLElementTagNameMap[A][];
-  <A extends string>(
-    selector: A,
-    parent?: Element,
-  ): As<Hi<Lo<A, " " | ">">, "." | "#" | "[" | ":">>[];
+  <A extends Tag>(tag: A, parent?: Element): HTMLElementTagNameMap[A][];
+  <A extends string>(selector: A, parent?: Element): As<Trim<A>>[];
   <A extends Element>(selector: string, parent?: Element): A[];
 };
-/** Creates an element, optionally setting attributes. */
+/** Creates an element. */
 export const ce = <A extends Tag>(
-  tag: A,
-  parent?: Element,
-  set?: { [B in keyof HTMLElementTagNameMap[A]]?: HTMLElementTagNameMap[A][B] },
-): HTMLElementTagNameMap[A] => {
-  const a = document.createElement(tag);
-  if (set) {
-    const b = Object.keys(set) as (keyof typeof set)[];
-    for (let z = 0; z < b.length; ++z) {
-      const c = set[b[z]];
-      if (c !== undefined) set[b[z]] = c;
-    }
-  }
-  return parent?.appendChild(a) ?? a;
-};
-type On<A extends HTMLElement> =
-  & {
-    [B in keyof HTMLElementEventMap]: (
-      listener: (
-        this: A,
-        event: HTMLElementEventMap[B] & { currentTarget: A },
-      ) => any,
-    ) => On<A>;
-  }
-  & { (): A };
-/** Wraps an element to attach event listeners. */
-export const on = <A extends HTMLElement>($: A): On<A> =>
-  new Proxy($, {
-    get: (target, name: keyof HTMLElementEventMap, receiver) =>
-    (
-      listener: EventListener,
-      options?: boolean | AddEventListenerOptions,
-    ) => (target.addEventListener(name, listener, options), receiver),
-    apply: (target) => target,
-  }) as any;
+  as: A,
+  parent?: Node,
+): HTMLElementTagNameMap[A] =>
+  parent?.appendChild(document.createElement(as)) ?? document.createElement(as);
