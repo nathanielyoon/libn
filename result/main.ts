aa2b0fea7c5ef90100b5fdae9a23d397a0f48331
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-26 11:36:24 -0400

    fix: update handlers

--- result/main.ts
+++ result/main.ts
@@ -68,6 +68,29 @@ export class Or<A = any, B = any> {
 export const no = <const A = void>($?: A): No<A> => new Or(false, $!);
 /** Creates a success. */
 export const ok = <const A = void>($?: A): Ok<A> => new Or(true, $!);
+/** Runs a function if the passed-in value exists. */
+export const lift = <A, const B = void, C = NonNullable<A>>(
+  if_none?: B,
+  if_some?: ($: NonNullable<A>) => C,
+): ($: A) => Or<B, C> =>
+($) => $ == null ? no(if_none) : ok(if_some ? if_some($) : $ as C);
+/** Tries a possibly-throwing function. */
+export const try_catch =
+  <A, B, C = Error>($$: ($: A) => B, or?: ($: any) => C): ($: A) => Or<C, B> =>
+  ($) => {
+    try {
+      return ok($$($));
+    } catch (cause) {
+      return no(or ? or(cause) : Error(undefined, { cause }) as C);
+    }
+  };
+type Falsy = undefined | null | false | 0 | 0n | "";
+/** Wraps a type guard. */
+export const not =
+  <A, B extends {}>($$: ($: A) => B | Falsy): ($: A) => Or<B, A> => ($) => {
+    const a = $$($);
+    return a ? no(a) : ok($);
+  };
 /** Executes an imperative block. */
 export const run =
   <A, B, C, D>($$: ($: A) => Generator<Or<B, C>, D, C>): ($: A) => Or<B, D> =>
@@ -77,21 +100,3 @@ export const run =
       $.done ? ok($.value) : $.value.bind(($) => b(a.next($)));
     return b(a.next());
   };
-/** Tries a possibly-throwing function. */
-export const try_catch = <A, B, C = Error>(
-  tryer: ($: A) => B,
-  catcher?: ($: unknown) => C,
-): ($: A) => Or<C, B> =>
-($) => {
-  try {
-    return ok(tryer($));
-  } catch (cause) {
-    return no(catcher ? catcher(cause) : Error(undefined, { cause }) as C);
-  }
-};
-/** Runs a function if the passed-in value exists. */
-export const lift = <A, const B = void, C = NonNullable<A>>(
-  if_none?: B,
-  if_some?: ($: NonNullable<A>) => C,
-): ($: A) => Or<B, C> =>
-($) => $ == null ? no(if_none) : ok(if_some ? if_some($) : $ as C);
