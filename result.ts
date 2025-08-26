48d927eb9a7b2ecdcd5147f61b98d1e3d35c40e5
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-25 20:32:29 -0400

    fix: improve types

--- result.ts
+++ result.ts
@@ -10,10 +10,8 @@
  * const base = Math.random() * 4;
  * const { result } = ok(base)
  *   .fmap(($) => $ & 3)
- *   .assert(($) => $ % 4 === 0)
- *   .assert(Number.isInteger)
  *   .bind(($) => $ & 1 ? no("odd") : ok($))
- *   .lift(($) => $ || null, "zero");
+ *   .lift("zero", ($) => $ || null);
  * if (result.is) assert(result.value === 2);
  * else if (result.value === "zero") assert(base < 1);
  * else assert(base >= 0.25 && base < 0.5 || base >= 0.75);
@@ -23,90 +21,80 @@
 /** Result type, either a {@linkcode A | failure} or {@linkcode B | success}. */
 export class Or<A = any, B = any> {
   /** Success! */
-  constructor(is: false, as: A);
+  constructor(state: false, value: A);
   /** Failure! */
-  constructor(is: true, as: B);
+  constructor(state: true, value: B);
   /** Creates a success or failure. */
-  constructor(private is: boolean, private value: any) {}
+  constructor(private state: boolean, private value: any) {}
   protected *[Symbol.iterator](): Iterator<Or<A, B>, B> {
     return yield this;
   }
   /** Maps a successful result. */
-  fmap<C>(ok: ($: B) => C): Or<A, C> {
-    return this.is && (this.value = ok(this.value)), this as Or;
+  fmap<C>($: ($: B) => C): Or<A, C> {
+    return this.state && (this.value = $(this.value)), this as Or;
   }
   /** Maps a successful result to a new result. */
-  bind<C, D>(ok: ($: B) => Or<C, D>): Or<A | C, D> {
-    if (this.is) {
-      const { is, value } = ok(this.value);
-      this.is = is, this.value = value;
-    }
-    return this as Or;
+  bind<C, D>($: ($: B) => Or<C, D>): Or<A | C, D> {
+    return this.state ? $(this.value) : this as Or;
   }
-  /** Mutates a successful result, and fails if the new value is nullish. */
-  lift<C, const D = never>(ok: ($: B) => C, no?: D): Or<A | D, NonNullable<C>> {
-    if (this.is) {
-      this.value = ok(this.value), this.value ??= (this.is = false, no);
+  /** Tests a successful result. */
+  lift(): Or<A | null, NonNullable<B>>;
+  lift<const C>($: C): Or<A | C, NonNullable<B>>;
+  lift<const C, D extends B>($: C, test: ($: B) => $ is D): Or<A | C, D>;
+  lift<const C>($: C, test: ($: B) => any): Or<A | C, B>;
+  lift($?: unknown, test?: ($: B) => any) {
+    if (this.state && (test ? !test(this.value) : this.value == null)) {
+      this.state = false, this.value = $ ?? null;
     }
-    return this as Or;
-  }
-  /** Restricts a successful result with a type predicate. */
-  assert<C extends B = B, const D = never>(
-    is: (($: B) => $ is C) | (($: B) => any),
-    no?: D,
-  ): Or<A | D, C> {
-    if (this.is) (this.is = is(this.value)) || (this.value = no);
     return this as Or;
   }
   /** Runs an imperative block, exiting early on failure. */
   do<C, D, E>(block: ($: B) => Generator<Or<C, D>, E>): Or<A | C, E> {
-    if (!this.is) return this as Or;
+    if (!this.state) return this as Or;
     const a = block(this.value);
     const b = ($: IteratorResult<Or<C, D>, E>): Or<C, E> =>
       $.done ? ok($.value) : $.value.bind(($) => b(a.next($)));
     return b(a.next());
   }
   /** Catches a possibly-throwing function. */
-  try<C, const D = Error>(ok: ($: B) => C, no?: ($: any) => D): Or<A | D, C> {
+  try<C, D = Error>(tryer: ($: B) => C, catcher?: ($: any) => D): Or<A | D, C> {
     try {
-      return this.fmap(ok);
+      return this.fmap(tryer);
     } catch (thrown) {
-      this.is = false;
-      this.value = no?.(thrown) ?? Error(undefined, { cause: thrown });
+      this.state = false;
+      this.value = catcher?.(thrown) ?? Error(undefined, { cause: thrown });
       return this as Or;
     }
   }
   /** Checks for failure. */
   is_no(): this is No<A> {
-    return !this.is;
+    return !this.state;
   }
   /** Checks for success. */
   is_ok(): this is Ok<B> {
-    return this.is;
+    return this.state;
   }
   /** Gets the result's value, optionally throwing if the state is wrong. */
   unwrap(): A | B;
   unwrap(only_if: false): A;
   unwrap(only_if: true): B;
-  unwrap(only_if?: boolean): A | B {
-    if (only_if === !this.is) throw Error(`${this.is}`, { cause: this.value });
+  unwrap<C extends boolean>($?: C) {
+    if ($ === !this.state) throw Error(undefined, { cause: this.value });
     return this.value;
   }
   /** Gets the result as a discriminated union. */
-  get result(): { is: false; value: A } | { is: true; value: B } {
-    return { is: this.is, value: this.value };
+  get result(): { state: false; value: A } | { state: true; value: B } {
+    return { state: this.state, value: this.value };
   }
   /** Applies a function based on the result state. */
-  match<C>(no: ($: A | B) => C): Or<C, C>;
-  match<C, D>(no: ($: A) => C, ok: ($: B) => D): Or<C | D>;
-  match<C, D>(no: ($: A) => C, ok?: ($: B) => D): Or<C | D> {
-    this.value = this.is && ok ? ok(this.value) : no(this.value);
+  match<C, D = C>(if_no: ($: A) => C, if_ok?: ($: B) => D): Or<C, D> {
+    this.value = this.state && if_ok ? if_ok(this.value) : if_no(this.value);
     return this as Or;
   }
 }
-/** Failure! */
+/** Dang! */
 export type No<A = any> = Or<A, never>;
-/** Success! */
+/** Nice! */
 export type Ok<A = any> = Or<never, A>;
 /** Creates a failure. */
 export const no = <const A = void>($?: A): No<A> => new Or(false, $!);
