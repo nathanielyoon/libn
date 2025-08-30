2d1f55588d85d4e39f11d943f304c33f4d650062
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-30 18:56:54 -0400

    fix: rename tests

--- 25519/test.ts
+++ 25519/test.ts
@@ -6,14 +6,14 @@ import { generate, sign, verify } from "./src/ed25519.ts";
 import { convert_public, convert_secret, x25519 } from "./src/x25519.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
-Deno.test("x25519 : rfc7748 sections 5.2, 6.1", () =>
+Deno.test("x25519-exchange matches rfc7748 sections 5.2, 6.1", () =>
   vectors.rfc7748.forEach(($) =>
     assertEquals(
       x25519(de_b16($.secret_key), de_b16($.public_key)),
       de_b16($.shared_secret),
     )
   ));
-Deno.test("ed25519 : rfc8032 section 7.1", () =>
+Deno.test("sign/verify match rfc8032 section 7.1", () =>
   vectors.rfc8032.forEach(($) => {
     const secret_key = de_b16($.secret_key), public_key = de_b16($.public_key);
     assertEquals(generate(secret_key), public_key);
@@ -21,21 +21,21 @@ Deno.test("ed25519 : rfc8032 section 7.1", () =>
     assertEquals(signature, de_b16($.signature));
     assert(verify(public_key, data, signature));
   }));
-Deno.test("x25519 : wycheproof x25519", () =>
+Deno.test("x25519-exchange matches wycheproof", () =>
   vectors.wycheproof_x25519.forEach(($) =>
     assertEquals(
       x25519(de_b16($.secret_key), de_b16($.public_key)),
       $.shared_secret && de_b16($.shared_secret),
     )
   ));
-Deno.test("ed25519 : wycheproof ed25519", () =>
+Deno.test("verify matches wycheproof", () =>
   vectors.wycheproof_ed25519.forEach(($) =>
     assertEquals(
       verify(de_b16($.public_key), de_b16($.data), de_b16($.signature)),
       $.result,
     )
   ));
-Deno.test("verification rejects bad points", () => {
+Deno.test("verify rejects bad points", () => {
   const big = de_b16((1n | 1n << 255n).toString(16)).reverse();
   const empty = new Uint8Array(32);
   assert(!verify(empty, empty, empty));
@@ -43,12 +43,12 @@ Deno.test("verification rejects bad points", () => {
   assert(!verify(empty, empty, new Uint8Array(64).fill(-1, 32)));
   assert(!verify(empty, empty, new Uint8Array([...big, ...empty])));
 });
-Deno.test("valid signatures verify", () =>
+Deno.test("verify validates correctly-generated signatures", () =>
   fc_check(
     fc.property(fc_key, fc_bin(), (key, data) =>
       assert(verify(generate(key), data, sign(key, data)))),
   ));
-Deno.test("key exchange works on montgomery directly or when converted", () =>
+Deno.test("x25519-exchange works on montgomery or converted edwards keys", () =>
   fc_check(fc.property(fc_key, fc_key, (key_1, key_2) => {
     assertEquals(x25519(key_1, x25519(key_2)), x25519(key_2, x25519(key_1)));
     assertEquals(
