9ac0ea821d8b21f3f270f192a157e2dd76b84f7c
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-30 19:25:06 -0400

    fix: bring in

--- csv/mod.ts
+++ csv/mod.ts
@@ -1,4 +1,23 @@
-export * from "./csv.ts";
-export * from "./header.ts";
-/** Parsed CSV row. */
-export type Row<A = string> = (string | A)[];
+/** Parses CSV. */
+export const de_csv = ($: string): (string | null)[][] | null => {
+  if ($.charCodeAt(0) === 0xfeff) $ = $.slice(1);
+  const a = /(?:("?)([^\n\r",]+)\1|"((?:[^"]|"")*)"|)(,|\r?\n|(?:\r?\n)?$)/y;
+  for (let b = [], c = [], d; d = a.exec($);) {
+    if (c.push(d[2] ?? d[3]?.replaceAll('""', '"') ?? null), d[4] !== ",") {
+      if (b.push(c), c = [], a.lastIndex === $.length) return b;
+    }
+  }
+  return null;
+};
+/** Stringifies CSV. */
+export const en_csv = ($: (string | null)[][]): string => {
+  let a = "";
+  for (let b, c, d, z = 0, y; z < $.length; a += c.replace(/,?$/, "\n"), ++z) {
+    for (b = $[z], c = "", y = 0; y < b.length; ++y) {
+      if ((d = b[y]) === null) c += ",";
+      else if (/^[^\n\r",]+$/.test(d)) c += d + ",";
+      else c += `"${d.replaceAll('"', '""')}",`;
+    }
+  }
+  return a;
+};
