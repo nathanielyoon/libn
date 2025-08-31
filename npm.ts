9564d4f292752a3c3c647a4eb4cc48d89910abd8
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-08-31 01:00:55 -0400

    new: add npm task

---
+++ npm.ts
@@ -0,0 +1,23 @@
+import { build, emptyDir } from "@deno/dnt";
+
+const { name, version } = JSON.parse(await Deno.readTextFile("./deno.json"));
+await Deno.mkdir("./npm", { recursive: true });
+await emptyDir("./npm");
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name,
+    version,
+    license: "GPL-3.0-or-later",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/nathanielyoon/lib.git",
+    },
+  },
+  shims: {},
+  typeCheck: false,
+  test: false,
+  scriptModule: false,
+  skipNpmInstall: true,
+});
