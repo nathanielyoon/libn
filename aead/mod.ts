a7ca9df348bc1e5bbd29fc0ae1d47e6b1d1b9623
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-01 11:25:46 -0400

    doc: add module doc comment

--- aead/mod.ts
+++ aead/mod.ts
@@ -1,3 +1,36 @@
+/**
+ * XChaCha20-Poly1305 ([RFC 8439](https://www.rfc-editor.org/rfc/rfc8439) and
+ * [draft-irtf-cfrg-xchacha-03](https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-xchacha-03)).
+ *
+ * @example Authenticated encryption
+ * ```ts
+ * import { assert, assertEquals } from "@std/assert";
+ *
+ * const key = crypto.getRandomValues(new Uint8Array(32));
+ * const message = crypto.getRandomValues(new Uint8Array(100));
+ *
+ * const secret = encrypt(key, message);
+ * assert(secret);
+ * assertEquals(decrypt(key, secret), message);
+ * ```
+ *
+ * @example Authenticated encryption with associated data
+ * ```ts
+ * import { assert, assertEquals } from "@std/assert";
+ *
+ * const key = crypto.getRandomValues(new Uint8Array(32));
+ * const message = crypto.getRandomValues(new Uint8Array(100));
+ * const data = crypto.getRandomValues(new Uint8Array(100));
+ *
+ * const secret = encrypt(key, message, data);
+ * assert(secret);
+ * assertEquals(decrypt(key, secret, data), message);
+ * assertEquals(decrypt(key, secret), null);
+ * ```
+ *
+ * @module aead
+ */
+
 import { polyxchacha, xchachapoly } from "./src/aead.ts";
 
 /** Encrypts with XChaCha20-Poly1305. */
