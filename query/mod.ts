71f4b039db73c3058c5b3ca6d9f57e55dfd8e7e5
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-01 14:16:25 -0400

    doc: add module doc comment

--- query/mod.ts
+++ query/mod.ts
@@ -1,3 +1,24 @@
+/**
+ * Short [querySelector](https://dev.mozilla.org/Web/API/Element/querySelector),
+ * [querySelectorAll](https://dev.mozilla.org/Web/API/Element/querySelectorAll),
+ * and [createElement](https://dev.mozilla.org/Web/API/Document/createElement).
+ *
+ * @example Query document
+ * ```ts
+ * import { DOMParser } from "@b-fuze/deno-dom/native";
+ * import { assertEquals } from "@std/assert";
+ *
+ * globalThis.document = new DOMParser().parseFromString(
+ *   '<a id="c"><b class="e">0</b><b>1</b></a><a id="d"><b>2</b></a>',
+ *   "text/html",
+ * ) as any;
+ * assertEquals(qs("a#d")?.textContent, "2");
+ * assertEquals(qa("b:not([class])").length, 2);
+ * ```
+ *
+ * @module query
+ */
+
 /** Element tag name. */
 export type Tag = keyof HTMLElementTagNameMap;
 type Trim<A extends string> = A extends `${any}${" " | ">"}${infer B}` ? Trim<B>
