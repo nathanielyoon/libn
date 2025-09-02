bcf2f2efe8a68ae1814384f6eb918ad9467c7df6
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-02 12:00:18 -0400

    new: add json type

--- json/src/types.ts
+++ json/src/types.ts
@@ -1,3 +1,5 @@
+/** Non-null JSON types. */
+export type Json = boolean | number | string | Json[] | { [_: string]: Json };
 /** Content encoding types. */
 export type Base = `base${16 | 32 | "32hex" | 64 | "64url"}`;
 /** String formats (asserted). */
@@ -53,14 +55,15 @@ export type Data<A extends Type> = A extends { enum: readonly (infer B)[] } ? B
   : A extends { format: infer B extends keyof Formats } ? Formats[B]
   : A["type"] extends "string" ? string
   : A extends { items: infer B extends Type } ? readonly Data<B>[]
-  : A["type"] extends "array" ? readonly unknown[]
-  : A extends { properties: infer B extends { [key: string]: Type } }
-    ? A extends { required: readonly (infer C extends string)[] } ? (
-        & { [D in Extract<keyof B, C>]: Data<B[D]> }
-        & { [D in Exclude<keyof B, C>]?: Data<B[D]> }
-      ) extends infer E ? { [F in keyof E]: E[F] } : never
-    : { [C in keyof B]?: Data<B[C]> }
-  : A["type"] extends "object" ? { [key: string]: unknown }
+  : A["type"] extends "array" ? readonly Json[]
+  : A extends { properties: infer B extends { [key: string]: Type } } ?
+      & (A extends { required: readonly (infer C extends string)[] } ? (
+          & { [D in Extract<keyof B, C>]: Data<B[D]> }
+          & { [D in Exclude<keyof B, C>]?: Data<B[D]> }
+        ) extends infer E ? { [F in keyof E]: E[F] } : never
+        : { [C in keyof B]?: Data<B[C]> })
+      & (A extends { additionalProperties: false } ? {} : { [_: string]: Json })
+  : A["type"] extends "object" ? { [_: string]: Json }
   : never;
 type Esc<A extends PropertyKey, B extends number, C extends string> = C extends
   `${infer D}${A & string}${infer E}` ? `${D}~${B}${Esc<A, B, E>}` : C;
