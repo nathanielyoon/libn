64c7406005270f3d61d9d5223ab8a6d0f85f92db
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-02 18:05:45 -0400

    fix: rename variables

--- signal/mod.ts
+++ signal/mod.ts
@@ -15,21 +15,15 @@ const enum Kind {
 }
 type Noder<A extends Kind> =
   & { kind: A; flags: Flag }
-  & { [_ in "head" | "deps" | "subs" | "tail"]: Link | null };
-type Source<A = any> = Noder<Kind.SOURCE> & { old: A; has: A };
-type Derive<A = any> = Noder<Kind.DERIVE> & { has?: A; get: (old?: A) => A };
-type Effect = Noder<Kind.EFFECT> & { run: () => void };
+  & { [_ in "head" | "dep" | "sub" | "tail"]: Link | null };
+type Source<A = any> = Noder<Kind.SOURCE> & { old: A; is: A };
+type Derive<A = any> = Noder<Kind.DERIVE> & { old?: A; is: (old?: A) => A };
+type Effect = Noder<Kind.EFFECT> & { is: () => void };
 type Node = Source | Derive | Effect;
-type Link = {
-  on: number;
-  dep_prev: Link | null;
-  dep: Node;
-  dep_next: Link | null;
-  sub_prev: Link | null;
-  sub: Node;
-  sub_next: Link | null;
-};
-const queue: Effect[] = [];
+type Link =
+  & { on: number; dep: Node; sub: Node }
+  & { [_ in `${"dep" | "sub"}_${"prev" | "next"}`]: Link | null };
+const queue: Node[] = [];
 let on = 0, at = 0, sub: Node | undefined;
 /** Manually sets the current subscriber (exported for testing). */
 export const set = ($?: Node): Node | undefined => ([sub, $] = [$, sub])[1];
@@ -37,7 +31,7 @@ const enlink = ($: Node) => {
   if (!sub) return;
   const a = sub.head;
   if (a?.dep === $) return;
-  const b = a ? a.dep_next : sub.deps;
+  const b = a ? a.dep_next : sub.dep;
   if (b?.dep === $) return b.on = on, sub.head = b;
   const c = $.tail;
   if (c?.on === on && c.sub === sub) return;
@@ -51,15 +45,15 @@ const enlink = ($: Node) => {
     sub_next: null,
   };
   if (b) b.dep_prev = d;
-  a ? a.dep_next = d : sub.deps = d, c ? c.sub_next = d : $.subs = d;
+  a ? a.dep_next = d : sub.dep = d, c ? c.sub_next = d : $.sub = d;
 };
 const delink = ($: Node, link: Link) => {
   const a = link.dep_prev, b = link.dep, c = link.dep_next, d = link.sub_prev;
-  c ? c.dep_prev = a : $.head = a, a ? a.dep_next = c : $.deps = c;
+  c ? c.dep_prev = a : $.head = a, a ? a.dep_next = c : $.dep = c;
   let e = link.sub_next;
   if (e ? e.sub_prev = d : b.tail = d) d!.sub_next = e;
-  else if (!(b.subs = e) && b.deps) {
-    for (b.flags = Flag.CLEAR, e = b.deps; e = delink(b, e););
+  else if (!(b.sub = e) && b.dep) {
+    for (b.flags = Flag.CLEAR, e = b.dep; e = delink(b, e););
   }
   return c;
 };
@@ -67,22 +61,22 @@ const track = ($: Node) => {
   ++on, $.head = null, $.flags = $.flags & ~56 | Flag.CHECK; // 56 = RECUR | DIRTY | READY
 };
 const close = ($: Node) => {
-  for (let a = $.head ? $.head.dep_next : $.deps; a; a = delink($, a));
+  for (let a = $.head ? $.head.dep_next : $.dep; a; a = delink($, a));
   $.flags &= ~Flag.CHECK;
 };
 const reset = ($: Node) =>
-  $.kind === Kind.SOURCE && ($.flags = Flag.MAYBE, $.old !== ($.old = $.has));
+  $.kind === Kind.SOURCE && ($.flags = Flag.MAYBE, $.old !== ($.old = $.is));
 const reget = ($: Node) => {
   if ($.kind !== Kind.DERIVE) return false;
   const a = set($);
   try {
-    return track($), $.has !== ($.has = $.get($.has));
+    return track($), $.old !== ($.old = $.is($.old));
   } finally {
     set(a), close($);
   }
 };
-const rerun = ($: Node): Node | number => ($.flags & Flag.QUEUE ||
-  ($.flags |= Flag.QUEUE, $.subs ? rerun($.subs.sub) : queue.push($ as any)));
+const rerun = ($: Node): number => ($.flags & Flag.QUEUE ||
+  ($.flags |= Flag.QUEUE, $.sub ? rerun($.sub.sub) : queue.push($)));
 const ok = (link: Link, sub: Node) => {
   for (let a = sub.head; a; a = a.dep_prev) if (a === link) return true;
 };
@@ -94,7 +88,7 @@ const deep = ($: Link) => {
     else if (d & 48 || !ok($, c)) d = 0; // 48 = DIRTY | READY
     else c.flags |= 40, d &= Flag.MAYBE; // 40 = RECUR | READY
     if (d & Flag.WATCH && rerun(c), d & Flag.MAYBE) {
-      c.subs && ($ = c.subs).sub_next && (a.push(b), b = $.sub_next);
+      c.sub && ($ = c.sub).sub_next && (a.push(b), b = $.sub_next);
     } else if (!b) {
       while (a.length) {
         if ($ = a.pop()!) {
@@ -106,24 +100,26 @@ const deep = ($: Link) => {
     } else $ = b, b = $.sub_next;
   }
 };
-const flat = ($: Link) => {
-  do ($.sub.flags & 48) === Flag.READY && // 48 = READY | DIRTY
+const flat = ($: Link | null) => {
+  while ($) { // 48 = READY | DIRTY
+    ($.sub.flags & 48) === Flag.READY &&
     ($.sub.flags |= Flag.DIRTY) & Flag.WATCH &&
-    rerun($.sub); while ($ = $.sub_next!);
+    rerun($.sub), $ = $?.sub_next!;
+  }
 };
 const check = ($: Node, link: Link) => {
   top: for (let a: Link[] = [], b = 0, c, d;;) {
     if (c = false, d = link.dep, $.flags & Flag.DIRTY) c = true;
     else if ((d.flags & Flag.CLEAR) === Flag.CLEAR) {
-      if (reset(d) || reget(d)) c = true, d.subs!.sub_next && flat(d.subs!);
+      if (reset(d) || reget(d)) c = true, d.sub!.sub_next && flat(d.sub!);
     } else if ((d.flags & 33) === 33) { // 33 = MAYBE | READY
-      if (link.sub_next || link.sub_prev) a.push(link);
-      ++b, $ = d, link = d.deps!;
+      if (link.sub_prev || link.sub_next) a.push(link);
+      ++b, $ = d, link = d.dep!;
       continue;
     }
     if (c || !link.dep_next) {
       while (b--) {
-        link = (d = $.subs!).sub_next ? a.pop()! : d;
+        link = (d = $.sub!).sub_next ? a.pop()! : d;
         if (!c) $.flags &= ~Flag.READY;
         else if (reset($) || reget($)) d.sub_next && flat(d), $ = link.sub;
         else if ($ = link.sub, link.dep_next) {
@@ -136,91 +132,65 @@ const check = ($: Node, link: Link) => {
   }
 };
 const run = ($: Node, flags: Flag) => {
-  if (flags & Flag.DIRTY || flags & Flag.READY && check($, $.deps!)) {
+  if (flags & Flag.DIRTY || flags & Flag.READY && check($, $.dep!)) {
     const a = set($);
     try {
-      track($), ($ as Effect).run();
+      track($), ($ as Effect).is();
     } finally {
       set(a), close($);
     }
     return;
   } else if (flags & Flag.READY) $.flags = flags & ~Flag.READY;
-  for (let a = $.deps; a; a = a.dep_next) {
+  for (let a = $.dep; a; a = a.dep_next) {
     a.dep.flags & Flag.QUEUE && run(a.dep, a.dep.flags &= ~Flag.QUEUE);
   }
 };
 function source(this: Source, ...$: [unknown]) {
   if (!$.length) {
-    this.flags & Flag.DIRTY && reset(this) && this.subs && flat(this.subs);
-    enlink(this);
+    this.flags & Flag.DIRTY && reset(this) && flat(this.sub), enlink(this);
   } else if (
-    this.has !==
-      (this.has = typeof $[0] === "function" ? $[0](this.has) : $[0]) &&
-    (this.flags = Flag.CLEAR, this.subs)
+    this.is !== (this.is = typeof $[0] === "function" ? $[0](this.is) : $[0]) &&
+    (this.flags = Flag.CLEAR, this.sub)
   ) {
-    deep(this.subs);
+    deep(this.sub);
     while (at < queue.length) run(queue[at], queue[at++].flags &= ~Flag.QUEUE);
     queue.length = at = 0;
   }
-  return this.has;
+  return this.is;
 }
 function derive(this: Derive) {
-  if (
-    this.flags & Flag.DIRTY ||
-    this.flags & Flag.READY && check(this, this.deps!)
-  ) reget(this) && this.subs && flat(this.subs);
-  else if (this.flags & Flag.READY) this.flags &= ~Flag.READY;
-  return enlink(this), this.has;
+  this.flags & Flag.DIRTY || this.flags & Flag.READY && check(this, this.dep!)
+    ? reget(this) && flat(this.sub)
+    : (this.flags &= ~Flag.READY);
+  return enlink(this), this.old;
 }
 function dispose(this: Effect) {
-  for (let a = this.deps; a; a = delink(this, a));
-  this.subs && delink(this.subs.sub, this.subs), this.flags &= 0;
+  for (let a = this.dep; a; a = delink(this, a));
+  this.sub && delink(this.sub.sub, this.sub), this.flags &= 0;
 }
+const node = <A extends Kind, B>(kind: A, flags: Flag, rest: B) => (
+  { kind, flags, head: null, dep: null, sub: null, tail: null, ...rest }
+);
+/** Reactive value. */
+export type Signal<A> = { (): A; <const C extends A>($: C | (($: C) => C)): C };
 /** Creates a reactive value. */
 export const signal = (
-  (initial: any, maybe_initial?: any) =>
-    typeof initial !== "function"
-      ? source.bind({
-        kind: Kind.SOURCE,
-        flags: Flag.MAYBE,
-        head: null,
-        deps: null,
-        subs: null,
-        tail: null,
-        old: initial,
-        has: initial,
-      })
-      : derive.bind({
-        kind: Kind.DERIVE,
-        flags: Flag.CLEAR,
-        head: null,
-        deps: null,
-        subs: null,
-        tail: null,
-        get: initial, // <https://github.com/microsoft/TypeScript/issues/47599>
-        has: maybe_initial,
-      })
+  ($: any, initial?: any) =>
+    typeof $ !== "function"
+      ? source.bind(node(Kind.SOURCE, Flag.MAYBE, { old: $, is: $ }))
+      : derive.bind(node(Kind.DERIVE, Flag.CLEAR, { old: initial, is: $ }))
 ) as {
-  <A>(deriver: (old?: A) => A): () => A;
+  <A>(deriver: (old?: A) => A): () => A; // <https://github.com/microsoft/TypeScript/issues/47599>
   <A>(deriver: (old: A) => A, initial: A): () => A;
-  <A>(): { (): A | undefined; <const B extends A>($: B | (($: B) => B)): B };
-  <A>(initial: A): { (): A; <const B extends A>($: B | (($: B) => B)): B };
+  <A>(initial: A): Signal<A>;
 };
 /** Creates a side effect and returns a disposer. */
-export const effect = (run: () => void): () => void => {
-  const a: Effect = {
-    kind: Kind.EFFECT,
-    flags: Flag.WATCH,
-    head: null,
-    deps: null,
-    subs: null,
-    tail: null,
-    run,
-  };
+export const effect = (is: () => void): () => void => {
+  const a = node(Kind.EFFECT, Flag.WATCH, { is });
   enlink(a);
   const b = set(a);
   try {
-    run();
+    is();
   } finally {
     set(b);
   }
