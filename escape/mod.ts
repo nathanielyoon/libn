7035e6346e245f9e48ad173b909cda39e5424ea2
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-03 10:28:54 -0400

    fix: simplify usage

--- escape/mod.ts
+++ escape/mod.ts
@@ -1,19 +1,21 @@
+const IN = /["&<>]/g;
 const OUT = Array<string>(63).fill("");
 OUT[34] = "&quot;", OUT[38] = "&amp;", OUT[60] = "&lt;", OUT[62] = "&gt;";
 /** Escapes a string. */
 export const escape = ($: string): string => {
-  const a = /["&<>]/g;
+  if (!IN.test($)) return $;
   let b = "", c = 0;
-  while (a.test($)) b += $.slice(c, c = ~-a.lastIndex) + OUT[$.charCodeAt(c++)];
+  do b += $.slice(c, c = IN.lastIndex - 1) +
+    OUT[$.charCodeAt(c++)]; while (IN.test($));
   return b + $.slice(c);
 };
 /** Escapes a HTML template. */
-export const html = ({ raw }: TemplateStringsArray, ...$: any[]): string => {
+export const html = ($: { raw: readonly string[] }, ...use: any[]): string => {
   let a = "";
-  for (let b, c, d, z = 0; z < $.length; ++z) {
-    b = raw[z], c = $[z] ?? "";
+  for (let b, c, d, z = 0; z < use.length; ++z) {
+    b = $.raw[z], c = use[z] ?? "";
     d = typeof c === "string" ? c : Array.isArray(c) ? c.join("") : `${c}`;
-    a += b.charCodeAt(b.length - 1) === 36 ? b.slice(1) + d : b + escape(d);
+    a += b.charCodeAt(b.length - 1) === 36 ? b.slice(0, -1) + d : b + escape(d);
   }
-  return a + raw[$.length];
+  return a + $.raw[use.length];
 };
