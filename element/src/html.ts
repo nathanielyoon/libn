84ba278bad59fe29d90d311be273b2ec93f13b17
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-03 15:29:27 -0400

    new: proxy-ify helper

--- element/src/html.ts
+++ element/src/html.ts
@@ -1,30 +1,36 @@
 type Not<A, B> = (<C>() => C extends A ? true : false) extends
   (<C>() => C extends Readonly<A> ? true : false) ? never : B;
-type Attributes<A> = {
-  [B in keyof A as Not<Pick<A, B>, B>]?: [B, NonNullable<A[B]>] extends
-    [`on${string}`, (event: infer C extends Event) => any]
-    ? ($: C & { currentTarget: A }) => any
-    : A[B];
-};
 type Child = undefined | null | string | Node;
-const get: ProxyHandler<($: () => void) => () => void>["get"] =
-  (effect, tag: string) => ($?: {}, children?: Child[]) => {
-    const a = document.createElement(tag);
-    for (const [key, value] of Object.entries($ ?? {})) {
+type Html<A extends HTMLElement> =
+  & {
+    [B in keyof A as Not<Pick<A, B>, B>]: (
+      $: [B, NonNullable<A[B]>] extends
+        [`on${string}`, (event: infer C extends Event) => any]
+        ? ($: C & { currentTarget: A }) => any
+        : A[B],
+    ) => Html<A>;
+  }
+  & { (children?: Child[]): A };
+const get = (effect: ($: () => void) => () => void, tag: string) =>
+  new Proxy<any>(document.createElement(tag), {
+    get: (target, key: string, proxy) => (value: any) => {
       if (value !== undefined) {
-        const b = typeof value !== "function" || key.startsWith("on"); // @ts-expect-error
-        b ? a[key] = value : effect(() => a[key] = value);
+        typeof value !== "function" || key.startsWith("on")
+          ? target[key] = value
+          : effect(() => target[key] = value);
       }
-    }
-    for (const node of children ?? []) {
-      node && a.appendChild(typeof node === "string" ? new Text(node) : node);
-    }
-    return a;
-  };
+      return proxy;
+    },
+    apply: (target, _, [children]: [Child[]?]) => {
+      for (const node of children ?? []) {
+        node && target.appendChild(
+          typeof node === "string" ? new Text(node) : node,
+        );
+      }
+      return target;
+    },
+  });
 /** Creates an HTML builder. */
-export const htmler = (effect: ($: () => void) => () => void): {
-  [A in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[A] extends infer B
-    ? B extends HTMLElement ? ($?: Attributes<B>, children?: Child[]) => B
-    : never
-    : never;
+export const runtime = (effect: ($: () => void) => () => void): {
+  [A in keyof HTMLElementTagNameMap]: Html<HTMLElementTagNameMap[A]>;
 } => new Proxy<any>(effect, { get });
