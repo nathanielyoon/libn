3adf21b1d17376c529eb14b7c6117aa163124e6a
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-06 16:08:14 -0400

    new: update tests

--- url/test.ts
+++ url/test.ts
@@ -1,24 +1,19 @@
 import { assertEquals, assertMatch } from "@std/assert";
-import { presign } from "./mod.ts";
+import { presign, signer } from "./mod.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
 Deno.test("match signature v4 documentation example", () => {
   const S3 = {
-    S3_HOST: `https://${vectors.signature_v4.bucket}.s3.amazonaws.com`,
-    S3_ID: vectors.signature_v4.id,
-    S3_KEY: vectors.signature_v4.key,
+    S3_HOST: `https://${vectors.docs.bucket}.s3.amazonaws.com`,
+    S3_ID: vectors.docs.id,
+    S3_KEY: vectors.docs.key,
   };
+  const a = ["GET", vectors.docs.path, {}, +vectors.docs.time] as const;
+  const b = vectors.docs.url.replaceAll("&amp;", "&");
+  assertEquals(presign(S3, ...a), b);
   assertEquals(
-    presign(
-      S3,
-      "GET",
-      vectors.signature_v4.path,
-      {},
-      +vectors.signature_v4.time,
-      vectors.signature_v4.region,
-      new Date(vectors.signature_v4.date),
-    ),
-    vectors.signature_v4.url.replaceAll("&amp;", "&"),
+    signer(S3, vectors.docs.region, new Date(vectors.docs.date))(...a),
+    b,
   );
   assertMatch(
     presign(S3, "PUT", "", { "content-type": "text/plain" }),
--- url/write.ts
+++ url/write.ts
@@ -1,9 +1,7 @@
-// https://web.archive.org/web/20140302054859/https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example
-
 import { write_vectors } from "../test.ts";
 
 await write_vectors(import.meta, {
-  signature_v4: await fetch(
+  docs: await fetch(
     "https://web.archive.org/web/20140302054859/https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example",
   ).then(($) => $.text()).then(($) =>
     /<code class="code">(?<path>.+?)<\/code>.*?<code class="code">(?<bucket>.+?)<\/code>.*?\((?<time>\d+).*?<code class="code">(?<date>.+?)<\/code>.*?<code.*?<code.*?<code class="code">(?<region>.+?)<\/code>.*?<code class="code">(?<id>.+?)<\/code>.*?<code class="code">(?<key>.+?)<\/code>.*?:<\/p><pre class="programlisting">(?<url>.+?)<\/pre>/s
