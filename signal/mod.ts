a1f9f1eb26419e8f3586b5b55f00b0291cb2f095
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-06 13:20:19 -0400

    doc: re-add module doc comment

--- signal/mod.ts
+++ signal/mod.ts
@@ -1,3 +1,23 @@
+/**
+ * Reactive state management.
+ *
+ * @example Push-pull updates
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ *
+ * const count = signal(0), half = signal(() => count() >> 1);
+ * let called = 0;
+ * effect(() => half() && ++called);
+ *
+ * assertEquals(called, 0);
+ * count(1), assertEquals(called, 0);
+ * count(2), assertEquals(called, 1);
+ * count(3), assertEquals(called, 1);
+ * ```
+ *
+ * @module signal
+ */
+
 const enum Flag {
   CLEAR = 0,
   BEGIN = 1 << 0, // is mutable
