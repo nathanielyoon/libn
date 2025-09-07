6575bf6150f732142f311a949764311e3ca1f1fe
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-07 10:23:12 -0400

    fix: unroll loop

--- aead/src/aead.ts
+++ aead/src/aead.ts
@@ -2,20 +2,32 @@ import { chacha, hchacha } from "./chacha.ts";
 import { poly } from "./poly.ts";
 
 const xor = (key: DataView, iv: DataView, $: Uint8Array, to: Uint8Array) => {
-  const a = iv.getUint32(16, true), b = iv.getUint32(20, true);
-  const c = $.length & ~63, d = new Uint32Array(16);
-  let e = new DataView(to.buffer, to.byteOffset), z = 0, y = 1, x;
-  while (z < c) {
-    chacha(key, y++, x = 0, a, b, d);
-    do e.setUint32(
-      z,
-      ($[z++] | $[z++] << 8 | $[z++] << 16 | $[z++] << 24) ^ d[x],
-      true,
-    ); while (++x < 16);
+  const a = $.length & ~63, b = new DataView($.buffer, $.byteOffset);
+  const c = iv.getUint32(16, true), d = iv.getUint32(20, true);
+  const e = new Uint32Array(16);
+  let f = new DataView(to.buffer, to.byteOffset), z = 0, y = 1;
+  while (z < a) {
+    chacha(key, y++, 0, c, d, e);
+    f.setUint32(z, b.getUint32(z, true) ^ e[0], true);
+    f.setUint32(z + 4, b.getUint32(z + 4, true) ^ e[1], true);
+    f.setUint32(z + 8, b.getUint32(z + 8, true) ^ e[2], true);
+    f.setUint32(z + 12, b.getUint32(z + 12, true) ^ e[3], true);
+    f.setUint32(z + 16, b.getUint32(z + 16, true) ^ e[4], true);
+    f.setUint32(z + 20, b.getUint32(z + 20, true) ^ e[5], true);
+    f.setUint32(z + 24, b.getUint32(z + 24, true) ^ e[6], true);
+    f.setUint32(z + 28, b.getUint32(z + 28, true) ^ e[7], true);
+    f.setUint32(z + 32, b.getUint32(z + 32, true) ^ e[8], true);
+    f.setUint32(z + 36, b.getUint32(z + 36, true) ^ e[9], true);
+    f.setUint32(z + 40, b.getUint32(z + 40, true) ^ e[10], true);
+    f.setUint32(z + 44, b.getUint32(z + 44, true) ^ e[11], true);
+    f.setUint32(z + 48, b.getUint32(z + 48, true) ^ e[12], true);
+    f.setUint32(z + 52, b.getUint32(z + 52, true) ^ e[13], true);
+    f.setUint32(z + 56, b.getUint32(z + 56, true) ^ e[14], true);
+    f.setUint32(z + 60, b.getUint32(z + 60, true) ^ e[15], true), z += 64;
   }
-  if (c < $.length) {
-    chacha(key, y, x = 0, a, b, d), e = new DataView(d.buffer);
-    do to[z] = $[z] ^ e.getUint8(x++); while (++z < $.length);
+  if (a < $.length) {
+    chacha(key, y, y = 0, c, d, e), f = new DataView(e.buffer);
+    do to[z] = $[z] ^ f.getUint8(y++); while (++z < $.length);
   }
 };
 const tag = (key: Uint32Array, $: Uint8Array, data: Uint8Array) => {
