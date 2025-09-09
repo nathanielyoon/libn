5570499cbeb38e36a4af61287091f8b2b1811579
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-09 12:01:05 -0400

    fix: improve types and names

--- element/src/escape.ts
+++ element/src/escape.ts
@@ -3,8 +3,8 @@ OUT[34] = "&quot;", OUT[38] = "&amp;", OUT[60] = "&lt;", OUT[62] = "&gt;";
 /** Escapes a string. */
 export const escape = ($: string): string => {
   if (!IN.test($)) return $;
-  let b = "", c = 0;
-  do b += $.slice(c, c = IN.lastIndex - 1) +
-    OUT[$.charCodeAt(c++)]; while (IN.test($));
-  return b + $.slice(c);
+  let escaped = "", z = 0;
+  do escaped += $.slice(z, z = IN.lastIndex - 1) +
+    OUT[$.charCodeAt(z++)]; while (IN.test($));
+  return escaped + $.slice(z);
 };
