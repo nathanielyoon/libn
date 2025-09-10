180c156f8bde52fbbb502b2af35b972027415ded
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-10 16:54:28 -0400

    fix: rewrite internals

--- hash/src/common.ts
+++ hash/src/common.ts
@@ -1,7 +1,9 @@
+export const enum Max {
+  U = 0x100000000,
+}
 /** Finds the minimum of two 32-bit integers. */
 export const min = (a: number, b: number): number => b + (a - b & a - b >> 31);
-/** Parses a base16-encoded initialization vector into 32-bit words. */
-export const iv = ($: string): Uint32Array<ArrayBuffer> =>
+const iv = ($: string) =>
   Uint32Array.from($.match(/.{8}/g)!, ($) => parseInt($, 16));
 /** SHA-512 IV. */
 export const SHA512 = /* @__PURE__ */ iv(
@@ -9,3 +11,34 @@ export const SHA512 = /* @__PURE__ */ iv(
 );
 /** SHA-256 IV. */
 export const SHA256 = /* @__PURE__ */ SHA512.filter((_, z) => z & 1 ^ 1);
+/** SHA-512 round constants (upper bits). */
+export const HI = /* @__PURE__ */ iv(
+  "d728ae2223ef65cdec4d3b2f8189dbbcf348b538b605d019af194f9bda6d8118a303024245706fbe4ee4b28cd5ffb4e2f27b896f3b1696b125c71235cf6926949ef14ad2384f25e38b8cd5b577ac9c65592b02756ea6e483bd41fbd4831153b5ee66dfab2db4321098fb213fbeef0ee43da88fc2930aa725e003826f0a0e6e7046d22ffc5c26c9265ac42aed9d95b3df8baf63de3c77b2a847edaee61482353b4cf10364bc423001d0f897910654be30d6ef52185565a9105771202a32bbd1b8b8d2d0c85141ab53df8eeb99e19b48a8c5c95a63e3418acb7763e373d6b2b8a35defb2fc43172f60a1f0ab721a6439ec23631e28de82bde9b2c67915e372532bea26619c21c0c207cde0eb1eee6ed17872176fbaa2c898a6bef90dae131c471b23047d8440c7249315c9bebc9c100d4ccb3e42b6fc657e2a3ad6faec4a475817",
+);
+/** SHA-512 round constants (lower bits). SHA-256 also uses the first 64. */
+export const LO = /* @__PURE__ */ iv(
+  "428a2f9871374491b5c0fbcfe9b5dba53956c25b59f111f1923f82a4ab1c5ed5d807aa9812835b01243185be550c7dc372be5d7480deb1fe9bdc06a7c19bf174e49b69c1efbe47860fc19dc6240ca1cc2de92c6f4a7484aa5cb0a9dc76f988da983e5152a831c66db00327c8bf597fc7c6e00bf3d5a7914706ca63511429296727b70a852e1b21384d2c6dfc53380d13650a7354766a0abb81c2c92e92722c85a2bfe8a1a81a664bc24b8b70c76c51a3d192e819d6990624f40e3585106aa07019a4c1161e376c082748774c34b0bcb5391c0cb34ed8aa4a5b9cca4f682e6ff3748f82ee78a5636f84c878148cc7020890befffaa4506cebbef9a3f7c67178f2ca273eced186b8c7eada7dd6f57d4f7f06f067aa0a637dc5113f98041b710b3528db77f532caab7b3c9ebe0a431d67c44cc5d4be597f299c5fcb6fab6c44198c",
+);
+/** Merkle-Damgard hash construction. */
+export const md = (
+  base: Uint32Array,
+  block: number,
+  out: number,
+  pad: number,
+  mix: (use: Uint32Array, data: DataView, at: number, to: Uint32Array) => void,
+  $: Uint8Array,
+): Uint8Array<ArrayBuffer> => {
+  const length = $.length, data = new Uint8Array(block);
+  const use = new Uint32Array(block * 10 >> 3), state = new Uint32Array(base);
+  let view = new DataView($.buffer, $.byteOffset), z = 0, y = 0;
+  while (z < length) {
+    const size = min(block - y, length - z);
+    if (size !== block) data.set($.subarray(z, z += size)), y += size;
+    else do mix(use, view, z, state), z += block; while (length - z >= block);
+  }
+  view = new DataView(data.buffer), data[y] = 128;
+  block - ++y < pad && mix(use, view, y = 0, state), data.fill(0, y), y = out;
+  view.setBigUint64(block - 8, BigInt(length) << 3n), mix(use, view, 0, state);
+  do view.setUint32(y -= 4, state[y >> 2]); while (y);
+  return new Uint8Array(data.subarray(0, out));
+};
--- hash/src/sha2.ts
+++ hash/src/sha2.ts
@@ -1,104 +1,85 @@
-import { iv, min, SHA256, SHA512 } from "./common.ts";
+import { HI, LO, Max, md, SHA256, SHA512 } from "./common.ts";
 
-const HI = /* @__PURE__ */ iv(
-  "428a2f9871374491b5c0fbcfe9b5dba53956c25b59f111f1923f82a4ab1c5ed5d807aa9812835b01243185be550c7dc372be5d7480deb1fe9bdc06a7c19bf174e49b69c1efbe47860fc19dc6240ca1cc2de92c6f4a7484aa5cb0a9dc76f988da983e5152a831c66db00327c8bf597fc7c6e00bf3d5a7914706ca63511429296727b70a852e1b21384d2c6dfc53380d13650a7354766a0abb81c2c92e92722c85a2bfe8a1a81a664bc24b8b70c76c51a3d192e819d6990624f40e3585106aa07019a4c1161e376c082748774c34b0bcb5391c0cb34ed8aa4a5b9cca4f682e6ff3748f82ee78a5636f84c878148cc7020890befffaa4506cebbef9a3f7c67178f2ca273eced186b8c7eada7dd6f57d4f7f06f067aa0a637dc5113f98041b710b3528db77f532caab7b3c9ebe0a431d67c44cc5d4be597f299c5fcb6fab6c44198c",
-);
-const LO = /* @__PURE__ */ iv(
-  "d728ae2223ef65cdec4d3b2f8189dbbcf348b538b605d019af194f9bda6d8118a303024245706fbe4ee4b28cd5ffb4e2f27b896f3b1696b125c71235cf6926949ef14ad2384f25e38b8cd5b577ac9c65592b02756ea6e483bd41fbd4831153b5ee66dfab2db4321098fb213fbeef0ee43da88fc2930aa725e003826f0a0e6e7046d22ffc5c26c9265ac42aed9d95b3df8baf63de3c77b2a847edaee61482353b4cf10364bc423001d0f897910654be30d6ef52185565a9105771202a32bbd1b8b8d2d0c85141ab53df8eeb99e19b48a8c5c95a63e3418acb7763e373d6b2b8a35defb2fc43172f60a1f0ab721a6439ec23631e28de82bde9b2c67915e372532bea26619c21c0c207cde0eb1eee6ed17872176fbaa2c898a6bef90dae131c471b23047d8440c7249315c9bebc9c100d4ccb3e42b6fc657e2a3ad6faec4a475817",
-);
-type Hash = ($: Uint8Array) => Uint8Array<ArrayBuffer>;
-const md = (
-  base: Uint32Array,
-  mix: (use: Uint32Array, data: DataView, at: number, to: Uint32Array) => void,
-): Hash =>
-($) => {
-  const a = new Uint32Array(base), b = a.length, c = b << 3;
-  const d = new Uint32Array(b * 10), e = new Uint8Array(c), f = $.length;
-  let g = new DataView($.buffer, $.byteOffset), z = 0, y = 0;
-  while (z < f) {
-    const j = min(c - y, f - z);
-    if (j !== c) e.set($.subarray(z, z += j)), y += j;
-    else do mix(d, g, z, a), z += c; while (f - z >= c);
-  }
-  g = new DataView(e.buffer), e[y] = 128, c - ++y < b && mix(d, g, y = 0, a);
-  e.fill(0, y), g.setBigUint64(c - 8, BigInt(f) << 3n), mix(d, g, y = 0, a);
-  do g.setUint32(y << 2, a[y]); while (++y < b);
-  return new Uint8Array(e.subarray(0, c >> 1));
-};
+/** SHA-2 function. */
+export type Sha2 = ($: Uint8Array) => Uint8Array<ArrayBuffer>;
 /** Hashes with SHA-256. */
-export const sha256: Hash = /* @__PURE__ */ md(SHA256, (use, data, at, to) => {
-  let a, b, z = 0;
-  do use[z] = data.getUint32(at), at += 4; while (++z < 16);
-  do a = use[z - 2],
-    b = use[z - 15],
-    use[z] = ((b >>> 7 | b << 25) ^ (b >>> 18 | b << 14) ^ b >>> 3) +
-      ((a >>> 17 | a << 15) ^ (a >>> 19 | a << 13) ^ a >>> 10) +
-      use[z - 7] + use[z - 16]; while (++z < 64);
-  let c = to[z = 0], d = to[1], e = to[2], f = to[3], g = to[4], h = to[5];
-  let i = to[6], j = to[7];
-  do a = ((g >>> 6 | g << 26) ^ (g >>> 11 | g << 21) ^ (g >>> 25 | g << 7)) +
-    (g & h ^ ~g & i) + j + HI[z] + use[z],
-    b = ((c >>> 2 | c << 30) ^ (c >>> 13 | c << 19) ^ (c >>> 22 | c << 10)) +
-      (d & e ^ c & d ^ c & e),
-    j = i,
-    i = h,
-    h = g,
-    g = f + a | 0,
-    f = e,
-    e = d,
-    d = c,
-    c = a + b | 0; while (++z < 64);
-  to[0] = to[0] + c | 0, to[1] = to[1] + d | 0, to[2] = to[2] + e | 0;
-  to[3] = to[3] + f | 0, to[4] = to[4] + g | 0, to[5] = to[5] + h | 0;
-  to[6] = to[6] + i | 0, to[7] = to[7] + j | 0;
-});
+export const sha256: Sha2 = /* @__PURE__ */
+  md.bind(null, SHA256, 64, 32, 8, (use, data, at, to) => {
+    let a, b, z = 0;
+    do use[z] = data.getUint32(at), at += 4; while (++z < 16);
+    do a = use[z - 2],
+      b = use[z - 15],
+      use[z] = ((b >>> 7 | b << 25) ^ (b >>> 18 | b << 14) ^ b >>> 3) +
+        ((a >>> 17 | a << 15) ^ (a >>> 19 | a << 13) ^ a >>> 10) +
+        use[z - 7] + use[z - 16]; while (++z < 64);
+    let c = to[z = 0], d = to[1], e = to[2], f = to[3], g = to[4], h = to[5];
+    let i = to[6], j = to[7];
+    do a = ((g >>> 6 | g << 26) ^ (g >>> 11 | g << 21) ^ (g >>> 25 | g << 7)) +
+      (g & h ^ ~g & i) + j + LO[z] + use[z],
+      b = ((c >>> 2 | c << 30) ^ (c >>> 13 | c << 19) ^ (c >>> 22 | c << 10)) +
+        (d & e ^ c & d ^ c & e),
+      j = i,
+      i = h,
+      h = g,
+      g = f + a | 0,
+      f = e,
+      e = d,
+      d = c,
+      c = a + b | 0; while (++z < 64);
+    to[0] = to[0] + c | 0, to[1] = to[1] + d | 0, to[2] = to[2] + e | 0;
+    to[3] = to[3] + f | 0, to[4] = to[4] + g | 0, to[5] = to[5] + h | 0;
+    to[6] = to[6] + i | 0, to[7] = to[7] + j | 0;
+  });
 /** Hashes with SHA-512. */
-export const sha512: Hash = /* @__PURE__ */ md(SHA512, (use, data, at, to) => {
-  let a, b, c, d, z = 0;
-  do use[z] = data.getUint32(at), use[z + 80] = data.getUint32(at + 4); while (
-    at += 8, ++z < 16
-  );
-  do (a = use[z - 15], b = use[z + 65]),
-    c = ((b >>> 1 | a << 31) ^ (b >>> 8 | a << 24) ^ (b >>> 7 | a << 25)) >>> 0,
-    d = (a >>> 1 | b << 31) ^ (a >>> 8 | b << 24) ^ a >>> 7,
-    (a = use[z - 2], b = use[z + 78]),
-    use[z + 80] = c += use[z + 64] + use[z + 73] + (((b >>> 19 | a << 13) ^
-      (a >>> 29 | b << 3) ^ (b >>> 6 | a << 26)) >>> 0),
-    use[z] = d + ((a >>> 19 | b << 13) ^ (b >>> 29 | a << 3) ^ a >>> 6) +
-      use[z - 7] + use[z - 16] + (c / 0x100000000 | 0); while (++z < 80);
-  let e = to[z = 0], f = to[1], g = to[2], h = to[3], i = to[4], j = to[5];
-  let k = to[6], l = to[7], m = to[8], n = to[9], o = to[10], p = to[11];
-  let q = to[12], r = to[13], s = to[14], t = to[15], u, v;
-  do a = (n >>> 9 | m << 23) ^ (m >>> 14 | n << 18) ^ (m >>> 18 | n << 14),
-    b = (m >>> 9 | n << 23) ^ (n >>> 14 | m << 18) ^ (n >>> 18 | m << 14),
-    u = t + (b >>> 0) + ((n & p ^ ~n & r) >>> 0) + LO[z] + use[z + 80],
-    v = a + s + (m & o ^ ~m & q) + HI[z] + use[z] + (u / 0x100000000 | 0) | 0,
-    a = (f >>> 2 | e << 30) ^ (f >>> 7 | e << 25) ^ (e >>> 28 | f << 4),
-    b = (e >>> 2 | f << 30) ^ (e >>> 7 | f << 25) ^ (f >>> 28 | e << 4),
-    c = g & i ^ g & e ^ i & e,
-    d = f & h ^ f & j ^ h & j,
-    s = q,
-    q = o,
-    o = m,
-    t = r,
-    r = p,
-    p = n >>> 0,
-    n = (u >>> 0) + l,
-    m = v + k + (n / 0x100000000 | 0) | 0,
-    k = i,
-    i = g,
-    g = e,
-    l = j,
-    j = h,
-    h = f >>> 0,
-    f = (b >>> 0) + (d >>> 0) + (u >>> 0),
-    e = (f / 0x100000000 | 0) + a + c + v | 0; while (++z < 80);
-  to[0] = to[0] + e + ((to[1] += f >>> 0) / 0x100000000 | 0) | 0;
-  to[2] = to[2] + g + ((to[3] += h) / 0x100000000 | 0) | 0;
-  to[4] = to[4] + i + ((to[5] += j) / 0x100000000 | 0) | 0;
-  to[6] = to[6] + k + ((to[7] += l) / 0x100000000 | 0) | 0;
-  to[8] = to[8] + m + ((to[9] += n >>> 0) / 0x100000000 | 0) | 0;
-  to[10] = to[10] + o + ((to[11] += p) / 0x100000000 | 0) | 0;
-  to[12] = to[12] + q + ((to[13] += r) / 0x100000000 | 0) | 0;
-  to[14] = to[14] + s + ((to[15] += t) / 0x100000000 | 0) | 0;
-});
+export const sha512: Sha2 = /* @__PURE__ */
+  md.bind(null, SHA512, 128, 64, 16, (use, data, at, to) => {
+    let a, b, c, d, z = 0;
+    do use[z] = data.getUint32(at),
+      use[z + 80] = data.getUint32(at + 4); while (at += 8, ++z < 16);
+    do a = use[z - 15],
+      b = use[z + 65],
+      c = ((b >>> 1 | a << 31) ^ (b >>> 8 | a << 24) ^ (b >>> 7 | a << 25)) >>>
+        0,
+      d = (a >>> 1 | b << 31) ^ (a >>> 8 | b << 24) ^ a >>> 7,
+      a = use[z - 2],
+      b = use[z + 78],
+      use[z + 80] = c += use[z + 64] + use[z + 73] + (((b >>> 19 | a << 13) ^
+        (a >>> 29 | b << 3) ^ (b >>> 6 | a << 26)) >>> 0),
+      use[z] = d + ((a >>> 19 | b << 13) ^ (b >>> 29 | a << 3) ^ a >>> 6) +
+        use[z - 7] + use[z - 16] + (c / Max.U | 0); while (++z < 80);
+    let e = to[z = 0], f = to[1], g = to[2], h = to[3], i = to[4], j = to[5];
+    let k = to[6], l = to[7], m = to[8], n = to[9], o = to[10], p = to[11];
+    let q = to[12], r = to[13], s = to[14], t = to[15], u, v;
+    do a = (n >>> 9 | m << 23) ^ (m >>> 14 | n << 18) ^ (m >>> 18 | n << 14),
+      b = (m >>> 9 | n << 23) ^ (n >>> 14 | m << 18) ^ (n >>> 18 | m << 14),
+      u = t + (b >>> 0) + ((n & p ^ ~n & r) >>> 0) + HI[z] + use[z + 80],
+      v = a + s + (m & o ^ ~m & q) + LO[z] + use[z] + (u / Max.U | 0) | 0,
+      a = (f >>> 2 | e << 30) ^ (f >>> 7 | e << 25) ^ (e >>> 28 | f << 4),
+      b = (e >>> 2 | f << 30) ^ (e >>> 7 | f << 25) ^ (f >>> 28 | e << 4),
+      c = g & i ^ g & e ^ i & e,
+      d = f & h ^ f & j ^ h & j,
+      s = q,
+      q = o,
+      o = m,
+      t = r,
+      r = p,
+      p = n >>> 0,
+      n = (u >>> 0) + l,
+      m = v + k + (n / Max.U | 0) | 0,
+      k = i,
+      i = g,
+      g = e,
+      l = j,
+      j = h,
+      h = f >>> 0,
+      f = (b >>> 0) + (d >>> 0) + (u >>> 0),
+      e = (f / Max.U | 0) + a + c + v | 0; while (++z < 80);
+    to[0] = to[0] + e + ((to[1] += f >>> 0) / Max.U | 0) | 0;
+    to[2] = to[2] + g + ((to[3] += h) / Max.U | 0) | 0;
+    to[4] = to[4] + i + ((to[5] += j) / Max.U | 0) | 0;
+    to[6] = to[6] + k + ((to[7] += l) / Max.U | 0) | 0;
+    to[8] = to[8] + m + ((to[9] += n >>> 0) / Max.U | 0) | 0;
+    to[10] = to[10] + o + ((to[11] += p) / Max.U | 0) | 0;
+    to[12] = to[12] + q + ((to[13] += r) / Max.U | 0) | 0;
+    to[14] = to[14] + s + ((to[15] += t) / Max.U | 0) | 0;
+  });
