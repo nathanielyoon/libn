f7b20afdaf990c1d66d072cf884877d2e73ef1e5
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-11 11:41:01 -0400

    fix: add explicit type

--- base/src/map.ts
+++ base/src/map.ts
@@ -1,5 +1,5 @@
 /** Creates a code-to-byte map. */
-export const map = ($: string) => {
+export const map = ($: string): Uint8Array<ArrayBuffer> => {
   const bin = new Uint8Array(256);
   for (let char, z = 0; z < 32; ++z) {
     bin[char = $.charCodeAt(z) | 32] = bin[char & 95] = z;
