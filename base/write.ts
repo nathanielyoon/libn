eeb1c65c5041779022f45e61ab1f996058e163a6
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-18 14:39:45 -0400

    fix: fetch txt

--- base/write.ts
+++ base/write.ts
@@ -1,6 +1,6 @@
 import { save } from "@libn/lib";
 
-await fetch("https://www.rfc-editor.org/rfc/rfc4648")
+await fetch("https://www.rfc-editor.org/rfc/rfc4648.txt")
   .then(async ($) => (await $.text()).slice(25691, 26723))
   .then((rfc4648) => ({
     rfc4648: ["16", "32", "32-hex", "64", "64url"].reduce((to, base) => ({
