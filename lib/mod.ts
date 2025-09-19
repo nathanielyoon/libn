7cedbddd93b59e45c4b1d0b09b86699342746db7
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-18 23:26:39 -0400

    fix: export types

--- lib/mod.ts
+++ lib/mod.ts
@@ -22,4 +22,16 @@ import type { Intersect, Json, Tuple } from "./src/types.ts";
 import { into, read, save, trim } from "./src/vectors.ts";
 import { fc_binary, fc_check, fc_number, fc_string } from "./src/fc.ts";
 
-export { fc_binary, fc_check, fc_number, fc_string, into, read, save, trim };
+export {
+  fc_binary,
+  fc_check,
+  fc_number,
+  fc_string,
+  type Intersect,
+  into,
+  type Json,
+  read,
+  save,
+  trim,
+  type Tuple,
+};
