0b41c5e422a16279b8c18861c20f1bff2f5b3cd2
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-19 15:39:57 -0400

    fix: rewrite tests

--- url/test.ts
+++ url/test.ts
@@ -4,29 +4,43 @@ import { fc_check, fc_str } from "../test.ts";
 import { Presigner } from "./mod.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
-const presigner = new Presigner({
-  S3_ENDPOINT: `https://${vectors.docs.bucket}.s3.amazonaws.com`,
-  S3_ID: vectors.docs.id,
-  S3_KEY: vectors.docs.key,
-  S3_REGION: vectors.docs.region,
-}, new Date(vectors.docs.date));
-Deno.test("Presigner matches signature v4 documentation example", () =>
-  assertEquals(
-    presigner.presign("GET", vectors.docs.path, {}, +vectors.docs.time),
-    vectors.docs.url.replaceAll("&amp;", "&"),
-  ));
-Deno.test("Presigner signs additional headers", () =>
-  fc_check(
-    fc.property(fc.dictionary(fc_str(), fc_str()), (headers) =>
-      assertMatch(
-        new URL(presigner.presign("GET", "", headers)).href,
-        RegExp(
-          `&X-Amz-SignedHeaders=${
-            ["host", ...Object.keys(headers)].map(($) => $.toLowerCase()).sort()
-              .join(";").replace(/[^-.\w~]/g, ($) =>
-                "%" +
-                $.charCodeAt(0).toString(16).toUpperCase().padStart(2, "0"))
-          }`,
+Deno.test("mod", async ({ step }) => {
+  await step("Presigner : signature v4 documentation", () => {
+    assertEquals(
+      new Presigner({
+        S3_ENDPOINT: `https://${vectors.docs.bucket}.s3.amazonaws.com`,
+        S3_ID: vectors.docs.id,
+        S3_KEY: vectors.docs.key,
+        S3_REGION: vectors.docs.region,
+      }, new Date(vectors.docs.date)).presign(
+        "GET",
+        vectors.docs.path,
+        {},
+        +vectors.docs.time,
+      ),
+      vectors.docs.url.replaceAll("&amp;", "&"),
+    );
+  });
+  await step("Presigner : additional headers", () => {
+    fc_check(fc.property(
+      fc.dictionary(fc_str(), fc_str()),
+      (headers) =>
+        assertMatch(
+          new Presigner({
+            S3_ENDPOINT: "https://example.com",
+            S3_ID: "",
+            S3_KEY: "",
+            S3_REGION: "auto",
+          }).presign("GET", "", headers),
+          RegExp(
+            `&X-Amz-SignedHeaders=${
+              ["host", ...Object.keys(headers)].map(($) => $.toLowerCase())
+                .sort().join(";").replace(/[^-.\w~]/g, ($) =>
+                  "%" +
+                  $.charCodeAt(0).toString(16).toUpperCase().padStart(2, "0"))
+            }`,
+          ),
         ),
-      )),
-  ));
+    ));
+  });
+});
--- url/write.ts
+++ url/write.ts
@@ -1,11 +1,10 @@
-import { write_vectors } from "../test.ts";
+import { save } from "@libn/lib";
 
-await write_vectors(import.meta, {
-  docs: await fetch(
-    "https://web.archive.org/web/20140302054859/https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example",
-  ).then(($) => $.text()).then(($) =>
+await fetch(
+  "https://web.archive.org/web/20140302054859/https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example",
+).then(($) => $.text()).then(($) => ({
+  docs:
     /<code class="code">(?<path>.+?)<\/code>.*?<code class="code">(?<bucket>.+?)<\/code>.*?\((?<time>\d+).*?<code class="code">(?<date>.+?)<\/code>.*?<code.*?<code.*?<code class="code">(?<region>.+?)<\/code>.*?<code class="code">(?<id>.+?)<\/code>.*?<code class="code">(?<key>.+?)<\/code>.*?:<\/p><pre class="programlisting">(?<url>.+?)<\/pre>/s
       .exec($.slice($.indexOf('id="query-string-auth-v4-signing-example"')))
-      ?.groups ?? null
-  ),
-});
+      ?.groups ?? null,
+})).then(save(import.meta));
