9764d224f2bb4363404ef470baacb15e0eaa6782
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-20 15:38:56 -0400

    fix: rewrite tests a little

--- weight/test.ts
+++ weight/test.ts
@@ -1,19 +1,25 @@
 import { assertAlmostEquals, assertEquals } from "@std/assert";
 import { assign } from "./src/assign.ts";
-import vectors from "./vectors.json" with { type: "json" };
 import { Blossom } from "./src/blossom.ts";
+import vectors from "./vectors.json" with { type: "json" };
 
-Deno.test("assign matches munkres.py", () =>
-  vectors.munkres.forEach(({ weights, total }) =>
-    assertAlmostEquals(
-      assign(weights).reduce((to, col, row) => to + weights[row][col], 0),
-      total,
-    )
-  ));
-Deno.test("blossom matches mwmatching.py", () =>
-  vectors.mwmatching.forEach(({ edges, max, matching }) =>
-    assertEquals(
-      new Blossom(edges as [number, number, number][], max).mate,
-      new Int32Array(matching),
-    )
-  ));
+Deno.test("assign", async ({ step }) => {
+  await step("assign : munkres.py", () => {
+    for (const $ of vectors.assign.munkres) {
+      assertAlmostEquals(
+        assign($.weights).reduce((to, col, row) => to + $.weights[row][col], 0),
+        $.total,
+      );
+    }
+  });
+});
+Deno.test("blossom", async ({ step }) => {
+  await step("Blossom : mwmatching.py", () => {
+    for (const $ of vectors.blossom.mwmatching) {
+      assertEquals(
+        new Blossom($.edges as [number, number, number][], $.max).mate,
+        new Int32Array($.matching),
+      );
+    }
+  });
+});
--- weight/write.ts
+++ weight/write.ts
@@ -1,12 +1,17 @@
-import { write_vectors } from "../test.ts";
+import { save } from "@libn/lib";
 
-await write_vectors(import.meta, {
-  munkres: await Promise.all(["", "test/test_"].map(($) =>
+await Promise.all([
+  ...["", "test/test_"].map(($) =>
     fetch(
       `https://raw.githubusercontent.com/bmc/munkres/99a5c54672ea03d0b4a1f7b168cd587325bc068e/${$}munkres.py`,
     ).then(($) => $.text())
-  )).then(([munkres, test_munkres]) =>
-    [
+  ),
+  fetch(
+    "https://jorisvr.nl/files/graphmatching/20130407/mwmatching.py",
+  ).then(async ($) => (await $.text()).slice(35659)),
+]).then(([munkres, test_munkres, mwmatching]) => ({
+  assign: {
+    munkres: [
       ...munkres.slice(17105, 17980).match(/(?<=\().+?(?=\))/gs)!,
       ...test_munkres.matchAll(
         /matrix = (\[[\s,-.\d[\]]+?\])\s+cost = _get_cost\(matrix\)\s+assert cost == (?:pytest\.approx\()?(\d+(?:\.\d+)?)/gs,
@@ -14,12 +19,10 @@ await write_vectors(import.meta, {
     ].map(($) => {
       const [weights, total] = JSON.parse(`[${$}]`);
       return { weights, total };
-    })
-  ),
-  mwmatching: await fetch(
-    "https://jorisvr.nl/files/graphmatching/20130407/mwmatching.py",
-  ).then(async ($) => (await $.text()).slice(35659)).then(($) =>
-    $.replace("math.pi", `${Math.PI}`)
+    }),
+  },
+  blossom: {
+    mwmatching: mwmatching.replace("math.pi", `${Math.PI}`)
       .replace("math.exp(1)", `${Math.exp(1)}`)
       .replace("math.sqrt(2.0)", `${Math.SQRT2}`)
       .matchAll(/\((\[[^\]]+\])(?:, (True|False))?\), (\[[^\]]+\])/g)
@@ -28,6 +31,6 @@ await write_vectors(import.meta, {
         edges: JSON.parse($[1].replaceAll("(", "[").replaceAll(")", "]")),
         max: $[2] === "True",
         matching: JSON.parse($[3]),
-      }))
-  ),
-});
+      })),
+  },
+})).then(save(import.meta));
