757dd78ac93eb4ac8b82c0f930d7b956d9831cca
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-23 11:41:41 -0400

    fix: rename message constructor

--- aead/src/aead.ts
+++ aead/src/aead.ts
@@ -1,7 +1,7 @@
 import { chacha, hchacha, xor } from "./chacha.ts";
 import { poly } from "./poly.ts";
 
-const message = (ciphertext: Uint8Array, additional: Uint8Array) => {
+const construct = (ciphertext: Uint8Array, additional: Uint8Array) => {
   const a = additional.length + 15 & ~15, b = ciphertext.length + a + 15 & ~15;
   const full = new Uint8Array(b + 16), view = new DataView(full.buffer);
   full.set(additional), view.setUint32(b, additional.length, true);
@@ -21,7 +21,7 @@ export const xchachapoly = (
   const iv0 = iv[16] | iv[17] << 8 | iv[18] << 16 | iv[19] << 24;
   const iv1 = iv[20] | iv[21] << 8 | iv[22] << 16 | iv[23] << 24;
   chacha(xorer, 0, 0, iv0, iv1, ZERO), xor(xorer, 0, iv0, iv1, plaintext, 1);
-  return poly(ZERO, message(plaintext, associated_data));
+  return poly(ZERO, construct(plaintext, associated_data));
 };
 /** If parameters are valid, checks a tag, then XORs the ciphertext in-place. */
 export const polyxchacha = (
@@ -36,7 +36,7 @@ export const polyxchacha = (
   const iv0 = iv[16] | iv[17] << 8 | iv[18] << 16 | iv[19] << 24;
   const iv1 = iv[20] | iv[21] << 8 | iv[22] << 16 | iv[23] << 24;
   chacha(xorer, 0, 0, iv0, iv1, ZERO);
-  const calculated_tag = poly(ZERO, message(ciphertext, associated_data));
+  const calculated_tag = poly(ZERO, construct(ciphertext, associated_data));
   let is_different = 0, z = 16;
   do is_different |= tag[--z] ^ calculated_tag[z]; while (z);
   return !is_different && (xor(xorer, 0, iv0, iv1, ciphertext, 1), true);
