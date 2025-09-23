d5261add57beb22c8eac381558a6941b5d92737f
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-23 13:33:26 -0400

    fix: ignore bundle error

--- lib/src/bundle.ts
+++ lib/src/bundle.ts
@@ -2,6 +2,7 @@
 export const bundle = async (meta: ImportMeta): Promise<string> => {
   const entry = new URL(meta.resolve("./entry.ts"));
   await Deno.writeTextFile(entry, 'import "./mod.ts";\n');
+  // @ts-ignore: `deno check` for workspace doesn't respect --unstable-bundle
   const result = await Deno.bundle({
     entrypoints: [entry.pathname],
     format: "esm",
