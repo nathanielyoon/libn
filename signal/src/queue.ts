c6c9f72c166a147ec3a77ebca139bcc0e6345d18
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-24 18:22:12 -0400

    fix: refactor inner methods

--- signal/src/queue.ts
+++ signal/src/queue.ts
@@ -1,4 +1,4 @@
-import { Flag } from "./flags.ts";
+import { Flag, Kind } from "./flags.ts";
 import type { Effect, Link, Node, Scoper } from "./nodes.ts";
 
 /** Effects to run. */
@@ -14,8 +14,11 @@ export const flush = (run: ($: Effect | Scoper) => void): void => {
 };
 const rerun = ($: Effect | Scoper) => {
   do if ($.flags & Flag.QUEUE) break;
-  else if ($.flags |= Flag.QUEUE, $.sub) $ = $.sub.sub as Effect | Scoper;
-  else return queue.push($); while ($);
+  else {
+    $.flags |= Flag.QUEUE;
+    if ($.sub) $ = $.sub.sub as Effect | Scoper;
+    else return queue.push($);
+  } while ($);
 };
 const valid = ($: Node, link: Link) => {
   for (let a = $.head; a; a = a.dep_prev) if (a === link) return true;
@@ -23,28 +26,37 @@ const valid = ($: Node, link: Link) => {
 /** Deeply propagates changes. */
 export const deep = ($: Link, run: ($: Effect | Scoper) => void): void => {
   top: for (let a: (Link | null)[] = [], b = $.sub_next, c, d;;) {
-    if (c = $.sub, d = c.flags, !(d & Flag.KNOWN)) c.flags |= Flag.READY;
-    else if (!(d & Flag.GOING)) d = Flag.CLEAR;
-    else if (!(d & Flag.CHECK)) c.flags = d & ~Flag.RECUR | Flag.READY;
-    else if (d & Flag.SETUP || !valid(c, $)) d = Flag.CLEAR;
-    else c.flags |= Flag.EARLY, d &= Flag.BEGIN;
-    if (d & Flag.WATCH && rerun(c as Effect | Scoper), d & Flag.BEGIN) {
-      if (c.sub && ($ = c.sub).sub_next) a.push(b), b = $.sub_next;
-    } else if (!b) {
-      while (a.length) {
-        if ($ = a.pop()!) {
-          b = $.sub_next;
-          continue top;
+    c = $.sub, d = c.flags;
+    switch (d & Flag.KNOWN) {
+      case Flag.RECUR:
+        c.flags = d & ~Flag.RECUR; // falls through
+      case Flag.CLEAR:
+        c.flags |= Flag.READY, c.kind === Kind.EFFECT && rerun(c);
+        break;
+      default:
+        if (d & Flag.SETUP || !valid(c, $)) d = Flag.CLEAR;
+        else c.flags |= Flag.EARLY, d &= Flag.BEGIN;
+    }
+    if (!(d & Flag.BEGIN)) {
+      if (!b) {
+        while (a.length) {
+          if ($ = a.pop()!) {
+            b = $.sub_next;
+            continue top;
+          }
         }
-      }
-      break;
-    } else b = ($ = b).sub_next;
+        break;
+      } else $ = b, b = b.sub_next;
+    } else if (c.sub) {
+      $ = c.sub;
+      if ($.sub_next) a.push(b), b = $.sub_next;
+    }
   }
   depth || flush(run);
 };
 /** Shallowly propagates changes. */
 export const flat = ($: Link): void => {
   do if (($.sub.flags & Flag.SETUP) === Flag.READY) {
-    ($.sub.flags |= Flag.DIRTY) & Flag.WATCH && rerun($.sub as Effect | Scoper);
+    $.sub.flags |= Flag.DIRTY, $.sub.kind === Kind.EFFECT && rerun($.sub);
   } while ($ = $.sub_next!);
 };
