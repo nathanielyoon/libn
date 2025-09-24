fad0ebde89ccb744e174da76991bc5053af03b8d
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-24 12:37:10 -0400

    fix: improve names

--- signal/src/api.ts
+++ signal/src/api.ts
@@ -34,7 +34,7 @@ function deriver(this: Derive) {
     : (this.flags &= ~Flag.READY);
   return link(this, scope ?? actor), this.prev;
 }
-const node = <A extends Kind, B>(kind: A, flags: Flag, rest: B) => (
+const construct = <A extends Kind, B>(kind: A, flags: Flag, rest: B) => (
   { kind, flags, head: null, dep: null, sub: null, tail: null, ...rest }
 );
 /** Reactive getter. */
@@ -44,7 +44,7 @@ export type Set<A> = <const B extends A>($: B | (($: A) => B)) => B;
 /** Creates a reactive value. */
 export const signal =
   ((initial: any, equals?: Equals<any, any>) =>
-    sourcer.bind(node(Kind.SIGNAL, Flag.BEGIN, {
+    sourcer.bind(construct(Kind.SIGNAL, Flag.BEGIN, {
       prev: initial,
       next: initial,
       same: equals,
@@ -58,43 +58,44 @@ export const signal =
 /** Creates a derived computation. */
 export const derive =
   ((compute: any, initial?: any, equals?: Equals<any, any>) =>
-    deriver.bind(node(Kind.DERIVE, Flag.RESET, {
+    deriver.bind(construct(Kind.DERIVE, Flag.RESET, {
       prev: initial,
       next: compute,
       same: equals,
     }))) as {
       <A>(compute: (was: A) => A, initial: A, equals?: Equals<A, A>): () => A;
       // Omitting the initial value limits type inference for the deriver's
-      // parameter (see <https://github.com/microsoft/TypeScript/issues/47599>).
+      // parameter (see <https://github.com/microsoft/TypeScript/issues/47599>),
+      // but it works fine if you add an explicit type.
       <A>(
-        compute: (was: A | undefined) => A,
+        compute: (prev: A | undefined) => A,
         initial?: undefined,
         equals?: Equals<A | undefined, A>,
       ): () => A;
     };
 /** Creates a side effect and returns a disposer. */
 export const effect = (run: () => void): () => void => {
-  const a = node(Kind.EFFECT, Flag.WATCH, { run });
-  link(a, scope ?? actor);
-  const b = set_actor(a);
+  const node = construct(Kind.EFFECT, Flag.WATCH, { run });
+  link(node, scope ?? actor);
+  const prev_actor = set_actor(node);
   try {
-    a.run();
+    node.run();
   } finally {
-    set_actor(b);
+    set_actor(prev_actor);
   }
-  return dispose.bind(a);
+  return dispose.bind(node);
 };
 /** Creates a group of effects and returns a disposer. */
 export const scoper = (all: () => void): () => void => {
-  const a = node(Kind.SCOPER, Flag.CLEAR, {});
-  link(a, scope);
-  const b = set_actor(null), c = set_scope(a);
+  const node = construct(Kind.SCOPER, Flag.CLEAR, {});
+  link(node, scope);
+  const prev_actor = set_actor(null), prev_scope = set_scope(node);
   try {
     all();
   } finally {
-    set_actor(b), set_scope(c);
+    set_actor(prev_actor), set_scope(prev_scope);
   }
-  return dispose.bind(a);
+  return dispose.bind(node);
 };
 /** Pauses updates, executes a function, then resumes. */
 export const batch = <A>($: () => A): A => {
