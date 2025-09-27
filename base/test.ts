141631615061f60d1162325c7a4d2b2021b2d5f9
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-27 08:56:04 -0400

    fix: simplify factoring

--- base/test.ts
+++ base/test.ts
@@ -17,7 +17,7 @@ import { de_b64, de_u64, en_b64, en_u64 } from "./src/64.ts";
 import { de_bin, en_bin } from "./mod.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
-const all = (encode: Encode, decode: Decode) =>
+const round = (encode: Encode, decode: Decode) => () =>
   fc_check(fc.property(fc_binary(), ($) => assertEquals(decode(encode($)), $)));
 Deno.test("16", async ({ step }) => {
   await step("en_b16/de_b16 : rfc4648 10", () => {
@@ -26,7 +26,7 @@ Deno.test("16", async ({ step }) => {
       assertEquals(de_bin(de_b16($.binary)), $.ascii);
     }
   });
-  await step("en_b16/de_b16 : arbitrary round-trip", () => all(en_b16, de_b16));
+  await step("en_b16/de_b16 : arbitrary round-trip", round(en_b16, de_b16));
 });
 Deno.test("32", async ({ step }) => {
   await step("en_b32/de_b32 : rfc4648 10", () => {
@@ -41,10 +41,10 @@ Deno.test("32", async ({ step }) => {
       assertEquals(de_bin(de_h32($.binary)), $.ascii);
     }
   });
-  await step("en_b32/de_b32 : arbitrary round-trip", () => all(en_b32, de_b32));
-  await step("en_h32/de_h32 : arbitrary round-trip", () => all(en_h32, de_h32));
-  await step("en_z32/de_z32 : arbitrary round-trip", () => all(en_z32, de_z32));
-  await step("en_c32/de_c32 : arbitrary round-trip", () => all(en_c32, de_c32));
+  await step("en_b32/de_b32 : arbitrary round-trip", round(en_b32, de_b32));
+  await step("en_h32/de_h32 : arbitrary round-trip", round(en_h32, de_h32));
+  await step("en_z32/de_z32 : arbitrary round-trip", round(en_z32, de_z32));
+  await step("en_c32/de_c32 : arbitrary round-trip", round(en_c32, de_c32));
 });
 Deno.test("64", async ({ step }) => {
   await step("en_b64/de_b64 : rfc4648 10", () => {
@@ -59,8 +59,8 @@ Deno.test("64", async ({ step }) => {
       assertEquals(de_bin(de_u64($.binary)), $.ascii);
     }
   });
-  await step("en_b64/de_b64 : arbitrary round-trip", () => all(en_b64, de_b64));
-  await step("en_u64/de_u64 : arbitrary round-trip", () => all(en_u64, de_u64));
+  await step("en_b64/de_b64 : arbitrary round-trip", round(en_b64, de_b64));
+  await step("en_u64/de_u64 : arbitrary round-trip", round(en_u64, de_u64));
 });
 Deno.test("mod", async ({ step }) => {
   await step("en_bin/de_bin :: separate instantiations and calls", () => {
