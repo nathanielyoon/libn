27827421be3c48422a59b7dda4ab1c478f4f2839
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-27 08:23:05 -0400

    fix: make map size dependent on string

--- base/src/common.ts
+++ base/src/common.ts
@@ -2,11 +2,9 @@
 export type Encode = ($: Uint8Array) => string;
 /** Text-to-binary decoder. */
 export type Decode = ($: string) => Uint8Array<ArrayBuffer>;
-/** Creates a case-insensitive code-to-byte map. */
+/** Creates a case-sensitive code-to-byte map. */
 export const map = ($: string): Uint8Array<ArrayBuffer> => {
   const bin = new Uint8Array(256);
-  for (let char, z = 0; z < 32; ++z) {
-    bin[char = $.charCodeAt(z) | 32] = bin[char & 95] = z;
-  }
+  for (let z = 0; z < $.length; ++z) bin[$.charCodeAt(z)] = z;
   return bin;
 };
