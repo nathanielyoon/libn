7853655d7e529902b8051e8b736fc83366886aa7
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-28 18:40:16 -0400

    fix: import from parent directory

--- lib/src/pure.ts
+++ lib/src/pure.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "@std/assert";
 /** Checks that a bundle is pure. */
 export const pure = async (t: Deno.TestContext): Promise<void> => {
   const entry = new URL(t.origin.replace(/[^/]+$/, "entry.ts"));
-  await Deno.writeTextFile(entry, 'import "./mod.ts";\n');
+  await Deno.writeTextFile(entry, 'import "../mod.ts";\n');
   // @ts-ignore: `deno check` for workspace doesn't respect --unstable-bundle
   const result = await Deno.bundle({
     entrypoints: [entry.pathname],
