2bed8db92eeee1b996f77d980aa0b739785a844f
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-01 14:26:41 -0400

    fix: make message and ad any length

--- aead/bench.ts
+++ aead/bench.ts
@@ -4,23 +4,31 @@ import { polyxchacha, xchachapoly } from "./src/aead.ts";
 import { xchacha20poly1305 as noble } from "@noble/ciphers/chacha.js";
 import * as stablelib from "@stablelib/xchacha20poly1305";
 
-fc_bench("encrypt", fc.tuple(fc_bin(32), fc_bin(24), fc_bin(100), fc_bin(10)), {
-  libn: (key, iv, plaintext, ad) => ({
-    ciphertext: plaintext,
-    tag: xchachapoly(key, iv, plaintext, ad),
-  }),
-  noble: (key, iv, plaintext, ad) => {
-    const output = noble(key, iv, ad).encrypt(plaintext);
-    return { ciphertext: output.subarray(0, -16), tag: output.subarray(-16) };
-  },
-  stablelib: (key, iv, plaintext, ad) => {
-    const output = new stablelib.XChaCha20Poly1305(key).seal(iv, plaintext, ad);
-    return { ciphertext: output.subarray(0, -16), tag: output.subarray(-16) };
-  },
-}, 100);
 fc_bench(
-  "decrypt",
-  fc.tuple(fc_bin(32), fc_bin(24), fc_bin(100), fc_bin(10)).map(
+  { group: "encrypt" },
+  fc.tuple(fc_bin(32), fc_bin(24), fc_bin(), fc_bin()),
+  {
+    libn: (key, iv, plaintext, ad) => ({
+      ciphertext: plaintext,
+      tag: xchachapoly(key, iv, plaintext, ad),
+    }),
+    noble: (key, iv, plaintext, ad) => {
+      const output = noble(key, iv, ad).encrypt(plaintext);
+      return { ciphertext: output.subarray(0, -16), tag: output.subarray(-16) };
+    },
+    stablelib: (key, iv, plaintext, ad) => {
+      const output = new stablelib.XChaCha20Poly1305(key).seal(
+        iv,
+        plaintext,
+        ad,
+      );
+      return { ciphertext: output.subarray(0, -16), tag: output.subarray(-16) };
+    },
+  },
+);
+fc_bench(
+  { group: "decrypt" },
+  fc.tuple(fc_bin(32), fc_bin(24), fc_bin(), fc_bin()).map(
     ([key, iv, plaintext, ad]) =>
       [key, iv, xchachapoly(key, iv, plaintext, ad)!, plaintext, ad] as const,
   ),
@@ -40,5 +48,4 @@ fc_bench(
       return new stablelib.XChaCha20Poly1305(key).open(iv, buffer, ad);
     },
   },
-  100,
 );
