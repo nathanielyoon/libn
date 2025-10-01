6cae7f8bcf2c192c478539582774e08aa507c264
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-09-30 23:25:56 -0400

    fix: simplify test

--- aead/test/mod.test.ts
+++ aead/test/mod.test.ts
@@ -1,45 +1,29 @@
 import { assert, assertEquals } from "@std/assert";
-import fc from "fast-check";
-import { fc_bin, fc_check, pure, read } from "@libn/lib";
+import { fc_assert, fc_bin, pure, read } from "@libn/lib";
 import { cipher, decrypt, encrypt } from "../mod.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
-Deno.test("mod", async ({ step }) => {
-  await step("cipher : xchacha-03 A.3.2.1", () => {
-    for (const $ of read(vectors.mod["xchacha-03 A.3.2.1"])) {
-      const text = new Uint8Array($.plaintext.length);
-      cipher($.key, $.iv, text);
-      assertEquals(text, $.keystream);
-      text.set($.plaintext);
-      cipher($.key, $.iv, text);
-      assertEquals(text, $.ciphertext);
-    }
-  });
-  await step("encrypt/decrypt : arbitrary round-trip", () => {
-    fc_check(fc.property(
-      fc_bin(32),
-      fc_bin(),
-      fc_bin(),
-      (key, plaintext, data) => {
-        const text_1 = encrypt(key, plaintext, data);
-        assert(text_1);
-        assertEquals(decrypt(key, text_1, data)!, plaintext);
-        const text_2 = encrypt(key, plaintext);
-        assert(text_2);
-        assertEquals(decrypt(key, text_2), plaintext);
-      },
-    ));
-  });
-  await step("encrypt/decrypt : wrong-size arguments", () => {
-    fc_check(fc.property(
-      fc_bin(-32),
-      fc_bin(),
-      fc_bin(),
-      (key, $, data) => {
-        assertEquals(encrypt(key, $, data), null);
-        assertEquals(decrypt(key, $, data), null);
-      },
-    ));
-  });
-  await step("bundle : pure", pure);
-});
+Deno.test("cipher : xchacha-03 A.3.2.1", () =>
+  read(vectors.mod["xchacha-03 A.3.2.1"]).forEach(($) => {
+    const text = new Uint8Array($.plaintext.length);
+    cipher($.key, $.iv, text);
+    assertEquals(text, $.keystream);
+    text.set($.plaintext);
+    cipher($.key, $.iv, text);
+    assertEquals(text, $.ciphertext);
+  }));
+Deno.test("encrypt/decrypt : arbitrary round-trip", () =>
+  fc_assert(fc_bin(32), fc_bin(), fc_bin())((key, plaintext, data) => {
+    const text_1 = encrypt(key, plaintext, data);
+    assert(text_1);
+    assertEquals(decrypt(key, text_1, data)!, plaintext);
+    const text_2 = encrypt(key, plaintext);
+    assert(text_2);
+    assertEquals(decrypt(key, text_2), plaintext);
+  }));
+Deno.test("encrypt/decrypt : wrong-size arguments", () =>
+  fc_assert(fc_bin(-32), fc_bin(), fc_bin())((key, $, data) => {
+    assertEquals(encrypt(key, $, data), null);
+    assertEquals(decrypt(key, $, data), null);
+  }));
+Deno.test("bundle : pure", pure);
