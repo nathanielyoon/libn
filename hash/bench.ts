e0bf56de4cb1aa0382114c1cb163bac5a0eba388
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-01 10:05:29 -0400

    new: add hash benchmark

---
+++ hash/bench.ts
@@ -0,0 +1,82 @@
+import fc from "fast-check";
+import { fc_bench, fc_bin } from "@libn/lib";
+import {
+  b2b,
+  b2s,
+  b3,
+  hkdf_sha256,
+  hmac_sha256,
+  sha224,
+  sha256,
+  sha384,
+  sha512,
+} from "./mod.ts";
+import * as noble_sha2 from "@noble/hashes/sha2.js";
+import { hmac as noble_hmac } from "@noble/hashes/hmac.js";
+import { hkdf as noble_hkdf } from "@noble/hashes/hkdf.js";
+import * as noble_b2 from "@noble/hashes/blake2.js";
+import { blake3 as noble_b3 } from "@noble/hashes/blake3.js";
+import { hash as stablelib_sha224 } from "@stablelib/sha224";
+import { hash as stablelib_sha256, SHA256 } from "@stablelib/sha256";
+import { hash as stablelib_sha384 } from "@stablelib/sha384";
+import { hash as stablelib_sha512 } from "@stablelib/sha512";
+import { hmac as stablelib_hmac } from "@stablelib/hmac";
+import { HKDF as stablelib_hkdf } from "@stablelib/hkdf";
+import { hash as stablelib_b2s } from "@stablelib/blake2s";
+import { hash as stablelib_b2b } from "@stablelib/blake2b";
+
+fc_bench("sha224", fc.tuple(fc_bin(100)), {
+  libn: sha224,
+  noble: noble_sha2.sha224,
+  stablelib: stablelib_sha224,
+});
+fc_bench("sha256", fc.tuple(fc_bin(100)), {
+  libn: sha256,
+  noble: noble_sha2.sha256,
+  stablelib: stablelib_sha256,
+});
+fc_bench("sha384", fc.tuple(fc_bin(100)), {
+  libn: sha384,
+  noble: noble_sha2.sha384,
+  stablelib: stablelib_sha384,
+});
+fc_bench("sha512", fc.tuple(fc_bin(100)), {
+  libn: sha512,
+  noble: noble_sha2.sha512,
+  stablelib: stablelib_sha512,
+});
+fc_bench("hmac", fc.tuple(fc_bin(32), fc_bin(100)), {
+  libn: hmac_sha256,
+  noble: noble_hmac.bind(null, noble_sha2.sha256),
+  stablelib: stablelib_hmac.bind(null, SHA256),
+});
+fc_bench(
+  "hkdf",
+  fc.tuple(
+    fc_bin(32),
+    fc_bin(100),
+    fc_bin(32),
+    fc.integer({ min: 1, max: 8160 }),
+  ),
+  {
+    libn: hkdf_sha256,
+    noble: (key, info, salt, size) =>
+      noble_hkdf(noble_sha2.sha256, key, salt, info, size),
+    stablelib: (key, info, salt, size) =>
+      new stablelib_hkdf(SHA256, key, salt, info).expand(size),
+  },
+);
+fc_bench("blake2s", fc.tuple(fc_bin(100)), {
+  libn: b2s,
+  noble: noble_b2.blake2s,
+  stablelib: stablelib_b2s,
+});
+fc_bench("blake2b", fc.tuple(fc_bin(100)), {
+  libn: b2b,
+  noble: noble_b2.blake2b,
+  stablelib: stablelib_b2b,
+});
+fc_bench("blake3", fc.tuple(fc_bin(100)), {
+  libn: b3,
+  noble: noble_b3,
+});
