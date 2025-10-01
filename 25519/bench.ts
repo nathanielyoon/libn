95dc9fc470f17adf4d2e1ef46a303d5db2f26bcf
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-01 01:40:06 -0400

    fix: rename

--- 25519/bench.ts
+++ 25519/bench.ts
@@ -20,7 +20,7 @@ import ed2curve from "ed2curve";
 
 fc_bench("generate", fc.tuple(fc_bin(32)), {
   libn: (secret_key) => generate(secret_key),
-  "@noble/curves": (secret_key) => noble_ed25519.getPublicKey(secret_key),
+  noble: (secret_key) => noble_ed25519.getPublicKey(secret_key),
   stablelib: (secret_key) =>
     stablelib_ed25519.generateKeyPairFromSeed(secret_key).publicKey,
   tweetnacl: (secret_key) =>
@@ -41,8 +41,7 @@ fc_bench(
   ),
   {
     libn: (secret_key, message) => sign(secret_key, message),
-    "@noble/curves": (secret_key, message) =>
-      noble_ed25519.sign(message, secret_key),
+    noble: (secret_key, message) => noble_ed25519.sign(message, secret_key),
     stablelib: (_, message, secret_key) =>
       stablelib_ed25519.sign(secret_key, message),
     tweetnacl: (_, message, __, secret_key) =>
@@ -57,7 +56,7 @@ fc_bench(
   {
     libn: (public_key, message, signature) =>
       verify(public_key, message, signature),
-    "@noble/curves": (public_key, message, signature) =>
+    noble: (public_key, message, signature) =>
       noble_ed25519.verify(signature, message, public_key),
     stablelib: (public_key, message, signature) =>
       stablelib_ed25519.verify(public_key, message, signature),
@@ -67,25 +66,25 @@ fc_bench(
 );
 fc_bench("derive", fc.tuple(fc_bin(32)), {
   libn: derive,
-  "@noble/curves": noble_x25519.getPublicKey,
+  noble: noble_x25519.getPublicKey,
   stablelib: stablelib_x25519.scalarMultBase,
   tweetnacl: tweetnacl.scalarMult.base,
 });
 fc_bench("exchange", fc.tuple(fc_bin(32), fc_bin(32).map(derive)), {
   libn: exchange,
-  "@noble/curves": noble_x25519.getSharedSecret,
+  noble: noble_x25519.getSharedSecret,
   stablelib: stablelib_x25519.sharedKey,
   tweetnacl: tweetnacl.scalarMult,
 });
 fc_bench("convert_secret", fc.tuple(fc_bin(32)), {
   libn: convert_secret,
-  "@noble/curves": noble_ed25519.utils.toMontgomerySecret,
+  noble: noble_ed25519.utils.toMontgomerySecret,
   stablelib: stablelib_ed25519.convertSecretKeyToX25519,
   tweetnacl: ed2curve.convertSecretKey,
 }, 10);
 fc_bench("convert_public", fc.tuple(fc_bin(32).map(generate)), {
   libn: convert_public,
-  "@noble/curves": noble_ed25519.utils.toMontgomery,
+  noble: noble_ed25519.utils.toMontgomery,
   stablelib: stablelib_ed25519.convertPublicKeyToX25519,
   tweetnacl: ed2curve.convertPublicKey,
 });
