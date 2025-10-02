187a2d9eaa216fbcd03781abe083fcb867920f98
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-02 01:32:27 -0400

    fix: simplify benchmark

--- 25519/bench.ts
+++ 25519/bench.ts
@@ -1,14 +1,6 @@
-import fc from "fast-check";
-import { fc_bench, fc_bin } from "@libn/lib";
-import {
-  convert_public,
-  convert_secret,
-  derive,
-  exchange,
-  generate,
-  sign,
-  verify,
-} from "./mod.ts";
+import { bench } from "@libn/lib";
+import { get_pair, set_pair } from "./test/common.ts";
+import * as libn from "./mod.ts";
 import {
   ed25519 as noble_ed25519,
   x25519 as noble_x25519,
@@ -18,68 +10,50 @@ import * as stablelib_x25519 from "@stablelib/x25519";
 import tweetnacl from "tweetnacl";
 import ed2curve from "ed2curve";
 
-fc_bench({ group: "generate" }, fc.tuple(fc_bin(32)), {
-  libn: (secret_key) => generate(secret_key),
-  noble: (secret_key) => noble_ed25519.getPublicKey(secret_key),
-  stablelib: (secret_key) =>
-    stablelib_ed25519.generateKeyPairFromSeed(secret_key).publicKey,
-  tweetnacl: (secret_key) =>
-    tweetnacl.sign.keyPair.fromSeed(secret_key).publicKey,
+const edwards = await get_pair("Ed");
+const [secret_e, public_e] = await set_pair(edwards);
+bench({ group: "generate" }, {
+  libn: () => libn.generate(secret_e),
+  noble: () => noble_ed25519.getPublicKey(secret_e),
+  stablelib: () =>
+    stablelib_ed25519.generateKeyPairFromSeed(secret_e).publicKey,
+  tweetnacl: () => tweetnacl.sign.keyPair.fromSeed(secret_e).publicKey,
 });
-fc_bench(
-  { group: "sign" },
-  fc.tuple(fc_bin(32), fc_bin()).map(([key, message]) => [
-    key,
-    message,
-    stablelib_ed25519.generateKeyPairFromSeed(key).secretKey,
-    tweetnacl.sign.keyPair.fromSeed(key).secretKey,
-  ]),
-  {
-    libn: (secret_key, message) => sign(secret_key, message),
-    noble: (secret_key, message) => noble_ed25519.sign(message, secret_key),
-    stablelib: (_, message, secret_key) =>
-      stablelib_ed25519.sign(secret_key, message),
-    tweetnacl: (_, message, __, secret_key) =>
-      tweetnacl.sign.detached(message, secret_key),
-  },
+const stablelib_key =
+  stablelib_ed25519.generateKeyPairFromSeed(secret_e).secretKey;
+const tweetnacl_key = tweetnacl.sign.keyPair.fromSeed(secret_e).secretKey;
+const message = crypto.getRandomValues(new Uint8Array(100));
+bench({ group: "sign" }, {
+  libn: () => libn.sign(secret_e, message),
+  noble: () => noble_ed25519.sign(message, secret_e),
+  stablelib: () => stablelib_ed25519.sign(stablelib_key, message),
+  tweetnacl: () => tweetnacl.sign.detached(message, tweetnacl_key),
+});
+const signature = new Uint8Array(
+  await crypto.subtle.sign("Ed25519", edwards.privateKey, message),
 );
-fc_bench(
-  { group: "verify" },
-  fc.tuple(fc_bin(32), fc_bin()).map(([key, message]) =>
-    [generate(key), message, sign(key, message)] as const
-  ),
-  {
-    libn: (public_key, message, signature) =>
-      verify(public_key, message, signature),
-    noble: (public_key, message, signature) =>
-      noble_ed25519.verify(signature, message, public_key),
-    stablelib: (public_key, message, signature) =>
-      stablelib_ed25519.verify(public_key, message, signature),
-    tweetnacl: (public_key, message, signature) =>
-      tweetnacl.sign.detached.verify(message, signature, public_key),
-  },
-);
-fc_bench({ group: "derive" }, fc.tuple(fc_bin(32)), {
-  libn: derive,
-  noble: noble_x25519.getPublicKey,
-  stablelib: stablelib_x25519.scalarMultBase,
-  tweetnacl: tweetnacl.scalarMult.base,
+bench({ group: "verify" }, {
+  libn: () => libn.verify(public_e, message, signature),
+  noble: () => noble_ed25519.verify(signature, message, public_e),
+  stablelib: () => stablelib_ed25519.verify(public_e, message, signature),
+  tweetnacl: () => tweetnacl.sign.detached.verify(message, signature, public_e),
 });
-fc_bench({ group: "exchange" }, fc.tuple(fc_bin(32), fc_bin(32).map(derive)), {
-  libn: exchange,
-  noble: noble_x25519.getSharedSecret,
-  stablelib: stablelib_x25519.sharedKey,
-  tweetnacl: tweetnacl.scalarMult,
+bench({ group: "convert_public" }, {
+  libn: () => libn.convert_public(public_e),
+  noble: () => noble_ed25519.utils.toMontgomery(public_e),
+  stablelib: () => stablelib_ed25519.convertPublicKeyToX25519(public_e),
+  tweetnacl: () => ed2curve.convertPublicKey(public_e),
 });
-fc_bench({ group: "convert_secret" }, fc.tuple(fc_bin(32)), {
-  libn: convert_secret,
-  noble: noble_ed25519.utils.toMontgomerySecret,
-  stablelib: stablelib_ed25519.convertSecretKeyToX25519,
-  tweetnacl: ed2curve.convertSecretKey,
+const [secret_m, public_m] = await set_pair(await get_pair("X"));
+bench({ group: "derive" }, {
+  libn: () => libn.derive(secret_m),
+  noble: () => noble_x25519.getPublicKey(secret_m),
+  stablelib: () => stablelib_x25519.scalarMultBase(secret_m),
+  tweetnacl: () => tweetnacl.scalarMult.base(secret_m),
 });
-fc_bench({ group: "convert_public" }, fc.tuple(fc_bin(32).map(generate)), {
-  libn: convert_public,
-  noble: noble_ed25519.utils.toMontgomery,
-  stablelib: stablelib_ed25519.convertPublicKeyToX25519,
-  tweetnacl: ed2curve.convertPublicKey,
+bench({ group: "exchange" }, {
+  libn: () => libn.exchange(secret_m, public_m),
+  noble: () => noble_x25519.getSharedSecret(secret_m, public_m),
+  stablelib: () => stablelib_x25519.sharedKey(secret_m, public_m),
+  tweetnacl: () => tweetnacl.scalarMult(secret_m, public_m),
 });
