f15eda5d679cf71149aa733f54f4577501a025d0
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-02 00:54:35 -0400

    new: assert bench output

--- lib/src/bench.ts
+++ lib/src/bench.ts
@@ -1,21 +1,31 @@
+import { assertEquals } from "@std/assert";
+
+type Bench = (b: Deno.BenchContext) => any;
 /** Runs benchmarks. */
 export const bench = <
   A extends Omit<Deno.BenchDefinition, "name" | "fn" | "baseline"> & {
     group: string;
   },
->(group: A, $: {
+>(group: A & { assert?: boolean }, $: {
   [name: string]:
-    | Deno.BenchDefinition["fn"]
+    | ((b: Deno.BenchContext) => any)
     | Omit<Deno.BenchDefinition, "name" | keyof A> & {
-      fn: Deno.BenchDefinition["fn"];
+      fn: (b: Deno.BenchContext) => any;
     };
 }): void => {
   if (Deno.args.length && !Deno.args.includes(group.group)) return;
+  let first = true, result: any;
   for (const [key, value] of Object.entries($)) {
-    Deno.bench({
-      name: key,
-      ...group,
-      ...(typeof value === "function" ? { fn: value } : value),
+    const { fn, ...rest } = typeof value === "function" ? { fn: value } : value;
+    Deno.bench({ name: key, ...group, ...rest }, async (b) => {
+      const actual = await fn(b);
+      try {
+        b.end();
+      } catch (thrown) {
+        if (!(thrown instanceof TypeError)) throw thrown;
+      }
+      if (group.assert === false || first) result = actual;
+      else assertEquals(actual, result);
     });
   }
 };
