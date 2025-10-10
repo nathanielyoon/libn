c479b24edfad43478911a288e0c473b81a04fcb9
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-10 14:12:25 -0400

    new: add vectors and simplify test structure

--- base/test.ts
+++ base/test.ts
@@ -1,7 +1,6 @@
-import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect/expect";
 import fc from "fast-check";
-import { type Decode, deUtf8, type Encode, enUtf8 } from "@libn/base";
+import { deUtf8, enUtf8 } from "@libn/base";
 import { B16, deB16, enB16 } from "@libn/base/16";
 import {
   B32,
@@ -17,154 +16,241 @@ import {
 import { B64, deB64, deU64, enB64, enU64, U64 } from "@libn/base/64";
 import { A85, deA85, deZ85, enA85, enZ85, Z85 } from "@libn/base/85";
 
-Deno.test("spec", async () => {
+Deno.test("vectors", async (t) => {
   const vectors = await import("./vectors.json", { with: { type: "json" } });
-  for (const $ of vectors.default.b16) {
-    expect(enB16(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deB16($.string)).toStrictEqual(enUtf8($.binary));
-  }
-  for (const $ of vectors.default.b32) {
-    expect(enB32(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deB32($.string)).toStrictEqual(enUtf8($.binary));
-  }
-  for (const $ of vectors.default.h32) {
-    expect(enH32(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deH32($.string)).toStrictEqual(enUtf8($.binary));
-  }
-  for (const $ of vectors.default.b64) {
-    expect(enB64(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deB64($.string)).toStrictEqual(enUtf8($.binary));
-  }
-  for (const $ of vectors.default.u64) {
-    expect(enU64(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deU64($.string)).toStrictEqual(enUtf8($.binary));
-  }
-  for (const $ of vectors.default.z85) {
-    expect(enZ85(deB16($.binary))).toStrictEqual($.string);
-    expect(deZ85($.string)).toStrictEqual(deB16($.binary));
-  }
-  for (const $ of vectors.default.a85) {
-    expect(enA85(enUtf8($.binary))).toStrictEqual($.string);
-    expect(deA85($.string)).toStrictEqual(enUtf8($.binary));
-  }
+  await t.step("B16", () =>
+    vectors.default.B16.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enB16(binary)).toStrictEqual($.string);
+      expect(deB16($.string)).toStrictEqual(binary);
+    }));
+  await t.step("B32", () =>
+    vectors.default.B32.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enB32(binary)).toStrictEqual($.string);
+      expect(deB32($.string)).toStrictEqual(binary);
+    }));
+  await t.step("H32", () =>
+    vectors.default.H32.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enH32(binary)).toStrictEqual($.string);
+      expect(deH32($.string)).toStrictEqual(binary);
+    }));
+  await t.step("C32", () =>
+    vectors.default.C32.forEach(($) => {
+      const binary = deB16($.binary);
+      expect(enC32(binary)).toStrictEqual($.string);
+      expect(deC32($.string)).toStrictEqual(binary);
+    }));
+  await t.step("B64", () =>
+    vectors.default.B64.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enB64(binary)).toStrictEqual($.string);
+      expect(deB64($.string)).toStrictEqual(binary);
+    }));
+  await t.step("U64", () =>
+    vectors.default.U64.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enU64(binary)).toStrictEqual($.string);
+      expect(deU64($.string)).toStrictEqual(binary);
+    }));
+  await t.step("Z85", () =>
+    vectors.default.Z85.forEach(($) => {
+      const binary = deB16($.binary);
+      expect(enZ85(binary)).toStrictEqual($.string);
+      expect(deZ85($.string)).toStrictEqual(binary);
+    }));
+  await t.step("A85", () =>
+    vectors.default.A85.forEach(($) => {
+      const binary = enUtf8($.binary);
+      expect(enA85(binary)).toStrictEqual($.string);
+      expect(deA85($.string)).toStrictEqual(binary);
+    }));
 });
-const roundTrip = <A, B>(
-  encode: ($: A) => B,
-  decode: ($: B) => A,
-  arbitrary: fc.Arbitrary<A>,
-) =>
-  fc.assert(fc.property(arbitrary, ($) => {
-    expect(decode(encode($))).toStrictEqual($);
-  }));
-const encodeMatch = (encode: Encode, pattern: RegExp) =>
+const fcInvalid = ($: RegExp) =>
+  fc.stringMatching(RegExp($.source.replaceAll("[", "[^")));
+Deno.test("deB16(enB16()) round-trips", () =>
   fc.assert(fc.property(fc.uint8Array(), ($) => {
-    expect(encode($)).toMatch(pattern);
-  }));
-const invalid = (decode: Decode, pattern: RegExp) =>
+    expect(deB16(enB16($))).toStrictEqual($);
+  })));
+Deno.test("enB16() returns string matching B16 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enB16($)).toMatch(B16);
+  })));
+Deno.test("enB16() follows built-in toHex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enB16($)).toStrictEqual($.toHex());
+  })));
+Deno.test("deB16() follows built-in fromHex", () =>
+  fc.assert(fc.property(fc.stringMatching(B16), ($) => {
+    expect(deB16($)).toStrictEqual(Uint8Array.fromHex($));
+  })));
+Deno.test("deB16() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(B16), ($) => {
+    expect(deB16($)).toStrictEqual(new Uint8Array($.length >> 1));
+  })));
+Deno.test("deB16() skips last character of odd-length strings", () =>
   fc.assert(fc.property(
-    fc.stringMatching(RegExp(pattern.source.replaceAll("[", "[^"))),
-    ($) => {
-      const binary = decode($);
-      expect(binary).toStrictEqual(new Uint8Array(binary.length));
+    fc.stringMatching(B16),
+    fc.string({ minLength: 1, maxLength: 1 }),
+    ($, extra) => {
+      expect(deB16($ + extra)).toStrictEqual(deB16($));
     },
-  ));
-describe("16", () => {
-  roundTrip(enB16, deB16, fc.uint8Array());
-  it("encodeB16() :: built-in toHex", () =>
-    fc.assert(fc.property(fc.uint8Array(), ($) => {
-      expect(enB16($)).toStrictEqual($.toHex());
-    })));
-  it("encodeB16() matches B16 regex", () => encodeMatch(enB16, B16));
-  it("decodeB16() :: built-in fromHex", () =>
-    fc.assert(fc.property(fc.stringMatching(B16), ($) => {
-      expect(deB16($)).toStrictEqual(Uint8Array.fromHex($));
-    })));
-  it("decodeB16() ignores invalid characters", () => invalid(deB16, B16));
-  it("decodeB16(odd-length) skips trailing character", () =>
-    fc.assert(fc.property(fc.stringMatching(B16), ($) => {
-      const base = deB16($);
-      for (let z = 0; z < 16; ++z) {
-        expect(deB16($ + z.toString(16))).toStrictEqual(base);
-      }
-    })));
+  )));
+Deno.test("deB32(enB32()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deB32(enB32($))).toStrictEqual($);
+  })));
+Deno.test("enB32() returns string matching B32 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enB32($)).toMatch(B32);
+  })));
+Deno.test("deB32() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(B32), ($) => {
+    expect(deB32($)).toStrictEqual(new Uint8Array($.length * 5 >> 3));
+  })));
+Deno.test("deH32(enH32()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deH32(enH32($))).toStrictEqual($);
+  })));
+Deno.test("enH32() returns string matching H32 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enH32($)).toMatch(H32);
+  })));
+Deno.test("deH32() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(H32), ($) => {
+    expect(deH32($)).toStrictEqual(new Uint8Array($.length * 5 >> 3));
+  })));
+Deno.test("deC32(enC32()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deC32(enC32($))).toStrictEqual($);
+  })));
+Deno.test("enC32() returns string matching C32 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enC32($)).toMatch(C32);
+  })));
+Deno.test("deC32() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(C32), ($) => {
+    expect(deC32($)).toStrictEqual(new Uint8Array($.length * 5 >> 3));
+  })));
+Deno.test("deC32() accepts similar characters", () => {
+  fc.assert(fc.property(fc.stringMatching(/^[\dA-HJKMNP-TV-Z]$/), ($) => {
+    const base = deC32($);
+    expect(deC32($.toLowerCase())).toStrictEqual(base);
+    for (const wrong of "Oo") {
+      expect(deC32($.replaceAll("0", wrong))).toStrictEqual(base);
+    }
+    for (const wrong of "IiLl") {
+      expect(deC32($.replaceAll("1", wrong))).toStrictEqual(base);
+    }
+  }));
 });
-describe("32", () => {
-  roundTrip(enB32, deB32, fc.uint8Array());
-  it("encodeB32() matches B32 regex", () => encodeMatch(enB32, B32));
-  it("decodeB32() ignores invalid characters", () => invalid(deB32, B32));
-  roundTrip(enH32, deH32, fc.uint8Array());
-  it("encodeH32() matches H32 regex", () => encodeMatch(enH32, H32));
-  it("decodeH32() ignores invalid characters", () => invalid(deH32, H32));
-  roundTrip(enC32, deC32, fc.uint8Array());
-  it("encodeC32() matches C32 regex", () => encodeMatch(enC32, C32));
-  it("decodeC32() ignores invalid characters", () => invalid(deC32, C32));
-});
-describe("64", () => {
-  roundTrip(enB64, deB64, fc.uint8Array());
-  it("encodeB64() :: built-in toBase64", () =>
-    fc.assert(fc.property(fc.uint8Array(), ($) => {
-      expect(enB64($)).toStrictEqual($.toBase64());
-    })));
-  it("encodeB64() matches B64 regex", () => encodeMatch(enB64, B64));
-  it("decodeB64() :: built-in fromBase64", () =>
-    fc.assert(fc.property(fc.stringMatching(B64), ($) => {
-      expect(deB64($)).toStrictEqual(Uint8Array.fromBase64($));
-    })));
-  it("decodeB64() ignores invalid characters", () => invalid(deB64, B64));
-  roundTrip(enU64, deU64, fc.uint8Array());
-  it("encodeU64() :: built-in toBase64", () =>
-    fc.assert(fc.property(fc.uint8Array(), ($) => {
-      expect(enU64($)).toStrictEqual(
-        $.toBase64({ alphabet: "base64url", omitPadding: true }),
-      );
-    })));
-  it("encodeU64() matches U64 regex", () => encodeMatch(enU64, U64));
-  it("decodeU64() :: built-in fromBase64", () =>
-    fc.assert(fc.property(fc.stringMatching(U64), ($) => {
-      expect(deU64($)).toStrictEqual(
-        Uint8Array.fromBase64($, { alphabet: "base64url" }),
-      );
-    })));
-  it("decodeU64() ignores invalid characters", () => invalid(deU64, U64));
-});
-describe("85", () => {
-  const fcStep4 = fc.integer({ min: 0, max: 1e3 }).map(($) => $ >> 2 << 2);
-  roundTrip(
-    enZ85,
-    deZ85,
+Deno.test("deC32() allows hyphens", () =>
+  fc.assert(fc.property(fc.stringMatching(C32), ($) => {
+    expect(deC32($)).toStrictEqual(deC32($.replaceAll("-", "")));
+  })));
+Deno.test("deB64(enB64()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deB64(enB64($))).toStrictEqual($);
+  })));
+Deno.test("enB64() returns string matching B64 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enB64($)).toMatch(B64);
+  })));
+Deno.test("enB64() follows built-in toBase64", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enB64($)).toStrictEqual($.toBase64());
+  })));
+Deno.test("deB64() follows built-in fromBase64", () =>
+  fc.assert(fc.property(fc.stringMatching(B64), ($) => {
+    expect(deB64($)).toStrictEqual(Uint8Array.fromBase64($));
+  })));
+Deno.test("deB64() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(B64), ($) => {
+    expect(deB64($)).toStrictEqual(new Uint8Array($.length * 3 >> 2));
+  })));
+Deno.test("deU64(enU64()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deU64(enU64($))).toStrictEqual($);
+  })));
+Deno.test("enU64() returns string matching U64 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enU64($)).toMatch(U64);
+  })));
+Deno.test("enU64() follows built-in toBase64", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enU64($)).toStrictEqual(
+      $.toBase64({ alphabet: "base64url", omitPadding: true }),
+    );
+  })));
+Deno.test("deU64() follows built-in fromBase64", () =>
+  fc.assert(fc.property(fc.stringMatching(U64), ($) => {
+    expect(deU64($)).toStrictEqual(
+      Uint8Array.fromBase64($, { alphabet: "base64url" }),
+    );
+  })));
+Deno.test("deU64() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(U64), ($) => {
+    expect(deU64($)).toStrictEqual(new Uint8Array($.length * 3 >> 2));
+  })));
+const fcStep4 = fc.integer({ min: 0, max: 1e3 }).map(($) => $ >> 2 << 2);
+Deno.test("deZ85(enZ85()) round-trips", () =>
+  fc.assert(fc.property(
     fcStep4.chain(($) => fc.uint8Array({ minLength: $, maxLength: $ })),
-  );
-  it("encodeZ85() matches Z85 regex", () => encodeMatch(enZ85, Z85));
-  it("decodeZ85() ignores invalid characters", () => invalid(deZ85, Z85));
-  roundTrip(enA85, deA85, fc.uint8Array());
-  it("encodeA85() matches A85 regex", () => encodeMatch(enA85, A85));
-  it("decodeA85() ignores invalid characters", () => invalid(deA85, A85));
-  it('encodeA85() replaces all-zero chunks with "z"', () =>
-    fc.assert(fc.property(fcStep4.map(($) => new Uint8Array($)), ($) => {
-      expect(enA85($)).toMatch(RegExp(`^z{${$.length >> 2}}$`));
-    })));
-});
-describe("mod", () => {
-  it("encodeUtf8() :: independent instantiation/call", () =>
-    fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
-      expect(enUtf8($)).toStrictEqual(new TextEncoder().encode($));
-    })));
-  it("decodeUtf8() :: independent instantiation/call", () =>
-    fc.assert(fc.property(fc.uint8Array(), ($) => {
-      expect(deUtf8($)).toStrictEqual(new TextDecoder().decode($));
-    })));
-});
+    ($) => {
+      expect(deZ85(enZ85($))).toStrictEqual($);
+    },
+  )));
+Deno.test("enZ85() returns string matching Z85 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enZ85($)).toMatch(Z85);
+  })));
+Deno.test("deZ85() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(Z85), ($) => {
+    expect(deZ85($)).toStrictEqual(new Uint8Array(Math.ceil($.length / 5 * 4)));
+  })));
+Deno.test("deA85(enA85()) round-trips", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deA85(enA85($))).toStrictEqual($);
+  })));
+Deno.test("enA85() returns string matching A85 regex", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(enA85($)).toMatch(A85);
+  })));
+Deno.test("enA85() replaces all-zero chunks", () =>
+  fc.assert(fc.property(fcStep4.map(($) => new Uint8Array($)), ($) => {
+    expect(enA85($)).toStrictEqual("z".repeat($.length >> 2));
+  })));
+Deno.test("deA85() ignores invalid characters", () =>
+  fc.assert(fc.property(fcInvalid(A85), ($) => {
+    expect(deA85($)).toStrictEqual(
+      new Uint8Array(
+        $.replace(/\s+/g, "").replaceAll("z", "!!!!!").length / 5 * 4,
+      ),
+    );
+  })));
+Deno.test("encodeUtf8() follows independent instantiation/call", () =>
+  fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
+    expect(enUtf8($)).toStrictEqual(new TextEncoder().encode($));
+  })));
+Deno.test("decodeUtf8() follows independent instantiation/call", () =>
+  fc.assert(fc.property(fc.uint8Array(), ($) => {
+    expect(deUtf8($)).toStrictEqual(new TextDecoder().decode($));
+  })));
 import.meta.main && await Promise.all([
   fetch(
     "https://www.rfc-editor.org/rfc/rfc4648.txt",
   ).then(($) => $.text()).then(($) => $.slice(25691, 26723)),
+  fetch(
+    "https://crockford.com/base32.html",
+  ).then(($) => $.text()).then(($) => $.slice(2219, 5211)),
   fetch(
     "https://raw.githubusercontent.com/zeromq/rfc/3d4c0cef87ed761fe09ab9abf8a6e5ea45df0e9f/src/spec_32.c",
   ).then(($) => $.text()).then(($) => $.slice(4717, 5975)),
   fetch(
     "https://en.wikipedia.org/w/index.php?title=Ascii85&oldid=1305034107",
   ).then(($) => $.text()),
-]).then(([rfc4648, spec32, wikipedia]) => {
+]).then(([rfc4648, crockford, spec32, wikipedia]) => {
   const matchRfc = (base: string, map: (string: string) => string) =>
     Array.from(
       rfc4648.matchAll(RegExp(`BASE${base}\\("(.*)"\\) = "(.*)"`, "g")),
@@ -174,12 +260,24 @@ import.meta.main && await Promise.all([
       }),
     );
   return {
-    b16: matchRfc("16", ($) => $.toLowerCase()),
-    b32: matchRfc("32", ($) => $.replace(/=+$/, "")),
-    h32: matchRfc("32-HEX", ($) => $.replace(/=+$/, "")),
-    b64: matchRfc("64", ($) => $),
-    u64: matchRfc("64", ($) => $.replace(/=+$/, "")),
-    z85: Array.from([1, 2], ($) => {
+    B16: matchRfc("16", ($) => $.toLowerCase()),
+    B32: matchRfc("32", ($) => $.replace(/=+$/, "")),
+    H32: matchRfc("32-HEX", ($) => $.replace(/=+$/, "")),
+    C32: Array.from(
+      crockford.matchAll(
+        /<tr>.*?<td>(\d+)<\/td>.*?<td><code>[\s\dA-Za-z]+<\/code><\/td>.*?<td><code>([\dA-Z])<\/code><\/td>.*?<\/tr>/gs,
+      ),
+      ([_, value, encode]) => {
+        let total = 0;
+        for (let base = +value, z = 0; z < 8; ++z) total = total * 32 + base;
+        const binary = new Uint8Array(8);
+        new DataView(binary.buffer).setBigUint64(0, BigInt(total));
+        return { binary: enB16(binary.subarray(3)), string: encode.repeat(8) };
+      },
+    ),
+    B64: matchRfc("64", ($) => $),
+    U64: matchRfc("64", ($) => $.replace(/=+$/, "")),
+    Z85: Array.from([1, 2], ($) => {
       const [_, bytes, string] = RegExp(
         `byte test_data_${$} \\[\\d+\\] = \\{(.+?)\\};.*?encoded = Z85_encode \\(test_data_${$}.*?assert \\(streq \\(encoded, "(.+?)"\\)\\)`,
         "s",
@@ -189,7 +287,7 @@ import.meta.main && await Promise.all([
         string,
       };
     }),
-    a85: [{
+    A85: [{
       binary: /(?<=<code>).{269}(?=<\/code>)/s.exec(wikipedia)![0],
       string: /(?<=<pre>)(.{395})(?=<\/pre>)/s.exec(wikipedia)![0]
         .replaceAll("&lt;", "<").replaceAll("&gt;", ">")
