00b7a2f8323c3ad69aec22e0e821328338d42e36
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-12 17:13:33 -0400

    fix: add explicit type

--- text/lib.ts
+++ text/lib.ts
@@ -3,4 +3,5 @@ export const enPoint: (this: string, index?: number) => number = /* @__PURE__ */
   (() =>
     String.prototype.codePointAt as (this: string, index?: number) => number)();
 /** Code point setter. */
-export const dePoint = /* @__PURE__ */ (() => String.fromCodePoint)();
+export const dePoint: (...codePoints: number[]) => string =
+  /* @__PURE__ */ (() => String.fromCodePoint)();
