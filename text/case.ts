5a4e810f3b41fe9ba543b19784f681449d0a8420
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-14 06:36:54 -0400

    fix: move range creation back in

--- text/case.ts
+++ text/case.ts
@@ -1,6 +1,19 @@
 import { dePoint, enPoint } from "./lib.ts";
 import ranges from "./ranges.json" with { type: "json" };
 
+/** Creates a case-folding range set from the Unicode source text. */
+export const createRanges = (text: string): string => {
+  let head = -1;
+  return text.match(/^.{4,5}; [CF];(?: [\dA-F]{4,5})+/gm)!.reduce((out, $) => {
+    const line = $.split(/; [CF]; /), to = line[1].split(" ");
+    let next = "";
+    for (let y = 0; y < to.length; ++y) next += dePoint(parseInt(to[y], 16));
+    const code = parseInt(line[0], 16), prev = out[out.length - 1];
+    if (code < head + prev?.length) prev.push(next);
+    else out.push([dePoint(head = code), next]);
+    return out;
+  }, [] as string[][]).map(($) => $.join(" ")).join(",");
+};
 const replace = /* @__PURE__ */ (() => String.prototype.replace)();
 const args = /* @__PURE__ */ ((): Parameters<typeof replace> => {
   const hex = ($: number) => {
