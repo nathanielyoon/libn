977401713280516aac6cfc2c74c5baf3b9544e29
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-15 17:04:44 -0400

    fix: simplify significantly

--- fp/or.ts
+++ fp/or.ts
@@ -1,22 +1,20 @@
 /** Failure/success discriminated union. */
 export type Or<A = any, B = any> =
-  | { state: false; value: A }
-  | { state: true; value: B };
+  & ({ is: false; of: A } | { is: true; of: B })
+  & { [Symbol.iterator](): Generator<Or<A, B>, B, any> };
 /** Failure value. */
-export type No<A extends Or> = Extract<A, { state: false }>["value"];
+export type No<A extends Or> = Extract<A, { is: false }>["of"];
 /** Success value. */
-export type Ok<A extends Or> = Extract<A, { state: true }>["value"];
-/** Creates a failure. */
-export const no = <const A = undefined>($?: A): Or<A, never> => (
-  { state: false, value: $! }
-);
+export type Ok<A extends Or> = Extract<A, { is: true }>["of"];
+/** Generator function to bind. */
+export function* generate<A, B>(this: Or<A, B>): Generator<Or<A, B>, B, any> {
+  return yield this;
+}
 /** Creates a success. */
-export const ok = <const A = undefined>($?: A): Or<never, A> => (
-  { state: true, value: $! }
+export const no = <const A>($: A): Or<A, never> => (
+  { is: false, of: $, [Symbol.iterator]: generate }
 );
-/** Falsy values (except `NaN`, which isn't representable). */
-export type Falsy = undefined | null | false | 0 | 0n | "";
-/** Creates a failure (if falsy) or a success (if truthy). */
-export const or = <const A>($: A): Or<Extract<Falsy, A>, Exclude<A, Falsy>> => (
-  { state: Boolean($), value: $ } as Or
+/** Creates a failure. */
+export const ok = <const A>($: A): Or<never, A> => (
+  { is: true, of: $, [Symbol.iterator]: generate }
 );
--- fp/wrap.ts
+++ fp/wrap.ts
@@ -1,112 +1,54 @@
-import { type Falsy, no, ok, type Or } from "./or.ts";
-import { type Both, Result, type Sync } from "./result.ts";
+import { no, type Ok, ok, type Or } from "./or.ts";
 
-/** Does nothing. */
-export const pass = <const A>($: A) => $;
-/** Lifts a value into a non-nullable success or nullish failure. */
-export const some = <const A, const B = undefined>(
-  $: Sync<A>,
-  ifNullish?: Sync<B>,
-): Result<B, NonNullable<A>, false> =>
-  // deno-lint-ignore eqeqeq
-  $ == null ? Result.no(ifNullish!) : Result.ok($);
-const error = ($: unknown) => $ instanceof Error ? $ : Error("", { cause: $ });
-/** Wraps an unsafe function. */
+/** Aggregates results. */
+export const join = <const A extends [Or, ...Or[]]>(
+  $: A,
+): Or<A, { [B in keyof A]: Ok<A[B]> }> => {
+  const oks = Array($.length) as { [B in keyof A]: Ok<A[B]> };
+  let z = 0;
+  do if ($[z].is) oks[z] = $[z].of;
+  else return no($); while (++z < $.length);
+  return ok(oks);
+};
+const sync = <A, B, C = never>(
+  $: A | Promise<A>,
+  use: ($: A) => B | Promise<B>,
+  or?: ($: unknown) => C,
+) => $ instanceof Promise ? $.then(use).catch(or) : use($);
+/** Try-catches a possibly-throwing function. */
 export const safe =
-  ((unsafe: ($: any) => any, ifThrown?: ($: any, thrown: any) => any) =>
+  ((unsafe: ($: any) => any, or?: ($: any, thrown: unknown) => any) =>
   ($: any) => {
     try {
-      const next = unsafe($);
-      if (next instanceof Promise) {
-        return Result.or(
-          next.then(ok).catch(async (cause) =>
-            no(ifThrown ? await ifThrown($, cause) : error(cause))
-          ),
-        );
-      }
-      return Result.ok(next);
+      return sync(unsafe($), ok, async (cause) => {
+        if (or) return no(await or($, cause));
+        return no(cause instanceof Error ? cause : Error("", { cause }));
+      });
     } catch (cause) {
-      return Result.no(ifThrown ? ifThrown($, cause) : error(cause));
+      if (or) return sync(or($, cause), no);
+      return no(cause instanceof Error ? cause : Error("", { cause }));
     }
   }) as {
     <A, B, C = Error>(
-      unsafe: ($: A) => Sync<B>,
-      ifThrown?: ($: A, thrown: unknown) => Sync<C>,
-    ): ($: A) => Result<C, B, false>;
+      unsafe: B extends Promise<any> ? never : ($: A) => B,
+      or?: C extends Promise<any> ? never : ($: A, cause: unknown) => C,
+    ): ($: A) => Or<C, B>;
     <A, B, C = Error>(
       unsafe: ($: A) => Promise<B>,
-      ifThrown?: ($: A, thrown: unknown) => Both<C>,
-    ): ($: A) => Result<C, B, true>;
-    <A, B, C>(
-      unsafe: ($: A) => Sync<B>,
-      ifThrown: ($: A, thrown: unknown) => Both<C>,
-    ): ($: A) => Result<C, B, boolean>;
+      or?: ($: A, cause: unknown) => C | Promise<C>,
+    ): ($: A) => Promise<Or<C, B>>;
   };
-const every = ($: Or[]) => { // empty lists returned early, so `$.length >= 1`
-  let z = $.length;
-  const out = Array(z);
-  do if ($[--z].state) out[z] = $[z].value;
-  else return no($); while (z);
-  return ok(out);
-};
-/** Combines a list of results. */
-export const join = (($: Result[]) => {
-  const all = Array($.length);
-  if (!$.length) return Result.ok(all);
-  // Since the async-ness of the returned result depends on the async-ness of
-  // the list elements, results from empty lists are always sync, and results
-  // from non-tuples (i.e. without a known length) are sync if all their members
-  // would've been and indeterminate otherwise.
-  let async = false;
-  for (let z = 0; z < $.length; ++z) {
-    all[z] = $[z].either, async ||= all[z] instanceof Promise;
-  }
-  return Result.or(async ? Promise.all(all).then(every) : every(all));
+/** Runs an imperative block, returning failures early. */
+export const exec = ((block: () => Generator<Or> | AsyncGenerator<Or>) => {
+  const generator = block();
+  const next = ($?: any): Or | Promise<Or> =>
+    sync(generator.next($), ({ done, value }): Or | Promise<Or> => {
+      if (done) return ok(value);
+      else if (value.is) return next(value.of);
+      return value;
+    });
+  return next();
 }) as {
-  ($: []): Result<never, [], false>;
-  <const A extends [Result, ...Result[]]>($: A): Result<
-    { [B in keyof A]: Awaited<A[B]["either"]> },
-    { [B in keyof A]: A[B] extends Result<any, infer C> ? C : never },
-    A[number] extends Result<any, any, infer B> ? B : never
-  >;
-  <A extends Result>($: A[]): Result<
-    Awaited<A["either"]>[],
-    A extends Result<any, infer B> ? B[] : never,
-    A extends Result<any, any, infer B> ? B | false : never
-  >;
+  <A, B, C>(block: () => Generator<Or<A, B>, C, B>): Or<A, C>;
+  <A, B, C>(block: () => AsyncGenerator<Or<A, B>, C, B>): Promise<Or<A, C>>;
 };
-/** Wraps a guard that returns an error, or something falsy when successful. */
-export const drop = ((not: ($: any) => Both<any>) => ($: any) => {
-  const next = not($);
-  if (next instanceof Promise) {
-    return Result.or(next.then((error) => error ? no(error) : ok($)));
-  } else if (next) return Result.no(next);
-  return Result.ok($);
-}) as {
-  <A, B>(not: ($: A) => Sync<Falsy | B>): ($: A) => Result<B, A, false>;
-  <A, B>(not: ($: A) => Promise<Falsy | B>): ($: A) => Result<B, A, true>;
-};
-/** Wraps an imperative block. */
-export const exec =
-  (<A>(run: ($: A) => AsyncGenerator<Result> | Generator<Result>) => ($: A) => {
-    const generator = run($);
-    return (function get(argument?: any): Both<Result> {
-      const result = generator.next(argument);
-      if (result instanceof Promise) {
-        return result.then(async (next) => {
-          if (next.done) return Result.ok(await next.value);
-          const { state, value } = await next.value.either;
-          if (state) return get(value);
-          return Result.no(value);
-        });
-      } else if (result.done) return Result.ok(result.value);
-      return result.value.bind(get);
-    })();
-  }) as {
-    <A, B, C, D extends boolean, E>(
-      block: ($: A) => Generator<Result<B, C, D>, E, C>,
-    ): ($: A) => Result<B, E, D>;
-    <A, B, C, D>(
-      block: ($: A) => AsyncGenerator<Result<B, C, boolean>, D, C>,
-    ): ($: A) => Promise<Result<B, D, false>>;
-  };
