3e5b854e12df0fc866830ddc40d79ed467ad9210
Nathaniel Yoon <nathanielyoon1@gmail.com>
2025-10-20 14:41:16 -0400

    fix: clean up declarations

--- json/compile.ts
+++ json/compile.ts
@@ -4,11 +4,9 @@ import { enToken, type Pointer } from "./pointer.ts";
 import { ENCODING, FORMAT } from "./regex.ts";
 import type { Instance, Schema } from "./schema.ts";
 
-const isSchema: {
-  [A in keyof Type]: ($: unknown) => $ is { type: A | [A, "null"] };
-} = ["boolean", "integer", "number", "string", "array", "object"].reduce<any>(
+const is = ["boolean", "integer", "number", "string", "array", "object"].reduce(
   (to, type) => ({ ...to, [type]: ($: Schema) => $.type[0][0] === type[0] }),
-  {},
+  {} as { [A in keyof Type]: ($: unknown) => $ is { type: A | [A, "null"] } },
 );
 const wrap = (
   $: TemplateStringsArray,
@@ -19,12 +17,12 @@ const regex = (pattern: string) => {
   try {
     return RegExp(pattern);
   } catch {
-    return "a\\bc"; // never-matching pattern
+    return /a\bc/; // never-matching pattern
   }
 };
 const body = ($: Schema) => {
   if ("enum" in $) {
-    if (!isSchema.string($)) {
+    if (!is.string($)) {
       return wrap`${
         [...new Set<Json>($.enum)].reduce<string>((body, option) =>
           `${body}case ${JSON.stringify(option)}:`, "switch(U){")
@@ -34,10 +32,10 @@ const body = ($: Schema) => {
     for (const option of $.enum) union += unrexp(JSON.stringify(option)) + "|";
     return wrap`${union.slice(0, -1)}$/.test(U)||${"enum"}`;
   }
-  let all = `if(`;
-  if (isSchema.boolean($)) all += 'typeof U!=="boolean")';
-  else if (isSchema.integer($) || isSchema.number($)) {
-    all += `Number.is${isSchema.integer($) ? "SafeInteger" : "Finite"}(U)){`;
+  let all = "if(";
+  if (is.boolean($)) all += 'typeof U!=="boolean")';
+  else if (is.integer($) || is.number($)) {
+    all += `Number.is${is.integer($) ? "SafeInteger" : "Finite"}(U)){`;
     if ($.minimum !== undefined) all += wrap`U<${$.minimum}&&${"minimum"}`;
     if ($.maximum !== undefined) all += wrap`U>${$.maximum}&&${"minimum"}`;
     if ($.exclusiveMinimum !== undefined) {
@@ -50,7 +48,7 @@ const body = ($: Schema) => {
     // `U % 0`, which is always falsy.
     if ($.multipleOf) all += wrap`U%${$.multipleOf}&&${"multipleOf"}`;
     all += "}else";
-  } else if (isSchema.string($)) {
+  } else if (is.string($)) {
     all += 'typeof U==="string"){';
     if ($.minLength && $.minLength > 0) {
       all += wrap`U.length<${$.minLength}&&${"minLength"}`;
@@ -67,7 +65,7 @@ const body = ($: Schema) => {
       all += wrap`${ENCODING[$.contentEncoding]}.test(U)||${"contentEncoding"}`;
     }
     all += "}else";
-  } else if (isSchema.array($)) {
+  } else if (is.array($)) {
     all += "Array.isArray(U)){";
     if ($.minItems && $.minItems > 0) {
       all += wrap`U.length<${$.minItems}&&${"minItems"}`;
@@ -89,7 +87,7 @@ const body = ($: Schema) => {
       }
     }
     all += "}else";
-  } else if (isSchema.object($)) {
+  } else if (is.object($)) {
     all +=
       'typeof U==="object"&&U!==null&&!Array.isArray(U)){const s=S,i=I,u=U,k=Object.keys(u);';
     if ($.minProperties && $.minProperties > 0) {
@@ -119,11 +117,10 @@ const body = ($: Schema) => {
     }
     all += "}else";
   }
-  return wrap`${all + (isArray($.type) ? " U===null||" : "")}${"type"}`;
+  return all + wrap`${isArray($.type) ? " U===null||" : ""}${"type"}`;
 };
 /** Compiles a checking generator function from a schema. */
 export const compile = <A extends Schema>(schema: A): ($: unknown) => Generator<
   Pointer<A>,
-  Instance<A>,
-  undefined
+  Instance<A>
 > => Function(`return function*(U){const S="",I="";${body(schema)}return U}`)();
