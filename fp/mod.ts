7642da552e01fa32f815bc4826adc8300543bf5b
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-10-23 13:46:47 -0400

    doc: add module doc comment

--- fp/mod.ts
+++ fp/mod.ts
@@ -1,2 +1,34 @@
-export { fail, type No, type Ok, pass, type Result } from "./result.ts";
+/**
+ * Functional result handling.
+ *
+ * @example Typed do-syntax block
+ * ```ts
+ * import { some } from "@libn/fp/result";
+ * import { exec, safe } from "@libn/fp/wrap";
+ * import { assertEquals } from "@std/assert";
+ *
+ * const ok = Math.random() > 0.5;
+ * assertEquals(
+ *   exec(function* () {
+ *     return yield* safe(() => {
+ *       if (ok) return ok;
+ *       throw ok;
+ *     }, () => ok)();
+ *   }),
+ *   some(ok),
+ * );
+ * ```
+ *
+ * @module fp
+ */
+
+export {
+  fail,
+  type Falsy,
+  type No,
+  type Ok,
+  pass,
+  type Result,
+  some,
+} from "./result.ts";
 export { exec, join, safe } from "./wrap.ts";
