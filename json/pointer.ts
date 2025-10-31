5b04bfbdc54c5a402051fc7d3d92a128fb7f3ff5
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-10-31 10:36:12 -0400

    fix: optimize pointer inference

--- json/pointer.ts
+++ json/pointer.ts
@@ -15,36 +15,51 @@ export const enToken = <A extends string>($: A) =>
 export const deToken = <A extends string>($: A) =>
   $.replaceAll("~1", "/").replaceAll("~0", "~") as DeToken<A>;
 /** Error indicator. */
-export type Pointer<A extends Schema, B extends [string, string] = ["", ""]> = {
-  [C in keyof A]: [C, A[C]] extends ["items", infer D extends Schema]
-    ? Pointer<D, [`${B[0]}/items`, `${B[1]}/${number}`]>
-    : [C, A[C]] extends ["prefixItems", infer D extends readonly Schema[]] ? {
-        [E in keyof D]: Pointer<
-          D[E],
-          [`${B[0]}/prefixItems/${E}`, `${B[1]}/${E}`]
-        >;
-      }[keyof D]
-    : [C, A[C]] extends [
-      infer E extends "additionalProperties" | "propertyNames",
-      infer D extends Schema,
-    ] ? Pointer<D, [`${B[0]}/${E}`, `${B[1]}/${string}`]>
-    : [C, A[C]] extends ["properties", infer D extends { [_: string]: Schema }]
-      ? {
-        [E in keyof D]: Pointer<
-          D[E],
-          [`${B[0]}/properties/${EnToken<E>}`, `${B[1]}/${EnToken<E>}`]
-        >;
-      }[keyof D]
-    : [C, A[C]] extends ["required", infer D extends readonly string[]] ? {
-        [E in keyof D]: `${B[0]}/required/${E}~${B[1]}/${EnToken<D[E]>}`;
-      }[keyof D]
-    : [C, A[C]] extends ["oneOf", infer D extends readonly Schema[]] ?
-        | {
-          [E in keyof D]: Pointer<D[E], [`${B[0]}/oneOf/${E}`, B[1]]>;
-        }[keyof D]
-        | `${B[0]}/oneOf~${B[1]}`
-    : `${B[0]}/${C & string}~${B[1]}`;
-}[keyof A] extends infer C ? C extends string ? C : never : never;
+export type Pointer<
+  A extends Schema,
+  B extends string = "",
+  C extends string = "",
+> = Schema extends A ? `${string}~${string}`
+  : A extends { const: Json } ? `${B}/const~${C}`
+  : A extends { enum: readonly Json[] } ? `${B}/enum~${C}`
+  : A["type"] extends "null" | "boolean" | "integer" | "number" | "string"
+    ? `${B}/${Extract<keyof A, string>}~${C}`
+  : A["type"] extends "array" ?
+      | (A extends { minItems: true } ? `${B}/minItems~${C}` : never)
+      | (A extends { maxItems: true } ? `${B}/maxItems~${C}` : never)
+      | (A extends { uniqueItems: true } ? `${B}/uniqueItems~${C}` : never)
+      | ((A extends { prefixItems: infer D extends readonly Schema[] } ?
+          | {
+            [E in keyof D]: Pointer<D[E], `${B}/prefixItems/${E}`, `${C}/${E}`>;
+          }[keyof D]
+          | `${B}/items~${C}`
+        : A extends { items: infer D extends Schema }
+          ? Pointer<D, `${B}/items`, `${C}/number`>
+        : never) extends infer D ? D extends string ? D : never : never)
+  :
+    | (A extends { minProperties: number } ? `${B}/minProperties~${C}` : never)
+    | (A extends { maxProperties: number } ? `${B}/maxProperties~${C}` : never)
+    | ((A extends { oneOf: infer D extends readonly Schema[] } ?
+        | `${B}/required/0~${C}`
+        | { [E in keyof D]: Pointer<D[E], `${B}/oneOf/${E}`, C> }[keyof D]
+      : A extends {
+        properties: infer D extends { [_: string]: Schema };
+        required: infer E extends readonly string[];
+      } ?
+          | `${B}/required/${Extract<keyof E, number>}~${C}`
+          | {
+            [F in keyof D]: Pointer<
+              D[F],
+              `${B}/properties/${EnToken<F>}`,
+              `${C}/${EnToken<F>}`
+            >;
+          }[keyof D]
+      : A extends { additionalProperties: infer D extends Schema } ?
+          | (A extends { propertyNames: infer E extends Schema }
+            ? Pointer<E, `${B}/propertyNames`, `${C}/${string}`>
+            : never)
+          | Pointer<D, `${B}/additionalProperties`, `${C}/${string}`>
+      : never) extends infer D ? D extends string ? D : never : never);
 /** Resolves a JSON pointer. */
 export const dereference = ($: Json, pointer: string): Json | undefined => {
   if (!pointer) return $;
