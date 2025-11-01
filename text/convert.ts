0209cb7e2624d22327967f1f01103607e761c9c8
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-01 15:38:03 -0400

    fix: simplify shared logic

--- text/convert.ts
+++ text/convert.ts
@@ -7,37 +7,33 @@
  *
  * let words = "This is a set of words";
  *
- * assertEquals(words = toLowerCamel(words), "thisIsASetOfWords");
- * assertEquals(words = toUpperCamel(words), "ThisIsASetOfWords");
- * assertEquals(words = toLowerKebab(words), "this-is-a-set-of-words");
- * assertEquals(words = toUpperKebab(words), "This-Is-A-Set-Of-Words");
- * assertEquals(words = toLowerSnake(words), "this_is_a_set_of_words");
- * assertEquals(words = toUpperSnake(words), "This_Is_A_Set_Of_Words");
+ * assertEquals(words = lowerCamel(words), "thisIsASetOfWords");
+ * assertEquals(words = upperCamel(words), "ThisIsASetOfWords");
+ * assertEquals(words = lowerKebab(words), "this-is-a-set-of-words");
+ * assertEquals(words = upperKebab(words), "This-Is-A-Set-Of-Words");
+ * assertEquals(words = lowerSnake(words), "this_is_a_set_of_words");
+ * assertEquals(words = upperSnake(words), "THIS_IS_A_SET_OF_WORDS");
  * ```
  *
  * @module convert
  */
 
 const WORD = /\p{Lu}?\p{Ll}+|\p{N}+|\p{Lu}+(?!\p{Ll})|\p{Lt}\p{Ll}*|\p{L}+/gu;
-const capitalize = ($: string) =>
-  $ && `${$[0].toUpperCase()}${$.slice(1).toLowerCase()}`;
-/** Capitalizes the first letter of all but the first word and concatenates. */
-export const toLowerCamel = ($: string): string => {
-  const [head, ...tail] = $.trim().match(WORD) ?? [];
-  return (head ?? "").toLowerCase() + tail.map(capitalize).join("");
+const up = ([head, ...tail]: string) =>
+  head.toUpperCase() + tail.join("").toLowerCase();
+const all = ($: string) => $.trim().match(WORD) ?? [];
+/** Uppercases the first letter of all but the first word and concatenates. */
+export const lowerCamel = ($: string): string => {
+  const [head, ...tail] = all($);
+  return (head ?? "").toLowerCase() + tail.map(up).join("");
 };
-/** Capitalizes the first letter of all words and concatenates. */
-export const toUpperCamel = ($: string): string =>
-  $.trim().match(WORD)?.map(capitalize).join("") ?? "";
+/** Uppercases the first letter of all words and concatenates. */
+export const upperCamel = ($: string): string => all($).map(up).join("");
 /** Lowercases all characters and joins words with hyphens. */
-export const toLowerKebab = ($: string): string =>
-  $.trim().match(WORD)?.join("-").toLowerCase() ?? "";
+export const lowerKebab = ($: string): string => all($).join("-").toLowerCase();
 /** Uppercases the first letter of each word and joins words with hyphens. */
-export const toUpperKebab = ($: string): string =>
-  $.trim().match(WORD)?.map(capitalize).join("-") ?? "";
+export const upperKebab = ($: string): string => all($).map(up).join("-");
 /** Lowercases all characters and joins words with underscore characters. */
-export const toLowerSnake = ($: string): string =>
-  $.trim().match(WORD)?.join("_").toLowerCase() ?? "";
+export const lowerSnake = ($: string): string => all($).join("_").toLowerCase();
 /** Uppercases all characters and joins words with underscore characters. */
-export const toUpperSnake = ($: string): string =>
-  $.trim().match(WORD)?.join("_").toUpperCase() ?? "";
+export const upperSnake = ($: string): string => all($).join("_").toUpperCase();
