01f87d906946e66c741bc054e0abaad5b8c05559
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-03 11:42:01 -0500

    fix: clarify api

--- base/utf.ts
+++ base/utf.ts
@@ -14,25 +14,25 @@
  * assertEquals(de(...binary), "Hello :)");
  * ```
  *
- * @module utf8
+ * @module utf
  */
 
-/** Character code getter. */
-export const en: (this: string, index?: number) => number =
-  /* @__PURE__ */ (() =>
-    String.prototype.charCodeAt as (this: string, index?: number) => number)();
-/** Character code setter. */
-export const de: (...codes: number[]) => string =
-  /* @__PURE__ */ (() => String.fromCharCode)();
 /** Converts UTF-8 to binary. */
-export const enUtf8: ($?: string) => Uint8Array<ArrayBuffer> =
+export const enUtf8: ($: string) => Uint8Array<ArrayBuffer> =
   /* @__PURE__ */ TextEncoder.prototype.encode.bind(
     /* @__PURE__ */ new TextEncoder(),
   );
 /** Converts binary to UTF-8. */
 export const deUtf8: (
-  $?: AllowSharedBufferSource,
+  $: AllowSharedBufferSource,
   options?: TextDecodeOptions,
 ) => string = /* @__PURE__ */ TextDecoder.prototype.decode.bind(
   /* @__PURE__ */ new TextDecoder("utf-8"),
 );
+/** UTF-16 character code encoder. */
+export const en: (this: string, index?: number) => number =
+  /* @__PURE__ */ (() =>
+    String.prototype.charCodeAt as (this: string, index?: number) => number)();
+/** UTF-16 character code decoder. */
+export const de: (...codes: number[]) => string =
+  /* @__PURE__ */ (() => String.fromCharCode)();
