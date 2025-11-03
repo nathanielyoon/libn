92af98bfa841bf587d73089594a8ecf470178d95
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-03 11:11:30 -0500

    fix: run tests after npm transform

--- npm.ts
+++ npm.ts
@@ -13,7 +13,6 @@ await build({
   shims: { deno: "dev" },
   skipSourceOutput: true,
   typeCheck: false,
-  test: false,
   package: {
     name,
     version,
@@ -24,6 +23,17 @@ await build({
       url: "git+https://github.com/nathanielyoon/libn.git",
     },
   },
+  postBuild: async () => {
+    Promise.all(["esm", "script"].map(($) =>
+      Deno.writeTextFile(
+        `./npm/${$}/test.js`,
+        `Uint8Array.prototype.toHex??=function(){return this.reduce((t,r)=>t+r.toString(16).padStart(2,"0"),"")};Uint8Array.fromHex??=t=>Uint8Array.from(t.match(/../g)??[],r=>parseInt(r,16));Uint8Array.prototype.toBase64??=function(t){let r=btoa(this.reduce((e,a)=>e+String.fromCharCode(a),""));return t?.alphabet==="base64url"&&(r=r.replaceAll("+","-").replaceAll("/","_")),t?.omitPadding&&(r=r.replace(/=+$/,"")),r};Uint8Array.fromBase64??=(t,r)=>Uint8Array.from(atob(r?.alphabet==="base64url"?t.replaceAll("-","+").replaceAll("_","/"):t),e=>e.charCodeAt(0));`,
+        { append: true },
+      )
+    ));
+  },
 });
 await Deno.copyFile("../LICENSE", "./npm/LICENSE");
 await Deno.copyFile("./README.md", "./npm/README.md");
+await new Deno.Command("bun", { args: ["run", "./npm/test_runner.js"] })
+  .output().then<{}>(($) => $.success || Deno.stderr.write($.stderr));
