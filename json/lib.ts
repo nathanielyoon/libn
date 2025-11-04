c91a142012fd8215f16f80948584284271473e0d
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-04 15:18:52 -0500

    new: add type helpers

--- json/lib.ts
+++ json/lib.ts
@@ -1,11 +1,15 @@
+import { assertEquals } from "@std/assert";
+
 /** JSON value. */
 export type Json = null | boolean | number | string | readonly Json[] | {
   [_: string]: Json;
 };
+/** Composite type. */
+export type Objecty = { [_: PropertyKey]: unknown };
 /** Condensed object intersection. */
-export type Merge<A> = A extends object ? { [B in keyof A]: A[B] } : A;
+export type Merge<A> = A extends Objecty ? { [B in keyof A]: A[B] } : A;
 /** Non-readonly. */
-export type Writable<A> = A extends object ? { -readonly [B in keyof A]: A[B] }
+export type Writable<A> = A extends Objecty ? { -readonly [B in keyof A]: A[B] }
   : A;
 /** Generic constraint against excess properties. */
 export type Exact<A, B extends A> =
@@ -14,11 +18,11 @@ export type Exact<A, B extends A> =
 /** @internal */
 type Only<A, B> = Merge<
   & A
-  & { [C in Exclude<B extends object ? keyof B : never, keyof A>]?: never }
+  & { [C in Exclude<B extends Objecty ? keyof B : never, keyof A>]?: never }
 >;
 /** Exclusive-or between a list of types. */
 export type Xor<A extends unknown[]> = {
-  [B in keyof A]: A[B] extends object ? Only<A[B], A[number]> : A[B];
+  [B in keyof A]: A[B] extends Objecty ? Only<A[B], A[number]> : A[B];
 }[number];
 /** Union to intersection. */
 export type And<A> = (A extends never ? never : (_: A) => void) extends
@@ -39,3 +43,23 @@ export const isArray = /* @__PURE__ */
 export const hasOwn = /* @__PURE__ */ (() => Object.hasOwn)() as <
   A extends PropertyKey,
 >($: object, key: A) => $ is { [_ in A]: unknown };
+type All<A> = A extends Objecty ? { [B in keyof A]: All<A[B]> } : A;
+declare const ANY: unique symbol;
+type Any<A> = false extends (true & A) ? typeof ANY : All<A>;
+type Both<A, B> = A extends B ? B extends A ? true : false : false;
+type Is<A, B> = Both<
+  <C>(_: A) => C extends A & C | C ? true : false,
+  <C>(_: B) => C extends B & C | C ? true : false
+>;
+/** Checks that two types are equal. */
+export type IsExact<A, B> = [A, B] extends [never, never] ? true
+  : [A & B] extends [never] ? false
+  : Is<Any<A>, Any<B>>;
+/** Checks the type of a value and returns it, optionally asserting equality. */
+export const type = <A>(
+  ...expected: [A?]
+): <B extends A>(actual: IsExact<A, B> extends true ? B : never) => B =>
+<B extends A>(actual: B) => {
+  if (expected.length) assertEquals<A | undefined>(actual, expected[0]);
+  return actual;
+};
