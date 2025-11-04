78a0c0fac3f77d0e4dced5141a590bd489a75e56
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-04 10:28:33 -0500

    env: account for query in npm script

--- npm.ts
+++ npm.ts
@@ -1,18 +1,23 @@
 import { build, emptyDir } from "@deno/dnt";
 
 await emptyDir("./npm");
-const { name, version, exports } = JSON.parse(
+const { name, version, exports, imports, compilerOptions = {} } = JSON.parse(
   await Deno.readTextFile("./deno.json"),
+);
+const test = !Object.hasOwn(imports ?? {}, "@b-fuze/deno-dom");
+compilerOptions.lib &&= compilerOptions.lib.filter(($: string) =>
+  !$.startsWith("deno")
 );
 await build({
   outDir: "./npm",
   entryPoints: typeof exports === "string"
     ? [exports]
     : Object.entries<string>(exports).map(([name, path]) => ({ name, path })),
-  configFile: import.meta.resolve("./deno.json"),
   shims: { deno: "dev" },
   skipSourceOutput: true,
   typeCheck: false,
+  compilerOptions,
+  test,
   package: {
     name,
     version,
@@ -24,7 +29,7 @@ await build({
     },
   },
   postBuild: async () => {
-    await Promise.all(["esm", "script"].map(($) =>
+    test && await Promise.all(["esm", "script"].map(($) =>
       Deno.writeTextFile(
         `./npm/${$}/test.js`,
         'Uint8Array.prototype.toHex??=function(){return this.reduce((t,r)=>t+r.toString(16).padStart(2,"0"),"")};Uint8Array.fromHex??=t=>Uint8Array.from(t.match(/../g)??[],r=>parseInt(r,16));Uint8Array.prototype.toBase64??=function(t){let r=btoa(this.reduce((e,a)=>e+String.fromCharCode(a),""));return t?.alphabet==="base64url"&&(r=r.replaceAll("+","-").replaceAll("/","_")),t?.omitPadding&&(r=r.replace(/=+$/,"")),r};Uint8Array.fromBase64??=(t,r)=>Uint8Array.from(atob(r?.alphabet==="base64url"?t.replaceAll("-","+").replaceAll("_","/"):t),e=>e.charCodeAt(0));',
@@ -35,5 +40,7 @@ await build({
 });
 await Deno.copyFile("../LICENSE", "./npm/LICENSE");
 await Deno.copyFile("./README.md", "./npm/README.md");
-await new Deno.Command("bun", { args: ["run", "./npm/test_runner.js"] })
-  .output().then<{}>(($) => $.success || Deno.stderr.write($.stderr));
+test && await new Deno.Command(
+  "bun",
+  { args: ["run", "./npm/test_runner.js"] },
+).output().then<{}>(($) => $.success || Deno.stderr.write($.stderr));
