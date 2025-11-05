b0d23c8b05cacb582c207503ce8bfa68b4237bce
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-05 12:18:25 -0500

    fix: prepend fill

--- npm.ts
+++ npm.ts
@@ -1,6 +1,8 @@
 import { build, emptyDir, type LibName } from "@deno/dnt";
 import config from "./deno.json" with { type: "json" };
 
+const FILL =
+  'Uint8Array.prototype.toHex??=function(){return this.reduce((t,r)=>t+r.toString(16).padStart(2,"0"),"")};Uint8Array.fromHex??=t=>Uint8Array.from(t.match(/../g)??[],r=>parseInt(r,16));Uint8Array.prototype.toBase64??=function(t){let r=btoa(this.reduce((e,a)=>e+String.fromCharCode(a),""));return t?.alphabet==="base64url"&&(r=r.replaceAll("+","-").replaceAll("/","_")),t?.omitPadding&&(r=r.replace(/=+$/,"")),r};Uint8Array.fromBase64??=(t,r)=>Uint8Array.from(atob(r?.alphabet==="base64url"?t.replaceAll("-","+").replaceAll("_","/"):t),e=>e.charCodeAt(0));';
 await emptyDir("./npm");
 const { name, version, exports, imports, compilerOptions = {} } = JSON.parse(
   await Deno.readTextFile("./deno.json"),
@@ -10,6 +12,8 @@ const test = !Object.hasOwn(imports ?? {}, "@b-fuze/deno-dom");
 compilerOptions.lib &&= compilerOptions.lib.filter(($: string) =>
   !$.startsWith("deno")
 ).concat(lib);
+const prepend = (path: string, text: string) =>
+  Deno.readTextFile(path).then(($) => Deno.writeTextFile(path, text + $));
 await build({
   outDir: "./npm",
   entryPoints: typeof exports === "string"
@@ -32,13 +36,18 @@ await build({
     },
   },
   postBuild: async () => {
-    test && await Promise.all(["esm", "script"].map(($) =>
-      Deno.writeTextFile(
-        `./npm/${$}/test.js`,
-        'Uint8Array.prototype.toHex??=function(){return this.reduce((t,r)=>t+r.toString(16).padStart(2,"0"),"")};Uint8Array.fromHex??=t=>Uint8Array.from(t.match(/../g)??[],r=>parseInt(r,16));Uint8Array.prototype.toBase64??=function(t){let r=btoa(this.reduce((e,a)=>e+String.fromCharCode(a),""));return t?.alphabet==="base64url"&&(r=r.replaceAll("+","-").replaceAll("/","_")),t?.omitPadding&&(r=r.replace(/=+$/,"")),r};Uint8Array.fromBase64??=(t,r)=>Uint8Array.from(atob(r?.alphabet==="base64url"?t.replaceAll("-","+").replaceAll("_","/"):t),e=>e.charCodeAt(0));',
-        { append: true },
-      )
-    ));
+    test && await Promise.all(
+      ["esm", "script"].map(($) =>
+        prepend(`./npm/${$}/test.js`, FILL).catch((error) => {
+          if (error instanceof Deno.errors.NotFound) {
+            return prepend(
+              `./npm/${$}/${name.slice(name.indexOf("/") + 1)}/test.js`,
+              FILL,
+            );
+          } else throw Error;
+        })
+      ),
+    );
   },
 });
 await Deno.copyFile("../LICENSE", "./npm/LICENSE");
