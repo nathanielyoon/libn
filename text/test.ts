851e650dcbeba1f385a15562ddff987e2adf92b6
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-05 01:21:51 -0500

    fix: flatten tests

--- text/test.ts
+++ text/test.ts
@@ -60,48 +60,36 @@ const capitalize = (delimiter: string) => (to: string, next: string) => {
   const [head = "", ...tail] = next;
   return to + delimiter + head.toUpperCase() + tail.join("").toLowerCase();
 };
-Deno.test("convert.lowerCamel() converts to lower camel case", () => {
-  assertEquals(lowerCamel(""), "");
+Deno.test("convert.lowerCamel() converts to lower camel case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(
       lowerCamel(together),
       separate.slice(1).reduce(capitalize(""), separate[0].toLowerCase()),
     );
-  }));
-});
-Deno.test("convert.upperCamel() converts to upper camel case", () => {
-  assertEquals(upperCamel(""), "");
+  })));
+Deno.test("convert.upperCamel() converts to upper camel case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(upperCamel(together), separate.reduce(capitalize(""), ""));
-  }));
-});
-Deno.test("convert.lowerKebab() converts to lower kebab case", () => {
-  assertEquals(lowerKebab(""), "");
+  })));
+Deno.test("convert.lowerKebab() converts to lower kebab case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(lowerKebab(together), separate.join("-").toLowerCase());
-  }));
-});
-Deno.test("convert.upperKebab() converts to upper kebab case", () => {
-  assertEquals(upperKebab(""), "");
+  })));
+Deno.test("convert.upperKebab() converts to upper kebab case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(
       upperKebab(together),
       separate.reduce(capitalize("-"), "").slice(1),
     );
-  }));
-});
-Deno.test("convert.lowerSnake() converts to lower snake case", () => {
-  assertEquals(lowerSnake(""), "");
+  })));
+Deno.test("convert.lowerSnake() converts to lower snake case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(lowerSnake(together), separate.join("_").toLowerCase());
-  }));
-});
-Deno.test("convert.upperSnake() converts to upper snake case", () => {
-  assertEquals(upperSnake(""), "");
+  })));
+Deno.test("convert.upperSnake() converts to upper snake case", () =>
   fc.assert(fc.property(fcWords, ({ separate, together }) => {
     assertEquals(upperSnake(together), separate.join("_").toUpperCase());
-  }));
-});
+  })));
 Deno.test("normalize.uncode() passes reference vectors", () => {
   const to = new Uint32Array(0x110000).fill(0xfffd);
   for (const $ of vectors.uncode) {
@@ -112,15 +100,14 @@ Deno.test("normalize.uncode() passes reference vectors", () => {
     assertEquals(to[uncode(String.fromCodePoint(z)).codePointAt(0)!], to[z]);
   }
 });
-Deno.test("normalize.unlone() replaces lone surrogates", () => {
+Deno.test("normalize.unlone() replaces lone surrogates", () =>
   fc.assert(fc.property(
     fc.uint16Array().map(($) => $.reduce((to, code) => to + de(code), "")),
     ($) => {
       assertEquals(unlone($), $.toWellFormed());
     },
-  ));
-});
-Deno.test("normalize.unlone() never changes string length", () => {
+  )));
+Deno.test("normalize.unlone() never changes string length", () =>
   fc.assert(fc.property(
     fc.stringMatching(
       /^(?:[\ud800-\udbff](?:[^\udc00-\udfff]|$)|(?:^|[^\ud800-\udbff])[\udc00-\udfff])*$/,
@@ -128,11 +115,10 @@ Deno.test("normalize.unlone() never changes string length", () => {
     ($) => {
       assertEquals(unlone($).length, $.length);
     },
-  ));
-});
+  )));
 Deno.test("normalize.uncode() replaces all control codes", () => {
   for (let z = 0x00; z <= 0x1f; ++z) {
-    z === 0x9 || z === 0xa || z === 0xd ||
+    z === 0x9 || z === 0xa || z === 0xd || // "useful"
       assertEquals(uncode(de(z)), "\ufffd");
   }
   for (let z = 0x7f; z <= 0x9f; ++z) assertEquals(uncode(de(z)), "\ufffd");
@@ -148,13 +134,12 @@ Deno.test("normalize.uncode() replaces all un-paired surrogates", () => {
     );
   }
 });
-Deno.test("normalize.uncode() replaces all noncharacters", () => {
-  for (let z = 0x00; z <= 0x10; ++z) {
-    assertEquals(uncode(String.fromCodePoint(z << 16 | 0xfffe)), "\ufffd");
-    assertEquals(uncode(String.fromCodePoint(z << 16 | 0xffff)), "\ufffd");
-  }
-});
-Deno.test("normalize.unline() replaces weird breaks with linefeeds", () => {
+Deno.test("normalize.uncode() replaces all noncharacters", () =>
+  Array(0x11).keys().forEach(($) => {
+    assertEquals(uncode(String.fromCodePoint($ << 16 | 0xfffe)), "\ufffd");
+    assertEquals(uncode(String.fromCodePoint($ << 16 | 0xffff)), "\ufffd");
+  }));
+Deno.test("normalize.unline() replaces weird breaks with linefeeds", () =>
   fc.assert(fc.property(
     fc.nat({ max: 255 }).chain(($) =>
       fc.record({
@@ -168,37 +153,30 @@ Deno.test("normalize.unline() replaces weird breaks with linefeeds", () => {
     ({ string, length }) => {
       assertEquals(unline(string), "\n".repeat(length));
     },
-  ));
-});
-Deno.test("normalize.unline() matches any break", () => {
-  for (const $ of ["\r\n", "\x85", "\u2028", "\u2029"]) {
+  )));
+Deno.test("normalize.unline() matches any break", () =>
+  ["\r\n", "\x85", "\u2028", "\u2029"].forEach(($) => {
     assertEquals(unline($), "\n");
-  }
-});
-Deno.test("normalize.unwide() trims", () => {
+  }));
+Deno.test("normalize.unwide() trims", () =>
   fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
     assertNotMatch(unwide($), /^\s|\s$/);
-  }));
-});
-Deno.test("normalize.unwide() collapses consecutive spaces", () => {
+  })));
+Deno.test("normalize.unwide() collapses consecutive spaces", () =>
   fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
     assertMatch(unwide($), /^(?:\S|(\s|\r\n)(?!\1))*$/);
-  }));
-});
-Deno.test("normalize.unwide() matches any space", () => {
-  for (
-    const $ of Array.from({ length: 11 }, (_, z) => z + 0x2000).concat(
-      [0x1680, 0x2028, 0x2029, 0x202f, 0x205f, 0x3000, 0xfeff],
-    ).map(($) => de($)).concat(
-      ["\t", "\n", "\v", "\f", "\r", "\r\n", " ", "\xa0"],
-    )
-  ) {
+  })));
+Deno.test("normalize.unwide() matches any space", () =>
+  Array.from({ length: 11 }, (_, z) => z + 0x2000).concat(
+    [0x1680, 0x2028, 0x2029, 0x202f, 0x205f, 0x3000, 0xfeff],
+  ).map(($) => de($)).concat(
+    ["\t", "\n", "\v", "\f", "\r", "\r\n", " ", "\xa0"],
+  ).forEach(($) => {
     for (let z = 1; z < 9; ++z) {
       assertEquals(unwide(`\0${$.repeat(z)}\0`).slice(1, -1), $);
     }
-  }
-});
-Deno.test("normalize.unmark() removes diacritics", () => {
+  }));
+Deno.test("normalize.unmark() removes diacritics", () =>
   fc.assert(fc.property(
     fc.record({
       character: fc.oneof(
@@ -210,14 +188,12 @@ Deno.test("normalize.unmark() removes diacritics", () => {
     ({ character, mark }) => {
       assertEquals(unmark(character + mark), character);
     },
-  ));
-});
-Deno.test("normalize.unhtml() removes special html characters", () => {
+  )));
+Deno.test("normalize.unhtml() removes special html characters", () =>
   fc.assert(fc.property(fc.string(), ($) => {
     assertMatch(unhtml($), /^(?:[^&"'<>]|&#\d\d;)*$/);
-  }));
-});
-Deno.test("normalize.unhtml() escapes with right codes", () => {
+  })));
+Deno.test("normalize.unhtml() escapes with right codes", () =>
   fc.assert(fc.property(
     fc.string({ unit: fc.constantFrom('"', "&", "'", "<", ">") }),
     ($) => {
@@ -227,13 +203,11 @@ Deno.test("normalize.unhtml() escapes with right codes", () => {
         assertEquals(+codes[z].slice(2, -1), en.call($, z));
       }
     },
-  ));
-});
-Deno.test("normalize.unrexp() makes literal", () => {
+  )));
+Deno.test("normalize.unrexp() makes literal", () =>
   fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
     assertMatch($, RegExp(`^${unrexp($)}$`));
-  }));
-});
+  })));
 Deno.test("normalize.unrexp() escapes all regex syntax characters", () => {
   for (const $ of "/^$\\*+?{}()[]|") assertEquals(unrexp($), `\\${$}`);
 });
@@ -284,19 +258,16 @@ Deno.test("fold.createRanges() creates same ranges", async () => {
     (await import("./ranges.json", { with: { type: "json" } })).default,
   );
 });
-Deno.test("fold.uncase() passes reference vectors", () => {
-  for (const $ of vectors.uncase) assertEquals(uncase($.source), $.target);
-});
-Deno.test("fold.uncase() un-mixes case", () => {
-  fc.assert(fc.property(
-    fc.string({ unit: "grapheme" }).chain(($) =>
-      fc.array(fc.mixedCase(fc.constant($)), { minLength: 2 })
-    ),
-    ($) => {
-      assertEquals(new Set($.map(uncase)), new Set([uncase($[0])]));
-    },
-  ));
-});
+Deno.test("fold.uncase() passes reference vectors", () =>
+  vectors.uncase.forEach(($) => {
+    assertEquals(uncase($.source), $.target);
+  }));
+Deno.test("fold.uncase() un-mixes case", () =>
+  fc.assert(fc.property(fc.string({ unit: "grapheme" }), ($) => {
+    ["ʕ", "ª"];
+    assertEquals(uncase($), uncase($.toUpperCase()));
+    assertEquals(uncase($), uncase($.toLowerCase()));
+  })));
 Deno.test("fold.uncase() uses full case folding", () => {
   assertNotEquals("\xdf".length, uncase("\xdf").length);
   assertNotEquals("\u0130".length, uncase("\u0130").length);
@@ -307,7 +278,7 @@ Deno.test("fold.uncase() uses non-Turkic mappings", () => {
   assertEquals(uncase("\x49"), "\x69");
   assertEquals(uncase("\u0130"), "\x69\u0307");
 });
-Deno.test("match.includes() follows built-in includes", () => {
+Deno.test("match.includes() follows built-in includes", () =>
   fc.assert(fc.property(
     fc.string({ size: "medium", unit: "grapheme" }),
     fc.string({ size: "medium", unit: "grapheme" }),
@@ -315,9 +286,8 @@ Deno.test("match.includes() follows built-in includes", () => {
       if (one.includes(two)) assert(includes(one, two));
       if (two.includes(one)) assert(includes(two, one));
     },
-  ));
-});
-Deno.test("match.includes() returns true for any partial substring", () => {
+  )));
+Deno.test("match.includes() returns true for any partial substring", () =>
   fc.assert(fc.property(
     fc.string().chain(($) =>
       fc.record({
@@ -328,9 +298,8 @@ Deno.test("match.includes() returns true for any partial substring", () => {
     ({ source, target }) => {
       assert(includes(source, target));
     },
-  ));
-});
-Deno.test("match.includes() returns false for longer targets", () => {
+  )));
+Deno.test("match.includes() returns false for longer targets", () =>
   fc.assert(fc.property(
     fc.string({ unit: "grapheme" }).chain(($) =>
       fc.record({
@@ -344,9 +313,8 @@ Deno.test("match.includes() returns false for longer targets", () => {
     ({ shorter, longer }) => {
       assert(!includes(shorter, longer));
     },
-  ));
-});
-Deno.test("match.includes() checks equality for same-length strings", () => {
+  )));
+Deno.test("match.includes() checks equality for same-length strings", () =>
   fc.assert(fc.property(
     fc.string({ unit: "grapheme" }).chain(($) =>
       fc.record({
@@ -362,8 +330,7 @@ Deno.test("match.includes() checks equality for same-length strings", () => {
       assertEquals(includes(one, two), one === two);
       assertEquals(includes(two, one), one === two);
     },
-  ));
-});
+  )));
 const levenshtein = (one: string, two: string) => {
   const c = [...Array(two.length).keys(), two.length];
   for (let d, e, z = 1, y; z <= one.length; c[two.length] = d, ++z) {
@@ -374,16 +341,15 @@ const levenshtein = (one: string, two: string) => {
   }
   return c[two.length];
 };
-Deno.test("match.distance() follows levenshtein", () => {
+Deno.test("match.distance() follows levenshtein", () =>
   fc.assert(fc.property(
     fc.string({ size: "medium", unit: "grapheme" }),
     fc.string({ size: "medium", unit: "grapheme" }),
     (one, two) => {
       assertEquals(distance(one, two), levenshtein(one, two));
     },
-  ));
-});
-Deno.test("match.distance() falls inside levenshtein bounds", () => {
+  )));
+Deno.test("match.distance() falls inside levenshtein bounds", () =>
   fc.assert(fc.property(
     fc.string({ size: "medium", unit: "grapheme" }),
     fc.string({ size: "medium", unit: "grapheme" }),
@@ -394,8 +360,7 @@ Deno.test("match.distance() falls inside levenshtein bounds", () => {
         Math.abs(one.length - two.length),
       );
     },
-  ));
-});
+  )));
 import.meta.main && Promise.all([
   fetch(
     "https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt",
@@ -411,13 +376,19 @@ import.meta.main && Promise.all([
   }),
 ]).then(([data, rfc9839, fold]) => ({
   convert: data.matchAll(
-    /^([\dA-F]{4,6});[^;]*;(L[ult](?=;)|L(?=[mo];)|N(?=[dlo];))/gm,
+    /^([\dA-F]{4,6});[^;]*;((L[ult](?=;)|L(?=[mo];)|N(?=[dlo];))[modl]?)/gm,
   ).reduce<{ [_ in `L${"u" | "l" | "t" | ""}` | "N"]: number[] }>(
-    (categories, [_, hex, category]) => {
+    (categories, [_, hex, subcategory, category]) => {
+      const character = String.fromCodePoint(parseInt(hex, 16));
       if (
-        RegExp(`^\\p{${category}}$`, "u").test(
-          String.fromCodePoint(parseInt(hex, 16)),
-        )
+        RegExp(`^\\p{${category}}$`, "u").test(character) &&
+        !RegExp(
+          `^(?:${
+            "|\\p{Lu}|\\p{Ll}|\\p{Lt}|\\p{Lm}|\\p{Lo}|\\p{Nd}|\\p{Nl}|\\p{No}"
+              .replace(RegExp(`\\|\\\\p\\{${subcategory}\\}`), "").slice(1)
+          })$`,
+          "u",
+        ).test(character)
       ) categories[category as keyof typeof categories].push(parseInt(hex, 16));
       return categories;
     },
