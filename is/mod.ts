a2bf1e01bdb14243923f3c48f0bfca4fa1400f40
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-06 20:15:26 -0500

    doc: improve type description

--- is/mod.ts
+++ is/mod.ts
@@ -12,7 +12,7 @@ type Are<A, B> = Both<
   <C>(_: A) => C extends A & C | C ? true : false,
   <C>(_: B) => C extends B & C | C ? true : false
 >;
-/** Checks that two types are equal. */
+/** Resolves to `true` if the types are exactly equal, and `false` otherwise. */
 export type Is<A, B> = [A, B] extends [never, never] ? true : Are<An<A>, An<B>>;
 /** Checks the type of a value and returns it. */
 export const is =
