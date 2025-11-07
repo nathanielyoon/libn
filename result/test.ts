213fa765680e4f2730865e6d29ee861b434b7a09
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-06 23:35:44 -0500

    fix: slightly rewrite tests

--- result/test.ts
+++ result/test.ts
@@ -1,10 +1,4 @@
-import { type } from "@libn/json/lib";
-import {
-  assert,
-  assertEquals,
-  assertStrictEquals,
-  assertThrows,
-} from "@std/assert";
+import { is } from "@libn/is";
 import {
   exec,
   fail,
@@ -17,59 +11,67 @@ import {
   some,
   type Yieldable,
 } from "@libn/result";
+import {
+  assert,
+  assertEquals,
+  assertStrictEquals,
+  assertThrows,
+} from "@std/assert";
 
-const S0 = Symbol("S0");
+const S0 = Symbol("S0"), S1 = Symbol("S1");
 Deno.test("fail() creates a failure", () => {
   const no = fail(S0);
-  assertEquals(type<Yieldable<typeof S0, never>>()(no).state, false);
-  assertEquals(type<No<typeof no>>()(no.value), S0);
+  assertEquals(is<Yieldable<typeof S0, never>>()(no).state, false);
+  assertEquals(is<No<typeof no>>()(no.value), S0);
 });
 Deno.test("pass() creates a success", () => {
-  const ok = pass(S0);
-  assertEquals(type<Yieldable<never, typeof S0>>()(ok).state, true);
-  assertEquals(type<Ok<typeof ok>>()(ok.value), S0);
+  const ok = pass(S1);
+  assertEquals(is<Yieldable<never, typeof S1>>()(ok).state, true);
+  assertEquals(is<Ok<typeof ok>>()(ok.value), S1);
 });
 Deno.test("open() unwraps", () => {
-  for (const $ of [pass(S0), fail(S0)]) assertEquals(type(S0)(open($)), S0);
+  for (const $ of [pass(S0), fail(S0)]) assertEquals(is(S0)(open($)), S0);
 });
 Deno.test("open() unwraps a success", () => {
   assertThrows(() => open(fail(S0), true));
-  assertEquals(type(S0)(open(pass(S0), true)), S0);
+  assertEquals(is(S1)(open(pass(S1), true)), S1);
 });
 Deno.test("open() unwraps a failure", () => {
-  assertEquals(type(S0)(open(fail(S0), false)), S0);
-  assertThrows(() => open(pass(S0), false));
+  assertEquals(is(S0)(open(fail(S0), false)), S0);
+  assertThrows(() => open(pass(S1), false));
 });
 Deno.test("some() creates a failure or success", () => {
   for (const $ of [false, true]) {
     const or = some($);
-    if (or.state) assertEquals(type(true)(or.value), $);
-    else assertEquals(type(false)(or.value), $);
+    if (or.state) assertEquals(is(true)(or.value), $);
+    else assertEquals(is(false)(or.value), $);
   }
 });
 Deno.test("some() coerces all falsy values", () => {
   for (const $ of [undefined, null, false, 0, 0n, ""] as const) {
     const or = some($);
-    or.state && type<Ok<typeof or>>()(or.value); // never
+    or.state && is<Ok<typeof or>>()(or.value); // never
     assertEquals(or.state, false);
     assertEquals(or.value, $);
   }
 });
-const no = fail(S0), no0 = no[Symbol.iterator]();
-const ok = pass(S0), ok0 = ok[Symbol.iterator]();
-Deno.test("result[Symbol.iterator]() yields itself", () => {
-  const no1 = type<IteratorResult<typeof no, never>>()(no0.next());
-  const ok1 = type<IteratorResult<typeof ok, typeof S0>>()(ok0.next());
-  assert(!no1.done), assert(!ok1.done);
-  assertStrictEquals(type<Yieldable<typeof S0, never>>()(no1.value), no);
-  assertStrictEquals(type<Yieldable<never, typeof S0>>()(ok1.value), ok);
-});
-Deno.test("result[Symbol.iterator]() returns passed-in argument", () => {
-  const no2 = type<IteratorResult<typeof no, never>>()(no0.next());
-  const ok2 = type<IteratorResult<typeof ok, typeof S0>>()(ok0.next(ok.value));
-  assert(no2.done), assert(ok2.done);
-  assertStrictEquals(type<Ok<typeof no>>()(no2.value), undefined);
-  assertStrictEquals(type<Ok<typeof ok>>()(ok2.value), S0);
+Deno.test("result[Symbol.iterator]()", async (t) => {
+  const no = fail(S0), no0 = no[Symbol.iterator]();
+  const ok = pass(S1), ok0 = ok[Symbol.iterator]();
+  await t.step("yields itself", () => {
+    const no1 = is<IteratorResult<typeof no, never>>()(no0.next());
+    const ok1 = is<IteratorResult<typeof ok, typeof S1>>()(ok0.next());
+    assert(!no1.done), assert(!ok1.done);
+    assertStrictEquals(is<Yieldable<typeof S0, never>>()(no1.value), no);
+    assertStrictEquals(is<Yieldable<never, typeof S1>>()(ok1.value), ok);
+  });
+  await t.step("returns passed-in argument", () => {
+    const no2 = is<IteratorResult<typeof no, never>>()(no0.next());
+    const ok2 = is<IteratorResult<typeof ok, typeof S1>>()(ok0.next(ok.value));
+    assert(no2.done), assert(ok2.done);
+    assertStrictEquals(is<Ok<typeof no>>()(no2.value), undefined);
+    assertStrictEquals(is<Ok<typeof ok>>()(ok2.value), S1);
+  });
 });
 Deno.test("join() aggregates", () => {
   for (const one of [some(false), some(true)]) {
@@ -91,10 +93,10 @@ Deno.test("safe() try-catches", async () => {
       if (ok) return ok;
       throw ok;
     };
-    const result1 = type<Yieldable<Error, true>>()(safe(unsafe)($));
+    const result1 = is<Yieldable<Error, true>>()(safe(unsafe)($));
     if ($) assert(result1.state), assertEquals(result1.value, $);
     else assert(!result1.state), assertEquals(result1.value.cause, $);
-    const result2 = type<Yieldable<unknown, true>>()(
+    const result2 = is<Yieldable<unknown, true>>()(
       safe(unsafe, (error) => error.cause)($),
     );
     assertEquals(result2.state, result2.value);
@@ -105,7 +107,7 @@ Deno.test("safe() try-catches", async () => {
       if (ok) return await Promise.resolve(ok);
       throw ok;
     };
-    await type<Promise<Yieldable<Error, true>>>()(safe(unsafe)($)).then(
+    await is<Promise<Yieldable<Error, true>>>()(safe(unsafe)($)).then(
       ({ state, value }) => {
         if ($) assert(state), assertEquals(value, $);
         else assert(!state), assertEquals(value.cause, $);
@@ -117,7 +119,7 @@ Deno.test("safe() try-catches", async () => {
         async (error: Error) => await Promise.resolve(error.cause),
       ]
     ) {
-      await type<Promise<Yieldable<unknown, true>>>()(safe(unsafe, or)($)).then(
+      await is<Promise<Yieldable<unknown, true>>>()(safe(unsafe, or)($)).then(
         ({ state, value }) => {
           assertEquals(state, value);
           assertEquals(value, $);
@@ -128,7 +130,7 @@ Deno.test("safe() try-catches", async () => {
 });
 Deno.test("exec() runs a block", async () => {
   for (const $ of [false, true]) {
-    const result = type<Yieldable<false, true>>()(
+    const result = is<Yieldable<false, true>>()(
       exec(function* ($: boolean) {
         return yield* some($);
       })($),
@@ -137,7 +139,7 @@ Deno.test("exec() runs a block", async () => {
     assertEquals(result.value, $);
   }
   for (const $ of [false, true]) {
-    await type<Promise<Yieldable<false, true>>>()(
+    await is<Promise<Yieldable<false, true>>>()(
       exec(async function* ($: boolean) {
         return await Promise.resolve(yield* some($));
       })($),
