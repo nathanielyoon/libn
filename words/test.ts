a76b84d5545676932e055e18e66828be018f6fe1
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-09 21:57:48 -0500

    fix: rewrite with new test helpers

--- words/test.ts
+++ words/test.ts
@@ -6,8 +6,8 @@ import {
   upperKebab,
   upperSnake,
 } from "@libn/words";
-import { assertEquals } from "@std/assert";
 import fc from "fast-check";
+import { getText, save, test } from "../test.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
 const codes = (["Lu", "Ll", "Lt", "L", "N"] as const).reduce((to, key) => ({
@@ -24,64 +24,36 @@ const fcWords = fc.array(
     fc.array(codes.L, { minLength: 1 }),
   ).map((letters) => letters.flat().join("")),
   { minLength: 1 },
-).map((words) => ({
-  all: words,
-  one: words.reduce(
-    (to, word, z) => to + " -._"[z & 3] + word,
-  ).padStart(words.length + 1).padEnd(words.length + 2),
-}));
+).map((words) => ({ all: words, one: ["", ...words, ""].join(" ") }));
 const first = (delimiter: string) => (to: string, next: string) => {
   const [head = "", ...tail] = next;
   return to + delimiter + head.toUpperCase() + tail.join("").toLowerCase();
 };
-Deno.test("lowerCamel() converts to lower camel case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all: [head = "", ...tail], one }) => {
-      assertEquals(lowerCamel(one), tail.reduce(first(""), head.toLowerCase()));
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-Deno.test("upperCamel() converts to upper camel case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all, one }) => {
-      assertEquals(upperCamel(one), all.reduce(first(""), ""));
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-Deno.test("lowerKebab() converts to lower kebab case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all, one }) => {
-      assertEquals(lowerKebab(one), all.join("-").toLowerCase());
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-Deno.test("upperKebab() converts to upper kebab case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all, one }) => {
-      assertEquals(upperKebab(one), all.reduce(first("-"), "").slice(1));
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-Deno.test("lowerSnake() converts to lower snake case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all, one }) => {
-      assertEquals(lowerSnake(one), all.join("_").toLowerCase());
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-Deno.test("upperSnake() converts to upper snake case", () =>
-  fc.assert(
-    fc.property(fcWords, ({ all, one }) => {
-      assertEquals(upperSnake(one), all.join("_").toUpperCase());
-    }),
-    { examples: [[{ all: [], one: "" }]] },
-  ));
-import.meta.main && fetch(
-  "https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt",
-).then(($) => $.text()).then((data) =>
+test("lowerCamel :: words : lower camel case", fcWords, ({ all, one }) => {
+  const [head = "", ...tail] = all;
+  return [lowerCamel(one), tail.reduce(first(""), head.toLowerCase())];
+}, { examples: [[{ all: [], one: "" }]] });
+test("upperCamel() :: words : to upper camel case", fcWords, ({ all, one }) => {
+  return [upperCamel(one), all.reduce(first(""), "")];
+}, { examples: [[{ all: [], one: "" }]] });
+test("lowerKebab() :: words : to lower kebab case", fcWords, ({ all, one }) => {
+  return [lowerKebab(one), all.join("-").toLowerCase()];
+}, { examples: [[{ all: [], one: "" }]] });
+test("upperKebab() :: words : to upper kebab case", fcWords, ({ all, one }) => {
+  return [upperKebab(one), all.reduce(first("-"), "").slice(1)];
+}, { examples: [[{ all: [], one: "" }]] });
+test("lowerSnake() :: words : to lower snake case", fcWords, ({ all, one }) => {
+  return [lowerSnake(one), all.join("_").toLowerCase()];
+}, { examples: [[{ all: [], one: "" }]] });
+test("upperSnake() :: words : to upper snake case", fcWords, ({ all, one }) => {
+  return [upperSnake(one), all.join("_").toUpperCase()];
+}, { examples: [[{ all: [], one: "" }]] });
+import.meta.main && getText(
+  "www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt",
+).then((data) =>
   data.matchAll(
     /^([\dA-F]{4,6});[^;]*;((L[ult](?=;)|L(?=[mo];)|N(?=[dlo];))[modl]?)/gm,
-  ).reduce<{ [_: string]: number[] }>((to, [_, hex, subcategory, category]) => {
+  ).reduce<{ [_: string]: number[] }>((to, [, hex, subcategory, category]) => {
     const code = parseInt(hex, 16);
     RegExp(
       `^(?!${
@@ -92,6 +64,4 @@ import.meta.main && fetch(
     ).test(String.fromCodePoint(code)) && to[category].push(code);
     return to;
   }, { Lu: [], Ll: [], Lt: [], L: [], N: [] })
-).then(($) =>
-  Deno.writeTextFile(`${import.meta.dirname}/vectors.json`, JSON.stringify($))
-);
+).then(save(import.meta));
