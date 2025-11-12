e9e316f5d9cca684703257708b1a94c4b7d69e8d
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-11 20:05:50 -0500

    new: add non-cryptographic hash module

---
+++ nchf/lib.ts
@@ -0,0 +1,20 @@
+/** @internal */
+type U64 = { hi: number; lo: number };
+/** Multiplies two 32-bit integers to an unsigned 64-bit product. */
+export const umul32 = (one: number, two: number): U64 => {
+  const a = one & 0xffff, b = one >>> 16, c = two & 0xffff, d = two >>> 16;
+  const e = a * c, f = b * c, g = (e >>> 16) + (f & 0xffff) + a * d;
+  return {
+    hi: (f >>> 16) + (g >>> 16) + b * d,
+    lo: (g << 16 | e & 0xffff) >>> 0,
+  };
+};
+/** Multiplies two 64-bit integers and updates them in-place. */
+export const umul64 = (one: U64, two: U64): void => {
+  const a = umul32(one.lo, two.lo), b = umul32(one.hi, two.lo);
+  const c = umul32(one.lo, two.hi), d = umul32(one.hi, two.hi);
+  one.lo = a.lo, one.hi = c.lo + b.lo >>> 0, one.hi < b.lo && ++c.hi;
+  one.hi = one.hi + a.hi >>> 0, one.hi < a.hi && ++c.hi, two.hi = d.hi;
+  two.lo = d.lo + b.hi >>> 0, two.lo < b.hi && ++two.hi;
+  two.lo = two.lo + c.hi >>> 0, two.lo < c.hi && ++two.hi;
+};
---
+++ nchf/test.ts
@@ -0,0 +1,129 @@
+import { a5hash32, a5hash64 } from "@libn/nchf/a5hash";
+import { oaat32 } from "@libn/nchf/oaat";
+import { assert, assertEquals } from "@std/assert";
+import fc from "fast-check";
+import { get, set } from "../test.ts";
+import { umul32, umul64 } from "./lib.ts";
+import vectors from "./vectors.json" with { type: "json" };
+
+const zip = async ($: BlobPart, use: CompressionStream | DecompressionStream) =>
+  new Uint8Array((await Array.fromAsync(
+    new Blob([$]).stream().pipeThrough(use),
+  )).flatMap(($) => [...$]));
+type Size<A extends string> = A extends `${string}${infer B}`
+  ? B extends "32" | "64" ? `u${B}` : Size<B>
+  : never;
+const size = <A extends string>($: A) => `u${$.slice(-2)}` as Size<A>;
+const ffi = async <A extends keyof typeof vectors>(hash: A) => {
+  const url = await Deno.makeTempFile();
+  await Deno.writeFile(
+    url,
+    await zip(
+      Uint8Array.fromBase64(vectors[hash]),
+      new DecompressionStream("gzip"),
+    ),
+  );
+  return [
+    url,
+    Deno.dlopen(url, {
+      ffi: { parameters: ["buffer", "usize", size(hash)], result: size(hash) },
+    }),
+  ] as const;
+};
+const fcU32 = fc.double({
+  min: 0,
+  max: -1 >>> 0,
+  noDefaultInfinity: true,
+  noNaN: true,
+}).map(($) => $ >>> 0);
+const fcU64 = fc.bigInt({ min: 0n, max: (1n << 64n) - 1n }).map(($) => ({
+  bigint: $,
+  number: deBig($),
+}));
+const enBig = ($: { hi: number; lo: number }) =>
+  BigInt($.hi) << 32n | BigInt($.lo);
+const deBig = ($: bigint) => ({
+  hi: Number($ >> 32n),
+  lo: Number($ & 0xffffffffn),
+});
+
+Deno.test("lib.umul32 : 32-bit integers", () => {
+  fc.assert(fc.property(fcU32, fcU32, (one, two) => {
+    const bigint = BigInt(one) * BigInt(two), number = umul32(one, two);
+    assertEquals(enBig(number), bigint);
+    assertEquals(number, deBig(bigint));
+  }));
+});
+Deno.test("lib.umul64 : 64-bit integers", () => {
+  fc.assert(fc.property(fcU64, fcU64, (one, two) => {
+    const bigint = one.bigint * two.bigint;
+    umul64(one.number, two.number);
+    assertEquals(enBig(one.number), bigint & 0xffffffffffffffffn);
+    assertEquals(enBig(two.number), bigint >> 64n);
+  }));
+});
+
+Deno.test("oaat.oaat32 : original GoodOAAT", async () => {
+  const [url, lib] = await ffi("oaat32");
+  fc.assert(fc.property(fc.uint8Array({ size: "large" }), fcU32, ($, seed) => {
+    assertEquals(oaat32($, seed), lib.symbols.ffi($, BigInt($.length), seed));
+  }));
+  lib.close(), await Deno.remove(url);
+});
+
+Deno.test("a5hash.a5hash32 : original a5hash32", async () => {
+  const [url, lib] = await ffi("a5hash32");
+  fc.assert(fc.property(fc.uint8Array({ size: "large" }), fcU32, ($, seed) => {
+    assertEquals(a5hash32($, seed), lib.symbols.ffi($, BigInt($.length), seed));
+  }));
+  lib.close(), await Deno.remove(url);
+});
+Deno.test("a5hash.a5hash64 : original a5hash", async () => {
+  const [url, lib] = await ffi("a5hash64");
+  fc.assert(fc.property(
+    fc.uint8Array({ size: "large" }),
+    fc.bigInt({ min: 0n, max: 0xffffffffffffffffn }),
+    ($, seed) => {
+      assertEquals(
+        a5hash64($, seed),
+        lib.symbols.ffi($, BigInt($.length), seed),
+      );
+    },
+  ));
+  lib.close(), await Deno.remove(url);
+});
+
+import.meta.main && Promise.all([
+  get`/rurban/smhasher/3931fd6f723f4fb2afab6ef9a628912220e90ce7/Hashes.cpp${6967}${7785}`,
+  get`/avaneev/a5hash/c9ce07aad514ef7073f1436e5bed26310aab8c2c/a5hash.h`,
+]).then(([oaat, a5hash]) =>
+  Promise.all([
+    `#include <cstddef>
+#include <cstdint>
+extern "C" uint32_t ffi(uint8_t *key, size_t len, uint32_t seed) ${
+      oaat.replace("*(uint32_t *) out =", "return")
+    }`,
+    `${a5hash}
+extern "C" uint32_t ffi(uint8_t *key, size_t len, uint32_t seed) {
+  return a5hash32(key, len, seed);
+}`,
+    `${a5hash}
+extern "C" uint64_t ffi(uint8_t *key, size_t len, uint64_t seed) {
+  return a5hash(key, len, seed);
+}`,
+  ].map(async (cpp) => {
+    const tmp = await Deno.makeTempDir();
+    await Deno.writeTextFile(`${tmp}/lib.cpp`, cpp);
+    assert(
+      (await new Deno.Command("g++", {
+        args: ["-shared", "-o", `${tmp}/lib.so`, `${tmp}/lib.cpp`],
+      }).output()).success,
+    );
+    const bin = await Deno.readFile(`${tmp}/lib.so`);
+    const [compressed] = await Promise.all([
+      zip(bin, new CompressionStream("gzip")),
+      await Deno.remove(tmp, { recursive: true }),
+    ]);
+    return compressed.toBase64();
+  })).then(([oaat32, a5hash32, a5hash64]) => ({ oaat32, a5hash32, a5hash64 }))
+).then(set(import.meta));
