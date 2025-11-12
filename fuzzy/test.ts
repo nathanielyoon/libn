b8d76b20617e400cd2a108f77517a59621bf6efb
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-12 17:06:45 -0500

    fix: use fc helpers

--- fuzzy/test.ts
+++ fuzzy/test.ts
@@ -6,20 +6,17 @@ import {
   assertLessOrEqual,
 } from "@std/assert";
 import fc from "fast-check";
+import { fcStr } from "../test.ts";
 
 Deno.test("includes :: includes", () => {
-  fc.assert(fc.property(
-    fc.string({ size: "medium", unit: "grapheme" }),
-    fc.string({ size: "medium", unit: "grapheme" }),
-    (one, two) => {
-      if (one.includes(two)) assert(includes(one, two));
-      if (two.includes(one)) assert(includes(two, one));
-    },
-  ));
+  fc.assert(fc.property(fcStr(), fcStr(), (one, two) => {
+    if (one.includes(two)) assert(includes(one, two));
+    if (two.includes(one)) assert(includes(two, one));
+  }));
 });
 Deno.test("includes : partial substring", () => {
   fc.assert(fc.property(
-    fc.string().chain(($) =>
+    fcStr().chain(($) =>
       fc.record({
         source: fc.constant($),
         target: fc.subarray($.split("")).map(($) => $.join("")),
@@ -32,13 +29,10 @@ Deno.test("includes : partial substring", () => {
 });
 Deno.test("includes : longer target", () => {
   fc.assert(fc.property(
-    fc.string({ unit: "grapheme" }).chain(($) =>
+    fcStr().chain(($) =>
       fc.record({
         shorter: fc.constant($),
-        longer: fc.string({
-          unit: "grapheme",
-          minLength: [...$].length + 1,
-        }),
+        longer: fcStr({ minLength: [...$].length + 1 }),
       })
     ),
     ({ shorter, longer }) => {
@@ -48,14 +42,10 @@ Deno.test("includes : longer target", () => {
 });
 Deno.test("includes : same-length strings", () => {
   fc.assert(fc.property(
-    fc.string({ unit: "grapheme" }).chain(($) =>
+    fcStr().chain(($) =>
       fc.record({
         one: fc.constant($),
-        two: fc.string({
-          unit: "grapheme",
-          minLength: $.length,
-          maxLength: $.length,
-        }),
+        two: fcStr({ minLength: $.length, maxLength: $.length }),
       })
     ),
     ({ one, two }) => {
@@ -76,23 +66,19 @@ const levenshtein = (one: string, two: string) => {
 };
 Deno.test("distance :: levenshtein", () => {
   fc.assert(fc.property(
-    fc.string({ size: "medium", unit: "grapheme", minLength: 1 }),
-    fc.string({ size: "medium", unit: "grapheme", minLength: 1 }),
+    fcStr({ minLength: 1 }),
+    fcStr({ minLength: 1 }),
     (one, two) => {
       assertEquals(distance(one, two), levenshtein(one, two));
     },
   ));
 });
 Deno.test("distance : levenshtein bounds", () => {
-  fc.assert(fc.property(
-    fc.string({ size: "medium", unit: "grapheme" }),
-    fc.string({ size: "medium", unit: "grapheme" }),
-    (one, two) => {
-      assertLessOrEqual(distance(one, two), Math.max(one.length, two.length));
-      assertGreaterOrEqual(
-        distance(one, two),
-        Math.abs(one.length - two.length),
-      );
-    },
-  ));
+  fc.assert(fc.property(fcStr(), fcStr(), (one, two) => {
+    assertLessOrEqual(distance(one, two), Math.max(one.length, two.length));
+    assertGreaterOrEqual(
+      distance(one, two),
+      Math.abs(one.length - two.length),
+    );
+  }));
 });
