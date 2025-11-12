6998c5e77b0b96490709631dba5e6aff9bfbd68a
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-12 17:02:31 -0500

    fix: use helpers

--- base/test.ts
+++ base/test.ts
@@ -15,7 +15,7 @@ import {
   assertThrows,
 } from "@std/assert";
 import fc from "fast-check";
-import { get, set } from "../test.ts";
+import { fcBin, fcStr } from "../test.ts";
 import vectors from "./vectors.json" with { type: "json" };
 
 Deno.test("b16 : vectors", () => {
@@ -27,18 +27,18 @@ Deno.test("b16 : vectors", () => {
   }
 });
 Deno.test("b16 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enB16($);
     assertMatch(string, B16), assertEquals(deB16(string), $);
   }));
 });
 Deno.test("b16.enB16 :: built-in toHex", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     assertEquals(enB16($), $.toHex().toUpperCase());
   }));
 });
 Deno.test("b16.deB16 :: built-in fromHex", () => {
-  fc.assert(fc.property(fc.uint8Array().map(enB16), ($) => {
+  fc.assert(fc.property(fcBin().map(enB16), ($) => {
     assertEquals(deB16($), Uint8Array.fromHex($));
   }));
 });
@@ -46,12 +46,9 @@ Deno.test("b16.B16 : invalid base16", () => {
   for (const $ of "GHIJKLMNOPQRSTUVWXYZghijklmnopqrstuvwxyz") {
     assertNotMatch($.repeat(2), B16);
   }
-  fc.assert(fc.property(
-    fc.stringMatching(RegExp(`^[\\dA-Fa-f]${B16.source.slice(1)}`)),
-    ($) => {
-      assertNotMatch($, B16);
-    },
-  ));
+  fc.assert(fc.property(fcStr(`^[\\dA-Fa-f]${B16.source.slice(1)}`), ($) => {
+    assertNotMatch($, B16);
+  }));
 });
 
 Deno.test("b32 : vectors", () => {
@@ -63,7 +60,7 @@ Deno.test("b32 : vectors", () => {
   }
 });
 Deno.test("b32 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enB32($);
     assertMatch(string, B32), assertEquals(deB32(string), $);
   }));
@@ -81,7 +78,7 @@ Deno.test("h32 : vectors", () => {
   }
 });
 Deno.test("h32 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enH32($);
     assertMatch(string, H32), assertEquals(deH32(string), $);
   }));
@@ -99,7 +96,7 @@ Deno.test("c32 : vectors", () => {
   }
 });
 Deno.test("c32 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enC32($);
     assertMatch(string, C32), assertEquals(deC32(string), $);
   }));
@@ -108,7 +105,7 @@ Deno.test("c32.C32 : invalid Crockford base32", () => {
   for (const $ of "Uu+/_") assertNotMatch($, C32);
 });
 Deno.test("c32.C32 : similar characters/hyphens", () => {
-  fc.assert(fc.property(fc.stringMatching(C32), ($) => {
+  fc.assert(fc.property(fcStr(C32), ($) => {
     const ok = deC32($);
     assertEquals(deC32($.toLowerCase()), ok);
     for (const no of "Oo") assertEquals(deC32($.replaceAll("0", no)), ok);
@@ -125,7 +122,7 @@ Deno.test("b58 : vectors", () => {
   }
 });
 Deno.test("b58 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enB58($);
     assertMatch(string, B58), assertEquals(deB58(string), $);
   }));
@@ -142,18 +139,18 @@ Deno.test("b64 : vectors", () => {
   }
 });
 Deno.test("b64 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enB64($);
     assertMatch(string, B64), assertEquals(deB64(string), $);
   }));
 });
 Deno.test("b64.enB64 :: built-in toBase64", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     assertEquals(enB64($), $.toBase64());
   }));
 });
 Deno.test("b64.deB64 :: built-in fromBase64", () => {
-  fc.assert(fc.property(fc.uint8Array().map(enB64), ($) => {
+  fc.assert(fc.property(fcBin().map(enB64), ($) => {
     assertEquals(deB64($), Uint8Array.fromBase64($));
   }));
 });
@@ -174,13 +171,13 @@ Deno.test("u64 : vectors", () => {
   }
 });
 Deno.test("u64 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enU64($);
     assertMatch(string, U64), assertEquals(deU64(string), $);
   }));
 });
 Deno.test("u64.enU64 :: built-in toBase64", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     assertEquals(
       enU64($),
       $.toBase64({ alphabet: "base64url", omitPadding: true }),
@@ -188,7 +185,7 @@ Deno.test("u64.enU64 :: built-in toBase64", () => {
   }));
 });
 Deno.test("u64.deU64 :: built-in fromBase64", () => {
-  fc.assert(fc.property(fc.uint8Array().map(enU64), ($) => {
+  fc.assert(fc.property(fcBin().map(enU64), ($) => {
     assertEquals(deU64($), Uint8Array.fromBase64($, { alphabet: "base64url" }));
   }));
 });
@@ -208,9 +205,7 @@ Deno.test("z85.Z85 : invalid Z85", () => {
 });
 Deno.test("z85 : binary", () => {
   fc.assert(fc.property(
-    fc.integer({ min: 0, max: 1e3 }).chain(($) =>
-      fc.uint8Array({ minLength: $ >> 2 << 2, maxLength: $ >> 2 << 2 })
-    ),
+    fc.integer({ min: 0, max: 1e3 }).chain(($) => fcBin($ >> 2 << 2)),
     ($) => {
       const string = enZ85($);
       assertMatch(string, Z85), assertEquals(deZ85(string), $);
@@ -226,7 +221,7 @@ Deno.test("a85 : vectors", () => {
   }
 });
 Deno.test("a85 : binary", () => {
-  fc.assert(fc.property(fc.uint8Array(), ($) => {
+  fc.assert(fc.property(fcBin(), ($) => {
     const string = enA85($);
     assertMatch(string, A85), assertEquals(deA85(string), $);
   }));
@@ -246,60 +241,3 @@ Deno.test("a85.enA85 : all-zero chunks", () => {
 Deno.test("a85.A85 : invalid Ascii85", () => {
   for (const $ of "vwxy{|}~") assertNotMatch($, A85);
 });
-
-import.meta.main && Promise.all([
-  get`www.rfc-editor.org/rfc/rfc4648.txt${25691}${26723}`,
-  get`crockford.com/base32.html${2215}${5383}`,
-  get`/zeromq/rfc/3d4c0cef87ed761fe09ab9abf8a6e5ea45df0e9f/src/spec_32.c${4717}`,
-  get`/bitcoin/bitcoin/5dd3a0d8a899e4c7263d5b999135f4d7584e1244/src/test/data/base58_encode_decode.json`,
-  get`en.wikipedia.org/w/index.php?title=Ascii85&oldid=1305034107${46088}${69413}`,
-]).then(([rfc4648, crockford, spec32, base58, wikipedia]) => ({
-  ...([
-    ["16", true],
-    ["32", false],
-    ["32-HEX", false, "h32"],
-    ["64", true],
-    ["64", false, "u64"],
-  ] as const).reduce((to, [base, pad, key]) => ({
-    ...to,
-    [key ?? `b${base}`]: Array.from(
-      rfc4648.matchAll(RegExp(`BASE${base}\\("(.*)"\\) = "(.*)"`, "g")),
-      ([_, binary, string]) => ({
-        binary,
-        string: pad ? string : string.replace(/=+$/, ""),
-      }),
-    ),
-  }), {}),
-  c32: Array.from(
-    crockford.matchAll(
-      /<tr>.*?<td>(\d+)<\/td>.*?<td><code>[\s\dA-Za-z]+<\/code><\/td>.*?<td><code>([\dA-Z])<\/code><\/td>.*?<\/tr>/gs,
-    ),
-    ([_, value, encode]) => {
-      let total = 0;
-      for (let base = +value, z = 0; z < 8; ++z) total = total * 32 + base;
-      const binary = new Uint8Array(8);
-      new DataView(binary.buffer).setBigUint64(0, BigInt(total));
-      return { binary: binary.subarray(3).toHex(), string: encode.repeat(8) };
-    },
-  ),
-  b58: JSON.parse(base58).map(($: [string, string]) => ({
-    binary: $[0],
-    string: $[1],
-  })),
-  z85: Array.from([1, 2], ($) => {
-    const [_, bytes, string] = RegExp(
-      `byte test_data_${$} \\[\\d+\\] = \\{(.+?)\\};.*?encoded = Z85_encode \\(test_data_${$}.*?assert \\(streq \\(encoded, "(.+?)"\\)\\)`,
-      "s",
-    ).exec(spec32)!;
-    return { binary: bytes.match(/(?<=0x)[\dA-F]{2}\b/g)!.join(""), string };
-  }),
-  a85: [{
-    binary: /<code>(.{269})<\/code>/s.exec(wikipedia)![1],
-    string: /<pre>(.{395})<\/pre>/s.exec(wikipedia)![1]
-      .replaceAll("&lt;", "<").replaceAll("&gt;", ">")
-      .replaceAll("&amp;", "&").replaceAll("\n", ""),
-  }, {
-    binary: "\0".repeat(4),
-    string: /<code>(.)<\/code>/.exec(wikipedia)![1],
-  }],
-})).then(set(import.meta));
