5b7dff1b1ecadb2a5b83b3ddead50a6fad13b8e8
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-12 17:28:00 -0500

    fix: separate logic

--- hash/vectors.ts
+++ hash/vectors.ts
@@ -1,47 +1,47 @@
 import { get, set } from "../test.ts";
 
-const [nist, hmac, hkdf, blake2, blake3] = await Promise.all([
-  Promise.all([224, 256, 384, 512].map((size) =>
-    get([
-      `/usnistgov/ACVP-Server/fb44dce5257aba23088256e63c9b950db6967610/gen-val/json-files/SHA2-${size}-1.0/internalProjection.json`,
-    ]).then(JSON.parse).then((groups: {
-      testGroups: { tests: { msg: string; len: number; md: string }[] }[];
-    }) =>
-      groups.testGroups[0].tests.filter(($) => $.len % 8 === 0).map(($) => ({
-        data: $.msg,
-        digest: $.md,
-      }))
-    )
-  )),
-  get`/C2SP/wycheproof/427a648c39e2edea11b75bcdcd72eea3da482d6f/testvectors_v1/hmac_sha256_test.json`,
-  get`/C2SP/wycheproof/427a648c39e2edea11b75bcdcd72eea3da482d6f/testvectors_v1/hkdf_sha256_test.json`,
-  Promise.all(["s", "b"].map((flavor) =>
-    get([
-      `/BLAKE2/BLAKE2/eec32b7170d8dbe4eb59c9afad2ee9297393fb5b/testvectors/blake2${flavor}-kat.txt`,
-    ]).then(($) =>
-      $.trim().split("\n\n").map((chunk) =>
-        Object.fromEntries(chunk.split("\n").map((line) => line.split(":\t")))
-      )
-    )
-  )),
-  get`/BLAKE3-team/BLAKE3/ae3e8e6b3a5ae3190ca5d62820789b17886a0038/test_vectors/test_vectors.json`
-    .then<{
-      key: string;
-      context_string: string;
-      cases: {
-        input_len: number;
-        hash: string;
-        keyed_hash: string;
-        derive_key: string;
-      }[];
-    }>(JSON.parse),
-]);
+const [sha224, sha256, sha384, sha512, hmac, hkdf, blake2s, blake2b, blake3] =
+  await Promise.all([
+    get`/usnistgov/ACVP-Server/fb44dce5257aba23088256e63c9b950db6967610/gen-val/json-files/SHA2-224-1.0/internalProjection.json`,
+    get`/usnistgov/ACVP-Server/fb44dce5257aba23088256e63c9b950db6967610/gen-val/json-files/SHA2-256-1.0/internalProjection.json`,
+    get`/usnistgov/ACVP-Server/fb44dce5257aba23088256e63c9b950db6967610/gen-val/json-files/SHA2-384-1.0/internalProjection.json`,
+    get`/usnistgov/ACVP-Server/fb44dce5257aba23088256e63c9b950db6967610/gen-val/json-files/SHA2-512-1.0/internalProjection.json`,
+    get`/C2SP/wycheproof/427a648c39e2edea11b75bcdcd72eea3da482d6f/testvectors_v1/hmac_sha256_test.json`,
+    get`/C2SP/wycheproof/427a648c39e2edea11b75bcdcd72eea3da482d6f/testvectors_v1/hkdf_sha256_test.json`,
+    ...["s", "b"].map((flavor) =>
+      get([
+        `/BLAKE2/BLAKE2/eec32b7170d8dbe4eb59c9afad2ee9297393fb5b/testvectors/blake2${flavor}-kat.txt`,
+      ])
+    ) as [Promise<string>, Promise<string>],
+    get`/BLAKE3-team/BLAKE3/ae3e8e6b3a5ae3190ca5d62820789b17886a0038/test_vectors/test_vectors.json`
+      .then<{
+        key: string;
+        context_string: string;
+        cases: {
+          input_len: number;
+          hash: string;
+          keyed_hash: string;
+          derive_key: string;
+        }[];
+      }>(JSON.parse),
+  ]);
 
+const nist = ($: string) =>
+  JSON.parse($).testGroups[0].tests
+    .filter(($: { len: number }) => $.len % 8 === 0)
+    .map(($: { msg: string; md: string }) => ({
+      data: $.msg,
+      digest: $.md,
+    }));
+const blake2 = ($: string) =>
+  $.trim().split("\n\n").map((chunk) =>
+    Object.fromEntries(chunk.split("\n").map((line) => line.split(":\t")))
+  );
 await set(import.meta, {
-  sha224: nist[0],
-  sha256: nist[1],
-  sha384: nist[2],
-  sha512: nist[3],
+  sha224: nist(sha224),
+  sha256: nist(sha256),
+  sha384: nist(sha384),
+  sha512: nist(sha512),
   hmac: JSON.parse(hmac).testGroups.flatMap((group: {
     tests: {
       key: string;
@@ -75,8 +75,8 @@ await set(import.meta, {
       derived: $.result === "valid" ? $.okm : "",
     }))
   ),
-  blake2s: blake2[0],
-  blake2b: blake2[1],
+  blake2s: blake2(blake2s),
+  blake2b: blake2(blake2b),
   blake3: {
     key: blake3.key,
     context: blake3.context_string,
