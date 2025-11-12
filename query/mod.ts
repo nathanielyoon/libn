6eac703b29ba5e71174c84730596a29334acb152
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-12 18:19:13 -0500

    fix: use globalthis

--- query/mod.ts
+++ query/mod.ts
@@ -16,32 +16,34 @@ type Either<A, B, C extends string> = C extends keyof A ? A[C]
   : C extends keyof B ? B[C]
   : Element;
 /** Query-selects an element. */
-export const queryFirst =
-  (($: string, parent = document.body) => parent.querySelector($)) as {
-    <A extends keyof Html>(tag: A, parent?: HTMLElement): Html[A] | null;
-    <A extends keyof Svg>(tag: A, parent?: SVGElement): Svg[A] | null;
-    <A extends string>(
-      selector: A,
-      parent?: HTMLElement,
-    ): Either<Html, Svg, Trim<List<A>>> | null;
-    <A extends string>(
-      selector: A,
-      parent?: SVGElement,
-    ): Either<Svg, Html, Trim<List<A>>> | null;
-    <A extends Element>(selector: string, parent?: Element): A | null;
-  };
+export const queryFirst = (($: string, parent = globalThis.document.body) => (
+  parent.querySelector($)
+)) as {
+  <A extends keyof Html>(tag: A, parent?: HTMLElement): Html[A] | null;
+  <A extends keyof Svg>(tag: A, parent?: SVGElement): Svg[A] | null;
+  <A extends string>(
+    selector: A,
+    parent?: HTMLElement,
+  ): Either<Html, Svg, Trim<List<A>>> | null;
+  <A extends string>(
+    selector: A,
+    parent?: SVGElement,
+  ): Either<Svg, Html, Trim<List<A>>> | null;
+  <A extends Element>(selector: string, parent?: Element): A | null;
+};
 /** Query-selects all elements. */
-export const queryEvery =
-  (($: string, parent = document.body) => [...parent.querySelectorAll($)]) as {
-    <A extends keyof Html>(tag: A, parent?: HTMLElement): Html[A][];
-    <A extends keyof Svg>(tag: A, parent?: SVGElement): Svg[A][];
-    <A extends string>(
-      selector: A,
-      parent?: HTMLElement,
-    ): Either<Html, Svg, Trim<List<A>>>[];
-    <A extends string>(
-      selector: A,
-      parent?: SVGElement,
-    ): Either<Svg, Html, Trim<List<A>>>[];
-    <A extends Element>(selector: string, parent?: Element): A[];
-  };
+export const queryEvery = (($: string, parent = globalThis.document.body) => [
+  ...parent.querySelectorAll($),
+]) as {
+  <A extends keyof Html>(tag: A, parent?: HTMLElement): Html[A][];
+  <A extends keyof Svg>(tag: A, parent?: SVGElement): Svg[A][];
+  <A extends string>(
+    selector: A,
+    parent?: HTMLElement,
+  ): Either<Html, Svg, Trim<List<A>>>[];
+  <A extends string>(
+    selector: A,
+    parent?: SVGElement,
+  ): Either<Svg, Html, Trim<List<A>>>[];
+  <A extends Element>(selector: string, parent?: Element): A[];
+};
