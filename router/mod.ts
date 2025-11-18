c9a87056e8f16b56b8a0b06ef180014fe2f3ae51
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-17 22:39:45 -0500

    new: rewrite simpler

--- router/mod.ts
+++ router/mod.ts
@@ -1,28 +1,123 @@
-interface Hollow extends Request {
-  readonly method: "GET" | "HEAD" | "CONNECT" | "TRACE";
-  // No body in these requests.
-  readonly body: never;
-  readonly bodyUsed: false;
-  readonly arrayBuffer: never;
-  readonly blob: never;
-  readonly bytes: never;
-  readonly formData: never;
-  readonly json: never;
-  readonly text: never;
-  // Clones to same type.
-  readonly clone: () => Hollow;
+/** HTTP methods. */
+export type Method =
+  | ("GET" | "HEAD" | "CONNECT" | "TRACE") // has no body
+  | ("POST" | "PUT" | "DELETE" | "OPTIONS" | "PATCH"); // (maybe) has body
+type Parameters<A extends string, B extends string> = string extends A
+  ? { [_: string]: string | undefined } & { ""?: string[] }
+  : A extends `?${infer C}`
+    ? C extends "" | `/${string}` ? { [_ in B]: string } & { "": string[] }
+    : C extends `${infer D}/${infer E}`
+      ? D extends B ? never : Parameters<E, B | D>
+    : { [_ in B | C]: string }
+  : A extends `${string}/${infer D}` ? Parameters<D, B>
+  : { [_ in B]: string };
+type From<A extends Method, B extends string> = {
+  url: URL;
+  path: Parameters<B, never>;
+  request:
+    & { method: A; clone: () => From<A, B>["request"] }
+    & Omit<
+      Request,
+      | ("method" | "clone")
+      | (A extends "GET" | "HEAD" | "CONNECT" | "TRACE" ?
+          | ("body" | "bodyUsed")
+          | ("arrayBuffer" | "blob" | "bytes" | "formData" | "json" | "text")
+        : never)
+    >;
+};
+type Into = string | { [_: string]: unknown } | BufferSource | Blob | Response;
+/** @internal */
+type Handler<A, B extends Method, C extends string> = B extends B
+  ? (this: A, from: From<B, C>) => Into | Promise<Into>
+  : never;
+class Node<A> {
+  children: { [_: string]: Node<A> } = {};
+  one?: { name: string; node: Node<A> };
+  all?: Node<A>;
+  handlers: { [C in Method]?: Handler<A, C, string> } = {};
 }
-interface Filled extends Request {
-  readonly method: "POST" | "PUT" | "DELETE" | "OPTIONS" | "PATCH";
-  // Clones to same type.
-  readonly clone: () => Filled;
+const split = ($: string) => $.match(/\/(?!$)[^/]*/g) ?? [];
+const wrap = ($: unknown) =>
+  $ instanceof Error
+    ? { name: $.name, message: $.message, cause: $.cause, stack: $.stack }
+    : { name: "", message: `${$}`, cause: $ };
+const fail = ($: unknown) =>
+  new Response(JSON.stringify($), {
+    status: 500,
+    headers: { "content-type": "application/json" },
+  });
+/** HTTP router base class. */
+export class Router<A> {
+  private routes: { method: Method; path: string[]; handler: any }[] = [];
+  private tree: Node<A> | null = null;
+  private catcher: (
+    this: A,
+    error: unknown,
+    from: From<Method, string>,
+  ) => Response | Promise<Response> = ($) => fail(wrap($));
+  /** Adds a route handler. */
+  if<B extends Method, C extends string>(
+    method: B,
+    path: `/${C}`,
+    handler: Handler<A, B, C>,
+  ): this {
+    this.routes.push({ method, path: split(path), handler }), this.tree = null;
+    return this;
+  }
+  /** Adds an application-wide error handler. */
+  else(catcher: typeof this.catcher): this {
+    return this.catcher = catcher, this;
+  }
+  /** Builds and saves the routing tree. */
+  private get router(): Node<A> {
+    if (this.tree) return this.tree;
+    const to = new Node();
+    for (let prev, next, use, z = 0, y; z < this.routes.length; ++z) {
+      for (prev = to, next = this.routes[z], y = 0; y < next.path.length; ++y) {
+        if (use = /^\/\?(.*)/.exec(next.path[y])) {
+          if (use[1]) prev.one = { name: use[1], node: prev = new Node() };
+          else prev = prev.all = new Node();
+        } else prev = prev.children[next.path[y]] ??= new Node();
+      }
+      prev.handlers[next.method] = next.handler;
+    }
+    return this.tree = to;
+  }
+  /** Handles an incoming request. */
+  async route($: A, request: Request): Promise<Response> {
+    const from = {
+      url: new URL(request.url),
+      path: {} as Parameters<string, never>,
+      request: request as any,
+    };
+    let node = this.router;
+    for (let path = split(from.url.pathname), on, z = 0; z < path.length; ++z) {
+      if (node.children[on = path[z]]) node = node.children[on];
+      else if (node.one) from.path[node.one.name] = on, node = node.one.node;
+      else if (node.all) {
+        from.path[""] = path.slice(z), node = node.all;
+        break;
+      } else return new Response(null, { status: 404 });
+    }
+    const handler = node.handlers[request.method as Method];
+    if (!handler) return new Response(null, { status: 405 });
+    let into, type;
+    try {
+      into = await handler.call($, from);
+    } catch (first) {
+      try {
+        into = await this.catcher.call($, first, from);
+      } catch (second) { // uh oh!
+        into = fail([wrap(first), wrap(second)]);
+      }
+    }
+    if (into instanceof Response) return into;
+    if (into instanceof Blob) type = into.type;
+    else if (ArrayBuffer.isView(into) || into instanceof ArrayBuffer) {
+      type = "application/octet-stream";
+    } else if (typeof into === "object") {
+      into = JSON.stringify(into), type = "application/json";
+    } else type = "text/plain";
+    return new Response(into, { headers: { "content-type": type } });
+  }
 }
-type Handled =
-  | string
-  | BufferSource
-  | Iterable<Uint8Array>
-  | AsyncIterable<Uint8Array>
-  | ReadableStream<Uint8Array>
-  | Blob
-  | Response;
-type Handler = (request: Hollow | Filled) => Handled | Promise<Handled>;
