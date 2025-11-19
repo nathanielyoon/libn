e69223f1cee0a62efa2741890f064425597b0dd8
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-18 21:05:15 -0500

    new: add custom segment arbitrary

--- router/test.ts
+++ router/test.ts
@@ -9,18 +9,54 @@ const assertResponse = async (actual: Response, expected: Response) => {
   assertEquals(actual.url, expected.url);
   assertEquals(actual.status, expected.status);
   assertEquals(actual.headers, expected.headers);
-  const all = await Promise.all([actual.arrayBuffer(), expected.arrayBuffer()]);
+  const all = await Promise.all([actual.text(), expected.text()]);
   assertEquals(all[0], all[1]);
 };
-const fcPath = fc.webPath().filter(($) =>
-  URL.canParse($, "http://localhost")
-) as fc.Arbitrary<`/${string}`>;
+const fcPart = fcStr({
+  unit: fc.constantFrom(
+    // ALPHA
+    ..."ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    ..."abcdefghijklmnopqrstuvwxyz",
+    // DIGIT
+    ..."0123456789",
+    // other unreserved
+    ..."-._~",
+    // pct-encoded
+    ...Array(128).keys().map(($) => `%${$.toString(16).padStart(2, "0")}`),
+    // sub-delims
+    ..."!$&'()*+,;=",
+    // others
+    ...":@",
+  ),
+  minLength: 1,
+});
+const fcPath = fc.array(fcPart, { minLength: 1 }).map(($) => ({
+  all: $,
+  one: `/${$.join("/")}` as const,
+}));
 
 Deno.test("router.route : static routes", async () => {
-  await fc.assert(fc.asyncProperty(fcPath, async (path) => {
+  await fc.assert(fc.asyncProperty(fcPath, async ({ one }) => {
     await assertResponse(
-      await new Router().route("GET", path, () => path).fetch(request(path)),
-      new Response(path),
+      await new Router().route("GET", one, () => one).fetch(request(one)),
+      new Response(one),
     );
   }));
+});
+Deno.test("router.route : dynamic routes", async () => {
+  await fc.assert(fc.asyncProperty(
+    fcPath,
+    fcStr(/^[^\s/?]+$/),
+    fcPart,
+    async (path, key, value) => {
+      await assertResponse(
+        await new Router().route(
+          "GET",
+          `${path.one}/?${key}`,
+          ($) => Response.json($.path),
+        ).fetch(request(`${path.one}/${value}`)),
+        Response.json({ [key]: decodeURIComponent(value) }),
+      );
+    },
+  ));
 });
