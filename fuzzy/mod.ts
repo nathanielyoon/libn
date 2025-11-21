b25832a3fe8c2fab81efc17c19dc224cd35852df
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-21 11:33:42 -0500

    fix: condense checks

--- fuzzy/mod.ts
+++ fuzzy/mod.ts
@@ -3,8 +3,7 @@
 /** Checks whether one string sorta-includes another. */
 export const includes = (source: string, target: string): boolean => {
   const max = source.length, min = target.length;
-  if (min > max) return false;
-  if (min === max) return source === target;
+  if (min >= max) return source === target;
   top: for (let z = 0, y = 0; z < min; ++z) {
     for (const next = target.charCodeAt(z); y < max; ++y) {
       if (source.charCodeAt(y) === next) continue top;
