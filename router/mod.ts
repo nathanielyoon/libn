d3cfad3dbbf0268651530cb6603452f0dde8fb29
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-22 08:33:59 -0500

    fix: improve response wrapping

--- router/mod.ts
+++ router/mod.ts
@@ -1,65 +1,71 @@
+import type { Result } from "@libn/result";
+
 const PATH =
   /^(?:\/(?:[A-Za-z\d-_~!$&'()*+,;=:@]|%[\dA-Fa-f]{2})+|\/#(?!__proto__)[^/#]+)+$|^\/$/;
 type Name<A extends string> = A extends `#${infer B}` ? B : never;
 type Path<A extends string, B extends string> = A extends
   `${infer C}/${infer D}` ? Path<D, B | Name<C>> : B | Name<A>;
-interface Source<A extends string = string> {
+interface On<A extends string = string> {
   url: URL;
   path: { [B in Path<A, never>]: string };
   request: Request;
 }
+type To = number | Error | ConstructorParameters<typeof Response>[0] | Response;
 /** @internal */
 type Handler<A extends unknown[], B extends string> = (
   this: Router<A>,
-  source: Source<B>,
-  ...context: A
-) => Response | Promise<Response>;
+  on: On<B>,
+  ...rest: A
+) => To | Result<To, To> | Promise<To | Result<To, To>>;
 class Node<A> {
   self: { [_: string]: A | undefined } = Object.create(null);
   next?: { name: string; node: Node<A> };
   rest: { [_: string]: Node<A> } = Object.create(null);
 }
 /** Wraps an `Error` in a `Response`. */
-export const error = ($: Error, status: number): Response =>
-  Response.json({
-    name: $.name,
-    message: $.message,
-    cause: $.cause,
-    stack: $.stack?.match(/(?<=^\s*).+$/gm),
-  }, { status });
+export const error = ($: Error, status = 400): Response =>
+  Response.json(
+    { name: $.name, message: $.message, cause: $.cause, stack: $.stack },
+    { status },
+  );
 /** HTTP router. */
 export class Router<A extends unknown[] = []> {
   private tree: Node<Handler<A, string>> = new Node();
   /** Adds a route. */
-  route<B extends string>($: `${string} /${B}`, handler: Handler<A, B>): this {
-    const [, method, path] = /^(\S+) (\S+)$/.exec($) ?? [];
-    if (!PATH.test(path)) throw Error(`Invalid route: ${JSON.stringify($)}`);
-    let at = this.tree;
+  route<B extends string>(on: `${string} /${B}`, handler: Handler<A, B>): this {
+    const [, method, path] = /^(\S+) (\S+)$/.exec(on) ?? [];
+    if (!PATH.test(path)) throw Error(`Invalid route: ${JSON.stringify(on)}`);
+    let node = this.tree;
     for (const [part, name] of path.matchAll(/#([^/#]+)|[^/]+/g)) {
-      if (!name) at = at.rest[decodeURIComponent(part)] ??= new Node();
-      else if (name === at.next?.name) at = at.next.node;
-      else at.next = { name, node: at = new Node() };
+      if (!name) node = node.rest[decodeURIComponent(part)] ??= new Node();
+      else if (name === node.next?.name) node = node.next.node;
+      else node.next = { name, node: node = new Node() };
     }
-    return at.self[method] = handler, this;
+    for (const $ of method.split(",")) node.self[$] = handler;
+    return this;
   }
   /** Handles a request. */
-  fetch = async (request: Request, ...context: A): Promise<Response> => {
-    const source = { url: new URL(request.url), path: {} as any, request };
-    let at = this.tree;
-    for (let part of source.url.pathname.match(/(?<=\/)[^/]+/g) ?? []) {
-      if (at.rest[part = decodeURIComponent(part)]) at = at.rest[part];
-      else if (at.next) source.path[at.next.name] = part, at = at.next.node;
+  fetch = async (request: Request, ...rest: A): Promise<Response> => {
+    const on = { url: new URL(request.url), path: {} as any, request };
+    let node = this.tree, to, status;
+    for (let part of on.url.pathname.match(/(?<=\/)[^/]+/g) ?? []) {
+      if (node.rest[part = decodeURIComponent(part)]) node = node.rest[part];
+      else if (node.next) on.path[node.next.name] = part, node = node.next.node;
       else return new Response(null, { status: 404 });
     }
-    const to = at.self[request.method];
-    if (!to) return new Response(null, { status: 405 });
+    const handler = node.self[request.method];
+    if (!handler) return new Response(null, { status: 405 });
     try {
-      return await to.call(this, source, ...context);
-    } catch (cause) {
-      return error(
-        cause instanceof Error ? cause : Error(`${cause}`, { cause }),
-        500,
-      );
+      to = await handler.call(this, on, ...rest);
+      if (typeof to === "object" && to !== null && "state" in to) {
+        status = to.state ? 200 : 400, to = to.value;
+      }
+    } catch ($) {
+      to = $ instanceof Error ? $ : Error(`${$}`, { cause: $ }), status = 500;
     }
+    if (to instanceof Response) return to;
+    else if (typeof to === "number") return new Response(null, { status: to });
+    else if (to instanceof Error) return error(to, status);
+    else return new Response(to, { status: status ?? 200 });
   };
 }
