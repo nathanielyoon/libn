669b050a7a313e25cc77254823eda75a55b8517d
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-22 21:26:54 -0500

    fix: improve parameter names

--- test.ts
+++ test.ts
@@ -20,16 +20,16 @@ export const zip = async (
   )).flatMap(($) => [...$]));
 /** Stringifies, checks, and writes test vectors. */
 export const set = async (
-  at: ImportMeta,
-  $: any,
+  meta: ImportMeta,
+  vectors: any,
   expected: string,
 ): Promise<void> => {
-  const json = JSON.stringify($);
+  const json = JSON.stringify(vectors);
   const actual = new Uint8Array(
     await crypto.subtle.digest("SHA-256", new TextEncoder().encode(json)),
   ).reduce((hex, byte) => hex + byte.toString(16).padStart(2, "0"), "");
   if (actual !== expected) throw Error(JSON.stringify({ actual, expected }));
-  await Deno.writeTextFile(new URL(at.resolve("./vectors.json")), json);
+  await Deno.writeTextFile(new URL(meta.resolve("./vectors.json")), json);
 };
 /** @internal */
 type Fc<A, B> = A extends ($?: infer C) => infer D ? ($?: C | B) => D : never;
