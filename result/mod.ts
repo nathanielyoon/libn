e234e2d5a369cbdc25f604ae0f439fb8175ac0fb
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-11-29 13:24:45 -0500

    new: rework

--- result/mod.ts
+++ result/mod.ts
@@ -1,45 +1,71 @@
-/** @internal */
-type Union = { [_: PropertyKey]: unknown };
 /** Success or tagged failure. */
-export type Result<A = any, B extends Union = Union, C extends string = never> =
+export type Result<A, B extends { [_: PropertyKey]: unknown }> =
   | { error: null; value: A }
-  | { [D in keyof B]: { error: D; value: B[D] } }[keyof B]
-  | (C extends never ? never : { error: C; value: Error });
+  | { [D in keyof B]: { error: D; value: B[D] } }[keyof B];
 /** @internal */
 declare const KEY: unique symbol;
-type Key<A extends Union> = symbol & { [KEY]: A };
+/** @internal */
+type Key<A extends { [_: PropertyKey]: unknown }> = symbol & { [KEY]: A };
 /** Creates a unique symbol associated with an error union. */
-export const define: <A extends Union = {}>(description?: string) => Key<A> =
-  Symbol as any;
+export const define: <A extends { [_: PropertyKey]: unknown } = {}>(
+  description?: string,
+) => Key<A> = Symbol as any;
+type Sync<A, B extends { [_: PropertyKey]: unknown }> = A extends A
+  ? A extends Promise<infer C> ? Promise<Result<C, B>> : Result<A, B>
+  : never;
 function no(this: symbol, error: PropertyKey, value: unknown): never {
   throw { [this]: { error, value } };
 }
-const unwrap = (key: symbol, $: any, or?: string) => {
-  if (Object.hasOwn($ ?? {}, key)) return $[key];
-  if (or !== undefined && $ instanceof Error) return { error: or, value: $ };
-  throw $;
-};
-/** Wraps an unsafe function in a `Result`. */
-export const wrap = <A, B extends Union, C extends string = never>(
+function or(this: symbol, result: any, map?: { [_: PropertyKey]: any }) {
+  if (result.error === null) return result.value;
+  const mapper = map?.[result.error];
+  if (mapper) return mapper(result.value);
+  throw { [this]: result };
+}
+function open(this: symbol, value: any) {
+  if (Object.hasOwn(value ?? {}, this)) return value[this];
+  throw value;
+}
+/** Wraps throws in a `Result`. */
+export const wrap = <A, B extends { [_: PropertyKey]: unknown }>(
   key: Key<B>,
-  unsafe: (no: <C extends keyof B>(error: C, value: B[C]) => never) => A,
-  or?: C,
-): Result<A, B, C> => {
+  unsafe: (no: <C extends keyof B>(error: C, value: B[C]) => never, or: {
+    <C>(result: Result<C, B>): C;
+    <C, D extends { [_: PropertyKey]: unknown }, E>(
+      result: Result<C, D>,
+      map:
+        & { [F in Exclude<keyof D, keyof B>]: (value: D[F]) => E }
+        & { [F in Extract<keyof D, keyof B>]?: (value: D[F]) => E },
+    ): C | E;
+  }) => A,
+): Sync<A, B> => {
   try {
-    return { error: null, value: unsafe(no.bind(key)) };
-  } catch (thrown) {
-    return unwrap(key, thrown, or);
+    const value = unsafe(no.bind(key), or.bind(key));
+    return (value instanceof Promise
+      ? value.then(($) => ({ error: null, value: $ })).catch(open.bind(key))
+      : { error: null, value }) as Sync<A, B>;
+  } catch (value) {
+    return open.call(key, value);
   }
 };
-/** Wraps an unsafe async function in a promised `Result`. */
-export const wait = async <A, B extends Union, C extends string = never>(
-  key: Key<B>,
-  unsafe: (no: <C extends keyof B>(error: C, value: B[C]) => never) => A,
-  or?: C,
-): Promise<Result<Awaited<A>, B, C>> => {
+function pass(this: PropertyKey, value: unknown) {
+  if (value instanceof Error) return { error: this, value };
+  throw value;
+}
+/** Wraps an unsafe function. */
+export const trap = <A extends unknown[], B, C extends PropertyKey = "error">(
+  unsafe: (...$: A) => B,
+  error?: C,
+): (...$: A) => Sync<B, { [_ in C]: Error }> =>
+(...$) => {
   try {
-    return { error: null, value: await unsafe(no.bind(key)) };
-  } catch (thrown) {
-    return unwrap(key, thrown, or);
+    const value = unsafe(...$);
+    return (value instanceof Promise
+      ? value.then(($) => ({ error: null, value: $ })).catch(
+        pass.bind(error! ?? "error"),
+      )
+      : { error: null, value }) as Sync<B, { [_ in C]: Error }>;
+  } catch (value) {
+    return pass.call(error! ?? "error", value) as Sync<B, { [_ in C]: Error }>;
   }
 };
